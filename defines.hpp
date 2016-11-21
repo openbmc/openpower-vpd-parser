fc520732cfad066f3be83c77e8dc3aa08ba0b34e
Deepak Kodihalli <dkodihal@in.ibm.com>
2016-11-20 22:50:39 -0600

    Add enums for OpenPOWER records and keywords
    OpenPOWER VPD is represented in a record/keyword format. This change
    adds enum definitions for records and record-contained keywords that the
    BMC is interested to parse out from an OpenPOWER VPD eeprom.
    
    Change-Id: I3ee0d85e509de15d192e795cb9b253688cad70a5
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ defines.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+namespace openpower
+{
+namespace vpd
+{
+
+/** @brief OpenPOWER VPD records we're interested in
+ *
+ *  VINI   Initial information, common to all OpenPOWER FRUs
+ *  OPFR   OpenPOWER FRU information, common to all OpenPOWER FRUs
+ *  OSYS   Information specific to a system board
+ */
+enum class Record
+{
+    VINI,
+    OPFR,
+    OSYS
+};
+
+namespace record
+{
+
+/** @brief OpenPOWER VPD keywords we're interested in
+ *
+ *  DR     FRU name/description
+ *  PN     FRU part number
+ *  SN     FRU serial number
+ *  CC     Customer Card Identification Number (CCIN)
+ *  HW     FRU version
+ *  B1     Custom manufacturing information about a FRU
+ *  VN     FRU manufacturer name
+ *  MB     FRU manufacture date
+ *  MM     FRU model
+ */
+enum class Keyword
+{
+    DR,
+    PN,
+    SN,
+    CC,
+    HW,
+    B1,
+    VN,
+    MB,
+    MM
+};
+
+} // namespeace record
+} // namespace vpd
+} // namespace openpower
