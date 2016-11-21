8302f4c4d52454994b39c7c020ffeef75af72523
Deepak Kodihalli <dkodihal@in.ibm.com>
2016-11-21 04:32:44 -0600

    Add API to provide access to parsed OpenPOWER VPD
    This change adds the Store class, which serves as a store for parsed
    OpenPOWER VPD, as well as provides access to the VPD specified by
    a record:keyword combination.
    
    Change-Id: Ie9efe5825a3e799d78b59333c06955b0214a6823
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ types.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <climits>
+#include <vector>
+
+namespace openpower
+{
+namespace vpd
+{
+
+/** @brief The OpenPOWER VPD, in binary, is specified as a
+ *         sequence of characters */
+static_assert((8 == CHAR_BIT), "A byte is not 8 bits!");
+using Byte = uint8_t;
+using Binary = std::vector<Byte>;
+
+} // namespace vpd
+} // namespace openpower
