f45269271bf05e06e14499323dc446e4bdcfd3a8
Deepak Kodihalli <dkodihal@in.ibm.com>
2016-11-22 10:45:38 -0600

    Implement parse API
    This change implements the parse API, which parses input OpenPOWER VPD.
    
    Change-Id: I646e75c642cf5872252633fef03a864e287f696b
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ parser.cpp
@@ -0,0 +1,17 @@
+#include <parser.hpp>
+#include <impl.hpp>
+
+namespace openpower
+{
+namespace vpd
+{
+
+Store parse(Binary&& vpd)
+{
+    parser::Impl p(std::move(vpd));
+    Store s = p.run();
+    return s;
+}
+
+} // namespace vpd
+} // namespace openpower
