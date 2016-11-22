ab7b632093b39294d24b043060931d4564422d9e
Deepak Kodihalli <dkodihal@in.ibm.com>
2016-11-22 11:42:51 -0600

    parser : implement header check
    This change implements a parser method to check if the OpenPOWER VPD has
    the mandatory header record, VHDR.
    
    Change-Id: I9e4cebd8f69e47e1ab0f1b569fe4af2c82a5a137
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ impl.cpp
@@ -0,0 +1,52 @@
+#include <exception>
+#include <iostream>
+#include "impl.hpp"
+
+namespace openpower
+{
+namespace vpd
+{
+namespace parser
+{
+
+namespace offsets
+{
+
+enum Offsets
+{
+    VHDR = 17
+};
+
+}
+
+namespace lengths
+{
+
+enum Lengths
+{
+    RECORD_NAME = 4,
+    RECORD_MIN = 44,
+};
+
+}
+
+void Impl::hasHeader() const
+{
+    if(_vpd.empty() || (lengths::RECORD_MIN > _vpd.size()))
+    {
+        throw std::runtime_error("Malformed VPD");
+    }
+    else
+    {
+        auto buf = _vpd.data() + offsets::VHDR;
+        std::string record(buf, lengths::RECORD_NAME);
+        if("VHDR" != record)
+        {
+            throw std::runtime_error("VHDR record not found");
+        }
+    }
+}
+
+} // namespace parser
+} // namespace vpd
+} // namespace openpower
