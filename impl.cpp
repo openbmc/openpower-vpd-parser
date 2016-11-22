d240f823e81b380481ea2dbba3720144386ed65d
Deepak Kodihalli <dkodihal@in.ibm.com>
2016-11-22 11:42:51 -0600

    parser : implement header check
    This change implements a parser method to check if the OpenPOWER VPD has
    the mandatory header record, VHDR.
    
    Change-Id: I9e4cebd8f69e47e1ab0f1b569fe4af2c82a5a137
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ impl.cpp
@@ -0,0 +1,144 @@
+#include <exception>
+#include <iostream>
+#include "impl.hpp"
+
+namespace openpower
+{
+namespace vpd
+{
+namespace parser
+{
+
+using RecordId = uint8_t;
+using RecordOffset = uint16_t;
+using RecordSize = uint16_t;
+using RecordType = uint16_t;
+using RecordLength = uint16_t;
+using KwSize = uint8_t;
+using KwLength = uint32_t;
+using ECCOffset = uint16_t;
+using ECCLength = uint16_t;
+
+namespace offsets
+{
+
+enum Offsets
+{
+    VHDR = 17,
+    VHDR_TOC_ENTRY = 29,
+    VTOC_PTR = 35,
+};
+
+}
+
+namespace lengths
+{
+
+enum Lengths
+{
+    RECORD_NAME = 4,
+    KW_NAME = 2,
+};
+
+}
+
+void Impl::hasHeader() const
+{
+    auto buf = _vpd.data() + offsets::VHDR;
+    std::string record(buf, lengths::RECORD_NAME);
+    if("VHDR" != record)
+    {
+        throw std::runtime_error("VHDR record not found");
+    }
+}
+
+OffsetList Impl::readTOC() const
+{
+    OffsetList offsets {};
+
+    // The offset to VTOC could be 1 or 2 bytes long
+    RecordOffset vtocOft = _vpd.at(offsets::VTOC_PTR);
+    RecordOffset highByte = _vpd.at(offsets::VTOC_PTR + 1);
+    if(highByte)
+    {
+        // Offset is 2 bytes long
+        vtocOft = (highByte << 8) |
+                  vtocOft;
+    }
+
+    // Got the offset to VTOC, skip past record header and keyword header
+    // to get to the record name.
+    auto vpdBuffer = _vpd.data() +
+                     vtocOft +
+                     sizeof(RecordId) +
+                     sizeof(RecordSize) +
+                     // Skip past the RT keyword, which contains
+                     // the record name.
+                     lengths::KW_NAME +
+                     sizeof(KwSize);
+
+    std::string record(vpdBuffer, lengths::RECORD_NAME);
+    if("VTOC" != record)
+    {
+        throw std::runtime_error("VTOC record not found");
+    }
+
+    // VTOC record name is good, now read through the TOC,
+    // stored in the PT keyword; vpdBuffer is now pointing at
+    // the first character of the name 'VTOC', jump to PT
+    // data.
+    vpdBuffer += (lengths::RECORD_NAME);
+    // Skip past KW name, 'PT'
+    vpdBuffer += lengths::KW_NAME;
+    // Note size of PT
+    KwLength ptLen = (*vpdBuffer);
+    // Skip past PT size
+    vpdBuffer += sizeof(KwSize);
+    // vpdBuffer is now pointing to PT data
+    offsets = readPT(vpdBuffer, ptLen);
+
+    return offsets;
+}
+
+OffsetList Impl::readPT(auto vpdBuffer, auto ptLength) const
+{
+    OffsetList offsets{};
+
+    auto end = vpdBuffer + ptLength;
+
+    // Look at each entry in the PT keyword. In the entry,
+    // we care only about the record offset information.
+    while(vpdBuffer < end)
+    {
+        // Skip record name
+        vpdBuffer += lengths::RECORD_NAME;
+        // Skip record type
+        vpdBuffer += sizeof(RecordType);
+
+        // Get record offset
+        RecordOffset recOft = *vpdBuffer;
+        RecordOffset highByte = *(vpdBuffer + 1);
+        if(highByte)
+        {
+            // Offset is 2 bytes long
+            recOft = (highByte << 8) |
+                     recOft;
+        }
+        offsets.push_back(recOft);
+
+        // Jump record size
+        vpdBuffer += sizeof(RecordSize);
+        // Skip record length
+        vpdBuffer += sizeof(RecordLength);
+        // Skip ECC offset
+        vpdBuffer += sizeof(ECCOffset);
+        // Skip ECC length
+        vpdBuffer += sizeof(ECCLength);
+    }
+
+    return offsets;
+}
+
+} // namespace parser
+} // namespace vpd
+} // namespace openpower
