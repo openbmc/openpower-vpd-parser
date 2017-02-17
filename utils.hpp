f9718d00ee64cf6112525bb5c5235c849b05afec
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-02-16 23:48:18 -0600

    refactor: move types and utils in their own files
    Change-Id: I477dd69f0c6c648b63171dc2db264d3e70dafaf4
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

---
+++ utils.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "types.hpp"
+
+namespace openpower
+{
+namespace vpd
+{
+
+namespace inventory
+{
+
+/** @brief Get inventory-manager's d-bus service
+ */
+auto getPIMService();
+
+/** @brief Call inventory-manager to add objects
+ *
+ *  @param [in] objects - Map of inventory object paths
+ */
+void callPIM(ObjectMap&& objects);
+
+} // namespace inventory
+
+} //namespace vpd
+} //namespace openpower
