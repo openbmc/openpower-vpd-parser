5e03f04fc7d74cbc8d79e96fcf995cf32db7a39a
Alpana Kumari <alpankum@in.ibm.com>
2019-09-10 23:53:58 -0500

    IPZ VPD Parser support
    This commit enables the app ipz-read-vpd to read the IPZ format vpd.
    To build this app , compilation need a flag "--enable_ipz_parser"
    
    Argument required to run this application-
    ipz-read-vpd --file vpd_file
    
    Test:
    Tested to display the content it read from the vpd file
    
    ./ipz-read-vpd   --file  800_vpd
    VHDR
    VTOC
    VINI
            DR : 53595354454d204241434b504c414e45
            CE : 31
            VZ : 3031
            FN : 3030564b343937
            PN : 3030564b373138
            SN : 594c33334247374342303135
            CC : 36423736
            PR : 200000000
            HE : 30303031
            CT : 40f621
            HW : 01
            B3 : 000000
            B4 : 0
            B7 : 000000000000
            SE : 4b494330313435
            TM : 2020202020202020
            FC : 373844362d303031
            RG : 0000
            RB : 20202020
    VCEN
            DR : 434543202020
            SE : 4b494330313435
            TM : 2020202020202020
            FC : 373844362d303031
            RG : 0000
            RB : 20202020
    VMSC
            IN :
    
    Change-Id: Ic9305f25625adced7f64123589dd083e2679afbb
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

---
+++ ipz_app.cpp
@@ -0,0 +1,47 @@
+#include "defines.hpp"
+#include "CLI/CLI.hpp"
+#include "parser.hpp"
+#include "write.hpp"
+
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+int main(int argc, char** argv)
+{
+    int rc = 0;
+
+    try
+    {
+        using namespace CLI;
+        using namespace openpower::vpd;
+
+        // Get the input binary file using CLI
+        CLI::App app{"ipz-read-vpd"};
+        std::string file{};
+
+        app.add_option("-f, --file", file)                  
+              ->required()                      
+              ->check(CLI::ExistingFile);
+
+        CLI11_PARSE(app, argc, argv);
+
+        std::ifstream vpdFile(file, std::ios::binary);
+        Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
+                   std::istreambuf_iterator<char>());
+
+        // Use ipz vpd Parser
+        auto vpdStore = parse(std::move(vpd));
+
+        //Write it to D-bus
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << "\n";
+    }
+
+    return rc;
+}
