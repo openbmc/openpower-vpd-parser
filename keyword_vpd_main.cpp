9ff47960a3eb505c19b046cd39d22d9e066a66c3
PriyangaRamasamy <priyanga24@in.ibm.com>
2019-09-16 11:03:51 +0530

    Keyword Vpd Parser support
    This commit parses the VPD into kw-val pairs and also has the
    checks for checksum, out of bounds and minimum size of the vpd.
    
    The flag which enables the keyword vpd parser support is
    "--enable-kw-vpd-parser".
    The flag to debug the keyword vpd parser is
    "--enable-debug-kw-vpd".
    
    Steps to build and execute:
     ./bootstrap.sh
     ./configure ${CONFIGURE_FLAGS} --prefix=/usr --enable-kw-vpd-parser --enable-debug-kw-vpd
     make
    
    Test:
    Tested on a witherspoon system
    to display the kw-val pairs and checksum.
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I1976aae908766af0976e8bcfb92259ba23869d86

---
+++ keyword_vpd_main.cpp
@@ -0,0 +1,56 @@
+#include "CLI11.hpp"
+#include "keyword_vpd_parser.hpp"
+
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace CLI;
+
+int main(int argc, char** argv)
+{
+    using namespace vpd::keyword::parser;
+
+    // Get the input binary file using CLI
+    App app{"Keyword VPD Parser"};
+
+    string fileName{};
+
+    // The keyword vpd file is taken from the command line using -f/--file
+    // flags.
+    app.add_option("-f, --file", fileName)->required()->check(ExistingFile);
+
+    CLI11_PARSE(app, argc, argv);
+
+    // Open the file in binary mode
+    ifstream keywordVpdFile(fileName, ios::binary);
+
+    // Read the content of the binary file into a vector
+    Binary keywordVpdVector((istreambuf_iterator<char>(keywordVpdFile)),
+                            istreambuf_iterator<char>());
+
+    // Creating Parser Object
+    KeywordVpdParser parserObj(move(keywordVpdVector));
+
+    try
+    {
+        KeywordVpdMap kwValMap = parserObj.parseKwVpd();
+
+#ifdef DEBUG_KW_VPD
+        cerr << '\n' << " KW " << '\t' << "  VALUE " << '\n';
+        for (auto it = kwValMap.begin(); it != kwValMap.end(); ++it)
+        {
+            cerr << '\n' << " " << it->first << '\t';
+            copy((it->second).begin(), (it->second).end(),
+                 ostream_iterator<int>(cout << hex, " "));
+        }
+#endif
+    }
+    catch (exception& e)
+    {
+        cerr << "Run time exception from keyword VPD parser: " << e.what()
+             << endl;
+    }
+}
