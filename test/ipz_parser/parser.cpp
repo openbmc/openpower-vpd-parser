27158cfbf5dbd5d1d2daa576111ac786dcb8c2a6
Alpana Kumari <alpankum@in.ibm.com>
2019-10-01 06:31:36 -0500

    IPZ Parser Test cases
    Currently only one GoodCase is supported.
    
    root@witherspoon:/tmp# ./ipz_parser_test
    Running main() from /usr/src/debug/googletest/1.8.1-r0/git/googletest/src/gtest_main.cc
    [==========] Running 1 test from 1 test case.
    [----------] Global test environment set-up.
    [----------] 1 test from runIpzApp
    [ RUN      ] runIpzApp.vpdGoodPath
    [       OK ] runIpzApp.vpdGoodPath (12 ms)
    [----------] 1 test from runIpzApp (14 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test case ran. (20 ms total)
    [  PASSED  ] 1 test.
    
    Change-Id: I68bf8d4e255403be2133037d5f3ff688a7466bd8
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

---
+++ test/ipz_parser/parser.cpp
@@ -0,0 +1,62 @@
+#include <cassert>
+#include <defines.hpp>
+#include <filesystem>
+#include <fstream>
+#include <impl.hpp>
+#include <iterator>
+#include <parser.hpp>
+#include <store.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace openpower::vpd;
+using namespace std;
+namespace fs = std::filesystem;
+
+TEST(IpzVpdParserApp, vpdGoodPath)
+{
+    // TODO 1: Hardcoded local file, create a binary file ?
+    std::cout << "Current path is " << fs::current_path() << '\n';
+
+    for (auto& p : fs::directory_iterator(fs::current_path()))
+        std::cout << p.path() << '\n';
+
+    std::ifstream vpdFile("./4900.dat",
+                          std::ifstream::in | std::ifstream::binary);
+    if (vpdFile.is_open())
+    {
+        cout << "Failed to open \n";
+    }
+    else
+    {
+        cout << "Opened file \n";
+    }
+    Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
+               std::istreambuf_iterator<char>());
+
+    cout << "size:" << vpd.size() << "\n";
+    cout << "Reached before p \n";
+    // call app for this vpd
+    parser::Impl p(std::move(vpd));
+    cout << "Reached before run \n";
+    Store vpdStore = p.run();
+
+    static const std::string record = "VINI";
+    static const std::string keyword = "DR";
+
+    // TODO 2: Move this as an utility to store.hpp
+    std::string dataFound;
+    Parsed st_bin = vpdStore.get();
+
+    auto kw = st_bin.find(record);
+    if (st_bin.end() != kw)
+    {
+        auto value = (kw->second).find(keyword);
+        if ((kw->second).end() != value)
+        {
+            dataFound = value->second;
+        }
+    }
+
+    ASSERT_EQ(dataFound, "APSS & TPM  CARD");
+}
