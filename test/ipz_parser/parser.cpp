c95e6e18ba9f27f34f96b1e57633410281a87a7d
Alpana Kumari <alpankum@in.ibm.com>
2019-10-01 06:31:36 -0500

    IPZ Parser Test cases
    Currently only one GoodCase is supported.
    
    root@witherspoon:/tmp# ./ipz_parser_test
    Running main() from /usr/src/debug/googletest/1.8.1-r0/git/googletest/src/gtest_main.cc
    [==========] Running 1 test from 1 test case.
    [----------] Global test environment set-up.
    [----------] 1 test from runIpzApp
    [ RUN      ] runIpzApp.vpdGoodPath
    [       OK ] runIpzApp.vpdGoodPath (12 ms)
    [----------] 1 test from runIpzApp (14 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test case ran. (20 ms total)
    [  PASSED  ] 1 test.
    
    Change-Id: I68bf8d4e255403be2133037d5f3ff688a7466bd8
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

---
+++ test/ipz_parser/parser.cpp
@@ -0,0 +1,42 @@
+#include <cassert>
+#include <defines.hpp>
+#include <fstream>
+#include <iterator>
+#include <parser.hpp>
+#include <store.hpp>
+#include <gtest/gtest.h>
+#include <impl.hpp>
+
+using namespace openpower::vpd;
+
+TEST(IpzVpdParserApp, vpdGoodPath)
+{
+//TODO 1: Hardcoded local file, create a binary file ?
+    std::ifstream vpdFile("4900_vpd", std::ios::binary);
+    Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
+               std::istreambuf_iterator<char>());
+
+    //call app for this vpd
+    parser::Impl p(std::move(vpd));
+    Store vpdStore = p.run();
+
+    static const std::string record = "VINI";
+    static const std::string keyword = "DR";
+
+//TODO 2: Move this as an utility to store.hpp
+    std::string dataFound ;
+    Parsed st_bin =  vpdStore.get();
+
+    auto kw = st_bin.find(record);
+    if (st_bin.end() != kw)
+    {
+        auto value = (kw->second).find(keyword);
+        if ((kw->second).end() != value)
+        {
+            dataFound = value->second;
+        }
+    }
+
+    ASSERT_EQ(dataFound, "APSS & TPM  CARD");
+}
+
