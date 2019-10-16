11df58140cde5baef386d9c4846f89f756ec4439
Alpana Kumari <alpankum@in.ibm.com>
2019-10-16 09:12:19 -0500

    IPZ Parser: Update VPD to Inventory
    This commit will populate the d-bus with the parsed VPD data
    collected from vpd file
    
    Test:
    pending due to interface and Property should be defined,
    which is needed for NOTIFY method call.
    
    Change-Id: I40545c7b4f252fbbc964d0c56789735a38a40b4f
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

--- ipz_app.cpp
+++ ipz_app.cpp
@@ -1,6 +1,7 @@
 #include "CLI/CLI.hpp"
 #include "defines.hpp"
 #include "parser.hpp"
+#include "utils.hpp"
 #include "write.hpp"
 
 #include <exception>
@@ -9,6 +10,41 @@
 #include <iterator>
 
 using namespace std;
+using namespace openpower::vpd;
+
+void populateDbus(Store& vpdStore)
+{
+    string objectPath{};
+    inventory::InterfaceMap interfaces;
+    inventory::ObjectMap objects;
+    sdbusplus::message::object_path object(objectPath);
+
+    Parsed vpdData = vpdStore.get();
+    constexpr auto preIntrStr = "com.ibm.interface.";
+
+    // loop through the record and append interface to the record
+    for (const auto& rec_kw : vpdData)
+    {
+        inventory::PropertyMap prop;
+        for (auto kw_val : rec_kw.second)
+        {
+            std::vector<uint8_t> vec(kw_val.second.begin(),
+                                     kw_val.second.end());
+            std::string kw = kw_val.first;
+            if (kw[0] == '#')
+            {
+                kw = std::string("PD_") + kw[1];
+            }
+            prop.emplace(move(kw), move(vec));
+        }
+        interfaces.emplace(preIntrStr + rec_kw.first, move(prop));
+    }
+
+    objects.emplace(move(object), move(interfaces));
+
+    // Notify method call
+    inventory::callPIM(move(objects));
+}
 
 int main(int argc, char** argv)
 {
@@ -19,16 +55,13 @@ int main(int argc, char** argv)
         using namespace CLI;
         using namespace openpower::vpd;
 
-        App app{"ipz-read-vpd - App to read IPZ format VPD, parse it and store in DBUS"};
+        App app{"ipz-read-vpd - App to read IPZ format VPD, parse it and store "
+                "in DBUS"};
         string file{};
-        string objectPath{};
 
         app.add_option("-f, --file", file, "File containing VPD in IPZ format")
             ->required()
             ->check(ExistingFile);
-
-        app.add_option("--object", objectPath)
-            ->required();
 
         CLI11_PARSE(app, argc, argv);
 
@@ -39,7 +72,8 @@ int main(int argc, char** argv)
         // Use ipz vpd Parser
         auto vpdStore = parse(move(vpd));
 
-        // Write it to D-bus
+        // Write it to the D-bus, Create an object Map
+        populateDbus(vpdStore);
     }
     catch (exception& e)
     {
