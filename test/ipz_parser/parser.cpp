a750868fa0d47c10789ba05015b36c9d2c4c46ab
Alpana Kumari <alpankum@in.ibm.com>
2020-01-02 00:35:24 -0600

    IPZ VPD Parser TestCases
    Replace using vpd data vector by a vpd File
    
    Tested:
    root@rainier:/tmp# ./ipz_parser_test
    [==========] Running 4 tests from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 4 tests from IpzVpdParserApp
    [ RUN      ] IpzVpdParserApp.vpdGoodPath
    [       OK ] IpzVpdParserApp.vpdGoodPath (1 ms)
    [ RUN      ] IpzVpdParserApp.vpdBadPathEmptyVPD
    [       OK ] IpzVpdParserApp.vpdBadPathEmptyVPD (0 ms)
    [ RUN      ] IpzVpdParserApp.vpdBadPathMissingHeader
    [       OK ] IpzVpdParserApp.vpdBadPathMissingHeader (0 ms)
    [ RUN      ] IpzVpdParserApp.vpdBadPathMissingVTOC
    [       OK ] IpzVpdParserApp.vpdBadPathMissingVTOC (0 ms)
    [----------] 4 tests from IpzVpdParserApp (1 ms total)
    
    [----------] Global test environment tear-down
    [==========] 4 tests from 1 test suite ran. (1 ms total)
    [  PASSED  ] 4 tests.
    root@rainier:/tmp#
    
    Change-Id: I3c7991b0a65643a2eb79c13379948a58a329ef5a
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

---
+++ test/ipz_parser/parser.cpp
@@ -0,0 +1,92 @@
+#include <defines.hpp>
+#include <fstream>
+#include <impl.hpp>
+#include <parser.hpp>
+#include <store.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace openpower::vpd;
+
+TEST(IpzVpdParserApp, vpdGoodPath)
+{
+    // Get the vpd
+    std::ifstream vpdFile("ipz.vpd", std::ios::binary);
+
+    Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
+               std::istreambuf_iterator<char>());
+
+    // call app for this vpd
+    parser::Impl p(std::move(vpd));
+    Store vpdStore = p.run();
+
+    static const std::string record = "VINI";
+    static const std::string keyword = "DR";
+
+    std::string dataFound;
+    Parsed st_bin = vpdStore.getVpdMap();
+
+    auto kw = st_bin.find(record);
+    if (st_bin.end() != kw)
+    {
+        auto value = (kw->second).find(keyword);
+        if ((kw->second).end() != value)
+        {
+            dataFound = value->second;
+        }
+    }
+
+    ASSERT_EQ(dataFound, "APSS & TPM  CARD");
+}
+
+TEST(IpzVpdParserApp, vpdBadPathEmptyVPD)
+{
+    Binary vpd = {};
+
+    // VPD is empty
+    parser::Impl p(std::move(vpd));
+
+    // Expecting a throw here
+    EXPECT_THROW(p.run(), std::runtime_error);
+}
+
+TEST(IpzVpdParserApp, vpdBadPathMissingHeader)
+{
+    // Get the vpd
+    std::ifstream vpdFile("ipz.vpd", std::ios::binary);
+
+    Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
+               std::istreambuf_iterator<char>());
+
+    // corrupt the VHDR
+    vpd[17] = 0x00;
+
+    parser::Impl p(std::move(vpd));
+
+    // Expecting a throw here
+    EXPECT_THROW(p.run(), std::runtime_error);
+}
+
+TEST(IpzVpdParserApp, vpdBadPathMissingVTOC)
+{
+    // Get the vpd
+    std::ifstream vpdFile("./ipz.vpd", std::ios::binary);
+
+    Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
+               std::istreambuf_iterator<char>());
+
+    // corrupt the VTOC
+    vpd[61] = 0x00;
+
+    parser::Impl p(std::move(vpd));
+
+    // Expecting a throw here
+    EXPECT_THROW(p.run(), std::runtime_error);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
