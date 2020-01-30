b255c94aa3a34a23e60975466f1191398d890c2a
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-01-30 02:55:21 -0600

    JSON PARSER FOR WRITE KEYWORD VPD APP
    As per the requirement application calling
    write vpd keyword provides the inventory
    path for which VPD file need to be updated.
    This is the implementation to parse JSON in
    order to get the mapping of Inventory path to EEPROM VPD.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: Ie233ad533165e6cf8cd75031d880d3d1a94ac4ba

--- kwd-editor/VPDKeywordEditor.cpp
+++ kwd-editor/VPDKeywordEditor.cpp
@@ -1,12 +1,15 @@
-#include <chrono>
-#include <iostream>
+#include "config.h"
+
 #include "VPDKeywordEditor.hpp"
+
 #include "parser.hpp"
-#include <fstream>
+
+#include <chrono>
 #include <exception>
+#include <fstream>
+#include <iostream>
 #include <nlohmann/json.hpp>
 #include <vector>
-#include "config.h"
 
 using namespace std::literals::chrono_literals;
 namespace openpower
@@ -17,36 +20,76 @@ namespace keyword
 {
 namespace editor
 {
-	VPDKeywordEditor::VPDKeywordEditor(sdbusplus::bus::bus&& bus, const char* busName, const char* objPath, const char* iFace)
-	:ServerObject<kwdEditorIface>(bus, objPath), _bus(std::move(bus)), _manager(_bus, objPath)
-	{
-		_bus.request_name(busName);
-	}
+VPDKeywordEditor::VPDKeywordEditor(sdbusplus::bus::bus&& bus,
+                                   const char* busName, const char* objPath,
+                                   const char* iFace) :
+    ServerObject<kwdEditorIface>(bus, objPath),
+    _bus(std::move(bus)), _manager(_bus, objPath)
+{
+    _bus.request_name(busName);
+}
 
-	void VPDKeywordEditor::run()
-	{
-		while (true)
-		{
-			try
-			{
-				_bus.process_discard();
-				//wait for event
-				_bus.wait();
-			}
-			catch (const std::exception& e)
-			{
-				std::cerr << e.what() << std::endl;
-			}
-		}
-	}
+void VPDKeywordEditor::run()
+{
+    while (true)
+    {
+        try
+        {
+            _bus.process_discard();
+            // wait for event
+            _bus.wait();
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+}
 
+inventory::vpdPath VPDKeywordEditor::processJSON(std::string inventoryPath)
+{
+    std::ifstream jsonFile(INVENTORY_JSON, std::ios::binary);
 
-	void VPDKeywordEditor::writeKeyword( std::string inventoryPath, std::string recordName, std::string keyword,  std::vector<uint8_t> value)
-	{
-		//implements the interface to write keyword VPD data
-	}
+    if (!jsonFile)
+    {
+        throw std::runtime_error("json file not found");
+    }
 
-} //namespace editor
-} //namespace keyword
-} //namespace vpd
-} //namespace openpower
+    nlohmann::json jfile = nlohmann::json::parse(jsonFile);
+    if (jfile.find("frus") == jfile.end())
+    {
+        throw std::runtime_error("frus group not found in json");
+    }
+
+    nlohmann::json groupFRUS = (jfile.find("frus")).value();
+    nlohmann::json::iterator itemFRUS;
+    for (itemFRUS = groupFRUS.begin(); itemFRUS != groupFRUS.end(); ++itemFRUS)
+    {
+        std::vector<nlohmann::json> groupEEPROM = itemFRUS.value();
+        std::vector<nlohmann::json>::iterator itemEEPROM;
+
+        for (itemEEPROM = groupEEPROM.begin(); itemEEPROM != groupEEPROM.end();
+             ++itemEEPROM)
+        {
+            nlohmann::json individualFRU = *itemEEPROM;
+            if (individualFRU.find("inventoryPath").value() == inventoryPath)
+            {
+                return itemFRUS.key();
+            }
+        }
+    }
+    throw std::runtime_error("Inventory path is not found");
+}
+
+void VPDKeywordEditor::writeKeyword(std::string inventoryPath,
+                                    std::string recordName, std::string keyword,
+                                    std::vector<uint8_t> value)
+{
+    // process the json to get path to VPD file
+    inventory::vpdPath vpdFilePath = processJSON(inventoryPath);
+}
+
+} // namespace editor
+} // namespace keyword
+} // namespace vpd
+} // namespace openpower
--- kwd-editor/VPD_KeywordEditor_main.cpp
+++ kwd-editor/VPD_KeywordEditor_main.cpp
@@ -1,21 +1,24 @@
-#include <cstdlib>
-#include <iostream>
-#include <exception>
-#include <sdbusplus/bus.hpp>
 #include "config.h"
+
 #include "VPDKeywordEditor.hpp"
 
-int main(int argc, char *argv[])
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <sdbusplus/bus.hpp>
+
+int main(int argc, char* argv[])
 {
-	try
-	{
-		openpower::vpd::keyword::editor::VPDKeywordEditor KeywordEditor(sdbusplus::bus::new_system(), BUSNAME, OBJPATH, IFACE);
-		KeywordEditor.run();
-		exit(EXIT_SUCCESS);
-	}
-	catch (const std::exception& e)
-	{
-		 std::cerr << e.what() << std::endl;
-	}
-	exit(EXIT_FAILURE);
+    try
+    {
+        openpower::vpd::keyword::editor::VPDKeywordEditor KeywordEditor(
+            sdbusplus::bus::new_system(), BUSNAME, OBJPATH, IFACE);
+        KeywordEditor.run();
+        exit(EXIT_SUCCESS);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    exit(EXIT_FAILURE);
 }
