ce6963c5d0667f10dbdc1e2353d8066db708bb1e
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-01-30 01:39:36 -0600

    WRITE KEYWORD_VPD APP SKELETON
    Implements required DBUS interfaces.
    Implements VPDKeywordEditor Interface.
    Skeleton to the interface to be called
    over DBUS.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I9dca870f09785edf589f8c6468a8a06e4272577c

---
+++ kwd-editor/VPD_KeywordEditor_main.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+#include <sdbusplus/bus.hpp>
+#include "config.h"
+#include "VPDKeywordEditor.hpp"
+
+int main(int argc, char *argv[])
+{
+	try
+	{
+		openpower::vpd::keyword::editor::VPDKeywordEditor KeywordEditor(sdbusplus::bus::new_system(), BUSNAME, OBJPATH, IFACE);
+		KeywordEditor.run();
+		exit(EXIT_SUCCESS);
+	}
+	catch (const std::exception& e)
+	{
+		 std::cerr << e.what() << std::endl;
+	}
+	exit(EXIT_FAILURE);
+}
