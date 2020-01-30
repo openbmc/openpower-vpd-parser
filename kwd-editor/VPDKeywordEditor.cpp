b05f414b62c0c9487a88bfdd50f0c71a2d28e5ca
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-01-30 02:55:21 -0600

    JSON PARSER FOR WRITE KEYWORD VPD APP
    As per the requirement application calling
    write vpd keyword provides the inventory
    path for which VPD file need to be updated.
    This is the implementation to parse JSON in
    order to get the mapping of Inventory path to EEPROM VPD.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: Ie233ad533165e6cf8cd75031d880d3d1a94ac4ba

---
+++ kwd-editor/VPDKeywordEditor.cpp
@@ -0,0 +1,92 @@
+#include "config.h"
+#include "VPDKeywordEditor.hpp"
+#include "parser.hpp"
+#include <chrono>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <vector>
+
+using namespace std::literals::chrono_literals;
+namespace openpower
+{
+namespace vpd
+{
+namespace keyword
+{
+namespace editor
+{
+VPDKeywordEditor::VPDKeywordEditor(sdbusplus::bus::bus&& bus,
+                                   const char* busName, const char* objPath,
+                                   const char* iFace) :
+    ServerObject<kwdEditorIface>(bus, objPath),
+    _bus(std::move(bus)), _manager(_bus, objPath)
+{
+    _bus.request_name(busName);
+}
+
+void VPDKeywordEditor::run()
+{
+    while (true)
+    {
+        try
+        {
+            _bus.process_discard();
+            // wait for event
+            _bus.wait();
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+}
+
+inventory::vpdPath VPDKeywordEditor::processJSON(std::string inventoryPath)
+{
+    std::ifstream jsonFile(INVENTORY_JSON, std::ios::binary);
+
+    if (!jsonFile)
+    {
+        throw std::runtime_error("json file not found");
+    }
+
+    nlohmann::json jfile = nlohmann::json::parse(jsonFile);
+    if (jfile.find("frus") == jfile.end())
+    {
+        throw std::runtime_error("frus group not found in json");
+    }
+
+    nlohmann::json groupFRUS = (jfile.find("frus")).value();
+    nlohmann::json::iterator itemFRUS;
+    for (itemFRUS = groupFRUS.begin(); itemFRUS != groupFRUS.end(); ++itemFRUS)
+    {
+        std::vector<nlohmann::json> groupEEPROM = itemFRUS.value();
+        std::vector<nlohmann::json>::iterator itemEEPROM;
+
+        for (itemEEPROM = groupEEPROM.begin(); itemEEPROM != groupEEPROM.end();
+             ++itemEEPROM)
+        {
+            nlohmann::json individualFRU = *itemEEPROM;
+            if (individualFRU.find("inventoryPath").value() == inventoryPath)
+            {
+                return itemFRUS.key();
+            }
+        }
+    }
+    throw std::runtime_error("Inventory path is not found");
+}
+
+void VPDKeywordEditor::writeKeyword(std::string inventoryPath,
+                                    std::string recordName, std::string keyword,
+                                    std::vector<uint8_t> value)
+{
+    // process the json to get path to VPD file
+    inventory::vpdPath vpdFilePath = processJSON(inventoryPath);
+}
+
+} // namespace editor
+} // namespace keyword
+} // namespace vpd
+} // namespace openpower
---
+++ kwd-editor/VPDKeywordEditor.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "types.hpp"
+#include <sdbusplus/server.hpp>
+#include <xyz/openbmc_project/Inventory/VPD/VPDKeywordEditor/server.hpp>
+
+namespace sdbusplus
+{
+namespace bus
+{
+class bus;
+}
+} // namespace sdbusplus
+
+namespace openpower
+{
+namespace vpd
+{
+namespace keyword
+{
+namespace editor
+{
+
+template <typename T>
+using ServerObject = T;
+
+using kwdEditorIface =
+    sdbusplus::xyz::openbmc_project::Inventory::VPD::server::VPDKeywordEditor;
+
+/** @class VPDKeywordEditor
+ * *  @brief OpenBMC keywod editor implementation.
+ * *
+ * *  A concrete implementation for the
+ * xyz.openbmc_project.Inventory.VPD.server.VPDKeywordEditor
+ * */
+class VPDKeywordEditor : public ServerObject<kwdEditorIface>
+{
+  public:
+    /* Define all of the basic class operations:
+     **     Not allowed:
+     **         - Default constructor to avoid nullptrs.
+     **         - Copy operations due to internal unique_ptr.
+     **         - Move operations due to 'this' being registered as the
+     **           'context' with sdbus.
+     **     Allowed:
+     **         - Destructor.
+     **/
+    VPDKeywordEditor() = delete;
+    VPDKeywordEditor(const VPDKeywordEditor&) = delete;
+    VPDKeywordEditor& operator=(const VPDKeywordEditor&) = delete;
+    VPDKeywordEditor(VPDKeywordEditor&&) = default;
+    ~VPDKeywordEditor() = default;
+
+    /** @brief Constructor to put object onto bus at a dbus path.
+    **  @param[in] bus - Bus connection.
+    **  @param[in] busName - Name to be requeted on Bus
+    **  @param[in] objPath - Path to attach at.
+    **  @param[in] iFace - interface to implement
+    **/
+    VPDKeywordEditor(sdbusplus::bus::bus&& bus, const char* busName,
+                     const char* objPath, const char* iFace);
+
+    /** @brief Implementation for WriteKeyword
+    **  Api to update the keyword value for a given inventory.
+    **
+    **  @param[in] path - Object path of the inventory
+    **  @param[in] recordName - name of the record for which the keyword value
+    *has to be modified
+    **  @param[in] keyword - keyword whose value needs to be updated
+    **  @param[in] value - value that needs to be updated
+    **/
+    void writeKeyword(std::string path, std::string recordName,
+                      std::string keyword, std::vector<uint8_t> value);
+
+    /** @brief Start processing DBus messages. */
+    void run();
+
+  private:
+    /** @brief process the given JSON file
+    **
+    **  @param[in] inventoryPath - object path of the inventory
+    **
+    **  @return[out] inventory::vpdPath - path to the VPD file
+    **/
+    inventory::vpdPath processJSON(std::string inventoryPath);
+
+    /** @brief Persistent sdbusplus DBus bus connection. */
+    sdbusplus::bus::bus _bus;
+
+    /** @brief sdbusplus org.freedesktop.DBus.ObjectManager reference. */
+    sdbusplus::server::manager::manager _manager;
+};
+
+} // namespace editor
+} // namespace keyword
+} // namespace vpd
+} // namespace openpower
