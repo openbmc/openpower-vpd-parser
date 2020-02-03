3f6ded04df08f76e1277c523d84c5a796b93f548
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-02-03 06:10:11 -0600

    EDITOR IMPLEMENTATION FOR WRITE KEYWORD VPD APP
    -Implementation of editor class which is
    responsible for parsing the VPD file to
    check for the required record and keyword
    and edit the Keyword data.
    - Changes in parser class to call impl
    methods to validate header and TOC in a
    given VPD file.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: Iee0256b958cfab2e3b58c697b711d1d4c771d12b

--- parser.cpp
+++ parser.cpp
@@ -14,5 +14,20 @@ Store parse(Binary&& vpd)
     return s;
 }
 
+namespace keyword
+{
+namespace editor
+{
+
+std::size_t processHeaderAndTOC(Binary& vpd, Binary::const_iterator& iterator)
+{
+    openpower::vpd::parser::Impl p(std::move(vpd));
+    std::size_t ptLength = p.processVPD(iterator);
+    return ptLength;
+}
+
+} // namespace editor
+} // namespace keyword
+
 } // namespace vpd
 } // namespace openpower
