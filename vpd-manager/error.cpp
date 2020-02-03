77a8d434f578ed13ea73e1588e8f3127a65f6cd6
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-02-03 09:58:56 -0600

    VPD-Manager app skeleton
    This commit implements the outline of VPD Manager
    application.
    Manager app provides multiple functionality over DBUS
    to manage VPD data in the system.
    
    This commit does not  implements the functionality, it
    just implement the prototype of functionality that will
    be provided by the app.
    
    To build the app:
    meson build
    ninja -C build
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: Iefc86468d0b7179c10764d35399189150773eada

---
+++ vpd-manager/error.cpp
@@ -0,0 +1,30 @@
+#include <com/ibm/vpd/error.hpp>
+
+namespace sdbusplus
+{
+namespace com
+{
+namespace ibm
+{
+namespace vpd
+{
+namespace Error
+{
+const char* NotFound::name() const noexcept
+{
+    return errName;
+}
+const char* NotFound::description() const noexcept
+{
+    return errDesc;
+}
+const char* NotFound::what() const noexcept
+{
+    return errWhat;
+}
+
+} // namespace Error
+} // namespace vpd
+} // namespace ibm
+} // namespace com
+} // namespace sdbusplus
