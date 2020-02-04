0a785166e8549c990ee8a6dabe7691cb88f06c64
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-02-04 12:50:40 -0600

    Keyword editor implementation for IPZ_VPD
    -Implementation of editor class which is
    responsible for parsing the VPD file to
    check for the required record and keyword
    and edit the Keyword data in case of IPZ VPD.
    - Updating ECC after kwd data update
    - Changes in parser class to call impl
    methods to validate header and TOC in a
    given VPD file.
    
    Steps to build:
    meson <build_directory>
    ninja -C <directory_path>
    
    -Tested with sample json and sample vpd file
    sample command to test
    busctl call xyz.openbmc_project.Inventory.VPD.VPDKeywordEditor
    /xyz/openbmc_project/Inventory/VPD
    xyz.openbmc_project.Inventory.VPD.VPDKeywordEditor WriteKeyword sssay
    <"inventory_path"> <"Record_Name"> <"Keyword_Name">
    <no. of bytes to update> <array_of_bytes>
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I569f03e329b0f62937c6e143a344d8e4ff02db6f

--- parser.cpp
+++ parser.cpp
@@ -14,5 +14,27 @@ Store parse(Binary&& vpd)
     return s;
 }
 
+namespace keyword
+{
+namespace editor
+{
+
+std::size_t processHeaderAndTOC(Binary&& vpd, uint16_t& ptOffset)
+{
+    openpower::vpd::parser::Impl p(std::move(vpd));
+    std::size_t ptLength = p.processVPD(ptOffset);
+    return ptLength;
+}
+
+Binary updateECC(Binary&& vpd, Binary::const_iterator iterator)
+{
+    openpower::vpd::parser::Impl p(std::move(vpd));
+    // update the ECC for the record
+    return p.updateRecordECC(iterator);
+}
+
+} // namespace editor
+} // namespace keyword
+
 } // namespace vpd
 } // namespace openpower
