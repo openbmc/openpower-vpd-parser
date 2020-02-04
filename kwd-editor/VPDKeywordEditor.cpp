1cc5ac02b2da65eab9c5fde511a9f2040acd1fb6
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-02-04 12:50:40 -0600

    Keyword editor implementation for IPZ_VPD
    -Implementation of editor class which is
    responsible for parsing the VPD file to
    check for the required record and keyword
    and edit the Keyword data in case of IPZ VPD.
    - Updating ECC after kwd data update
    - Changes in parser class to call impl
    methods to validate header and TOC in a
    given VPD file.
    
    Steps to build:
    meson <build_directory>
    ninja -C <directory_path>
    
    -Tested with sample json and sample vpd file
    sample command to test
    busctl call xyz.openbmc_project.Inventory.VPD.VPDKeywordEditor
    /xyz/openbmc_project/Inventory/VPD
    xyz.openbmc_project.Inventory.VPD.VPDKeywordEditor WriteKeyword sssay
    <"inventory_path"> <"Record_Name"> <"Keyword_Name">
    <no. of bytes to update> <array_of_bytes>
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I569f03e329b0f62937c6e143a344d8e4ff02db6f

--- kwd-editor/VPDKeywordEditor.cpp
+++ kwd-editor/VPDKeywordEditor.cpp
@@ -20,6 +20,17 @@ namespace keyword
 {
 namespace editor
 {
+
+constexpr int IPZ_DATA_START = 11;
+constexpr uint8_t KW_VAL_PAIR_START_TAG = 0x84;
+constexpr uint8_t RECORD_END_TAG = 0x78;
+
+enum length
+{
+    VHDR_HEADER_LENGTH = 44,
+    VHDR_ECC_LENGTH = 11,
+};
+
 VPDKeywordEditor::VPDKeywordEditor(sdbusplus::bus::bus&& bus,
                                    const char* busName, const char* objPath,
                                    const char* iFace) :
@@ -78,12 +89,68 @@ void VPDKeywordEditor::processJSON()
 
 void VPDKeywordEditor::writeKeyword(const inventory::Path inventoryPath,
                                     const std::string recordName,
-                                    const std::string keyword,
-                                    std::vector<uint8_t> value)
+                                    const std::string keyword, Binary value)
 {
-    // implement write functionality here
-}
+    try
+    {
 
+        if (frus.find(inventoryPath) == frus.end())
+        {
+            throw std::runtime_error("Inventory path not found");
+        }
+
+        inventory::Path vpdFilePath = frus.find(inventoryPath)->second;
+        std::ifstream vpdStream(vpdFilePath, std::ios::binary);
+        if (!vpdStream)
+        {
+            throw std::runtime_error("file not found");
+        }
+
+        Binary vpdHeader(length::VHDR_HEADER_LENGTH + length::VHDR_ECC_LENGTH);
+        vpdStream.read(reinterpret_cast<char*>(vpdHeader.data()),
+                       vpdHeader.capacity());
+
+        Binary vpdTOC;
+
+        Byte data;
+        while (vpdStream.get(*(reinterpret_cast<char*>(&data))))
+        {
+            vpdTOC.push_back(data);
+            if (data == RECORD_END_TAG)
+            {
+                // record has been fully read
+                break;
+            }
+        }
+
+        vpdHeader.insert(vpdHeader.end(), vpdTOC.begin(), vpdTOC.end());
+
+        vpdStream.seekg(IPZ_DATA_START, std::ios::beg);
+        vpdStream.get(*(reinterpret_cast<char*>(&data)));
+
+        // imples it is IPZ VPD
+        if (data == KW_VAL_PAIR_START_TAG)
+        {
+            RecordOffset ptOffset;
+
+            // parse vpd to validate Header and check TOC for PT record
+            std::size_t ptLength =
+                openpower::vpd::keyword::editor::processHeaderAndTOC(
+                    std::move(vpdHeader), ptOffset);
+
+            // instantiate editor class to update the data
+            Editor edit(vpdFilePath, recordName, keyword);
+            edit.updateKeyword(ptOffset, ptLength, value);
+
+            return;
+        }
+        throw std::runtime_error("Invalid VPD file type");
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+}
 } // namespace editor
 } // namespace keyword
 } // namespace vpd
