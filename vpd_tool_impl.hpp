dc844f5ca454e55cbcb631a41c39f1c1084f3107
PriyangaRamasamy <priyanga24@in.ibm.com>
2020-02-20 20:48:25 +0530

    Implement VPD tool-dumpInventory & dumpObject options
    VPD tool has four options.
    
    a) Dump Complete Inventory - no additional arguments are needed.
    b) Dump Specific Object - by providing the object name.
    c) Read the keyword - by providing the object name, record name and keyword to be read.
    d) Write/Update the keyword - by providing the object name,  record name, keyword and the value to be updated.
    
    {value - in ascii or hex & all the other arguments in string}
    
    "--help" option provides details on how to use the above mentioned options.
    
    This commit has implementation of dump inventory and dump specific object.
    
    Flag to enable VPD tool:
    There is no seperate flag for VPD tool.
    --enable-ibm-parser will generate the vpd-tool binary.
    
    Steps to build and generate the executable using autotools:
    ./bootstrap.sh
    ./configure ${CONFIGURE_FLAGS} --enable-ibm-parser
    make
    
    Steps to build and generate the executable using meson:
    meson -Denabled=ibm-parser build
    ninja -C build
    
    Test:
    Tested on rainier simics.
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I712f7ad4303eefa68f232685b6b0e53646f859f5

---
+++ vpd_tool_impl.hpp
@@ -0,0 +1,46 @@
+#include "config.h"
+
+#include "types.hpp"
+
+#include <nlohmann/json.hpp>
+
+class VpdTool
+{
+  private:
+    const std::string fruPath;
+
+  public:
+    /**
+     * @brief Dump the complete inventory in JSON format
+     *
+     * @param[in] jsObject - Inventory JSON specified in configure file.
+     */
+    void dumpInventory(const nlohmann::basic_json<>& jsObject);
+
+    /**
+     * @brief Dump the given inventory object in JSON format
+     *
+     * @param[in] jsObject - Inventory JSON specified in configure file.
+     */
+    void dumpObject(const nlohmann::basic_json<>& jsObject);
+
+    /**
+     * @brief A Constructor
+     *
+     * Constructor is called during the
+     * object instantiation for dumpInventory option.
+     */
+    VpdTool()
+    {
+    }
+
+    /**
+     * @brief Constructor
+     *
+     * Constructor is called during the
+     * object instantiation for dumpObject option.
+     */
+    VpdTool(const std::string&& fru) : fruPath(std::move(fru))
+    {
+    }
+};
