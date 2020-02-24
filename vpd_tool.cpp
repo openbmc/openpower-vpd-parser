508056ece1ae538392e78df0583fd47c813c85e0
PriyangaRamasamy <priyanga24@in.ibm.com>
2020-02-24 14:54:45 +0530

    Implement VPD tool-Read Keyword option
    One of the options the VPD tool provides is
    to Read the value of the keyword. This commit has its implementation.
    
    The user should provide a valid object path, valid record name
    and valid keyword inorder to get the value of the keyword.
    
    Test:
    Tested on simics.
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I244b9fe276feefa27e4c99063a9e9aa01aeb2f12

--- vpd_tool.cpp
+++ vpd_tool.cpp
@@ -1,6 +1,6 @@
 #include "vpd_tool_impl.hpp"
 
-#include <CLI11/CLI.hpp>
+#include <CLI/CLI.hpp>
 #include <fstream>
 #include <iostream>
 
@@ -14,9 +14,14 @@ int main(int argc, char** argv)
             "update the keywords"};
 
     string objectPath;
+    string recordName;
+    string keyword;
 
     auto object =
         app.add_option("--object, -O", objectPath, "Enter the Object Path");
+    auto record =
+        app.add_option("--record, -R", recordName, "Enter the Record Name");
+    auto kw = app.add_option("--keyword, -K", keyword, "Enter the Keyword");
 
     auto dumpObjFlag = app.add_flag("--dumpObject, -o",
                                     "Dump the given object from the inventory")
@@ -24,6 +29,12 @@ int main(int argc, char** argv)
 
     auto dumpInvFlag =
         app.add_flag("--dumpInventory, -i", "Dump all the inventory objects");
+
+    auto readFlag =
+        app.add_flag("--readKeyword, -r", "Read the data of the given keyword")
+            ->needs(object)
+            ->needs(record)
+            ->needs(kw);
 
     CLI11_PARSE(app, argc, argv);
 
@@ -42,6 +53,13 @@ int main(int argc, char** argv)
         {
             VpdTool vpdToolObj;
             vpdToolObj.dumpInventory(jsObject);
+        }
+
+        else if (*readFlag)
+        {
+            VpdTool vpdToolObj(move(objectPath), move(recordName),
+                               move(keyword));
+            vpdToolObj.readKeyword();
         }
 
         else
