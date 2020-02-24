a4909a46df1a7ea30c9026f32d60ab55dd47d56b
PriyangaRamasamy <priyanga24@in.ibm.com>
2020-02-24 14:54:45 +0530

    Implement VPD tool-Read Keyword option
    One of the options the VPD tool provides is
    to Read the value of the keyword. This commit has its implementation.
    
    The user should provide a valid object path, valid record name
    and valid keyword inorder to get the value of the keyword.
    
    Test:
    Tested on simics.
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I244b9fe276feefa27e4c99063a9e9aa01aeb2f12

--- vpd_tool_impl.hpp
+++ vpd_tool_impl.hpp
@@ -8,6 +8,8 @@ class VpdTool
 {
   private:
     const std::string fruPath;
+    const std::string recordName;
+    const std::string keyword;
 
   public:
     /**
@@ -25,6 +27,14 @@ class VpdTool
     void dumpObject(const nlohmann::basic_json<>& jsObject);
 
     /**
+     * @brief Read keyword
+     * Read the given object path, record name and keyword
+     * from the inventory and display the value of the keyword
+     * in JSON format.
+     */
+    void readKeyword();
+
+    /**
      * @brief A Constructor
      * Constructor is called during the
      * object instantiation for dumpInventory option.
@@ -39,6 +49,18 @@ class VpdTool
      * object instantiation for dumpObject option.
      */
     VpdTool(const std::string&& fru) : fruPath(std::move(fru))
+    {
+    }
+
+    /**
+     * @brief Constructor
+     * Constructor is called during the
+     * object instantiation for readKeyword option.
+     */
+    VpdTool(const std::string&& fru, const std::string&& recName,
+            const std::string&& kw) :
+        fruPath(std::move(fru)),
+        recordName(std::move(recName)), keyword(std::move(kw))
     {
     }
 };
