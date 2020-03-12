70952d299f7607bc35f5e1cf28912442b94c1c70
PriyangaRamasamy <priyanga24@in.ibm.com>
2020-03-12 14:11:50 +0530

    VPD Tool: Write keyword command line interface
    This commit has the command line interface
    for writeKeyword option in the VPD tool.
    
    --help option will provide the user - the argument format
    for writeKeyword option.
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I6c586a9848497e44bf88eda78694989d0287cbba

--- vpd_tool.cpp
+++ vpd_tool.cpp
@@ -10,18 +10,24 @@ using json = nlohmann::json;
 
 int main(int argc, char** argv)
 {
+    int rc = 0;
     App app{"VPD Command line tool to dump the inventory and to read and "
             "update the keywords"};
 
     string objectPath;
     string recordName;
     string keyword;
+    string val;
 
     auto object =
         app.add_option("--object, -O", objectPath, "Enter the Object Path");
     auto record =
         app.add_option("--record, -R", recordName, "Enter the Record Name");
     auto kw = app.add_option("--keyword, -K", keyword, "Enter the Keyword");
+    auto valOption = app.add_option(
+        "--value, -V", val,
+        "Enter the value. The value to be updated should be either in ascii or "
+        "in hex. ascii eg: 01234; hex eg: 0x30313233");
 
     auto dumpObjFlag =
         app.add_flag("--dumpObject, -o",
@@ -45,6 +51,17 @@ int main(int argc, char** argv)
             ->needs(object)
             ->needs(record)
             ->needs(kw);
+    auto writeFlag =
+        app.add_flag(
+               "--writeKeyword, -w, --updateKeyword, -u",
+               "Update the value. { vpd-tool_Executable --writeKeyword "
+               "--object object_name --record record_name --keyword "
+               "keyword_name --value value_to_be_updated / vpd-tool_Executable "
+               "-w -O object_name -R record_name -K keyword -V value }")
+            ->needs(object)
+            ->needs(record)
+            ->needs(kw)
+            ->needs(valOption);
 
     CLI11_PARSE(app, argc, argv);
 
@@ -72,6 +89,13 @@ int main(int argc, char** argv)
             vpdToolObj.readKeyword();
         }
 
+        else if (*writeFlag)
+        {
+            VpdTool vpdToolObj(move(objectPath), move(recordName),
+                               move(keyword), move(val));
+            rc = vpdToolObj.updateKeyword();
+        }
+
         else
         {
             throw runtime_error("One of the valid options is required. Refer "
@@ -84,5 +108,5 @@ int main(int argc, char** argv)
         cerr << e.what();
     }
 
-    return 0;
+    return rc;
 }
