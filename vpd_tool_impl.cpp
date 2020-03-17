4ff0b4e485c224f74f513ae1c5520f19f3287b7f
PriyangaRamasamy <priyanga24@in.ibm.com>
2020-03-18 02:25:30 +0530

    Added PowerSupply frus:
    The dumpInventory and dumpObject options of VPD tool
    now includes PowerSupply frus.
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I6078d966780898a20a4c3e7c5cc61c3f4f2e6ddd

--- vpd_tool_impl.cpp
+++ vpd_tool_impl.cpp
@@ -13,6 +13,30 @@ using sdbusplus::exception::SdBusError;
 using namespace openpower::vpd;
 
 /**
+ * @brief getPowerSupplyFruPath
+ */
+void getPowerSupplyFruPath(vector<string>& powSuppFrus)
+{
+    auto bus = sdbusplus::bus::new_default();
+    auto properties = bus.new_method_call(
+        OBJECT_MAPPER_SERVICE, OBJECT_MAPPER_OBJECT,
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths");
+    properties.append(INVENTORY_PATH);
+    properties.append(0);
+    properties.append(1);
+    properties.append(POWER_SUPPLY_INTERFACE);
+
+    auto result = bus.call(properties);
+
+    if (result.is_method_error())
+    {
+        throw runtime_error("Get api failed");
+    }
+
+    result.read(powSuppFrus);
+}
+
+/**
  * @brief Debugger
  *
  * Displays the output in JSON.
@@ -61,13 +85,23 @@ auto makeDBusCall(const string& objectName, const string& interface,
  */
 void addFruTypeAndLocation(json exIntf, const string& object, json& kwVal)
 {
-    for (auto intf : exIntf.items())
+    if (object.find("powersupply") != string::npos)
     {
-        if ((intf.key().find("Item") != string::npos) &&
-            (intf.value().is_null()))
+        kwVal.emplace("type", POWER_SUPPLY_TYPE);
+    }
+
+    // add else if statement for fan fru
+
+    else
+    {
+        for (auto intf : exIntf.items())
         {
-            kwVal.emplace("type", intf.key());
-            break;
+            if ((intf.key().find("Item") != string::npos) &&
+                (intf.value().is_null()))
+            {
+                kwVal.emplace("type", intf.key());
+                break;
+            }
         }
     }
 
@@ -285,13 +319,37 @@ void VpdTool::dumpInventory(const nlohmann::basic_json<>& jsObject)
 {
     char flag = 'I';
     json output = parseInvJson(jsObject, flag, "");
+
+    vector<string> powSuppFrus;
+
+    getPowerSupplyFruPath(powSuppFrus);
+
+    for (const auto& fru : powSuppFrus)
+    {
+        output.emplace_back(callVINI(fru, nlohmann::detail::value_t::null));
+    }
+
     debugger(output);
 }
 
 void VpdTool::dumpObject(const nlohmann::basic_json<>& jsObject)
 {
     char flag = 'O';
-    json output = parseInvJson(jsObject, flag, fruPath);
+    json output = json::object({});
+    vector<string> powSuppFrus;
+
+    getPowerSupplyFruPath(powSuppFrus);
+
+    if (find(powSuppFrus.begin(), powSuppFrus.end(), fruPath) !=
+        powSuppFrus.end())
+    {
+        output.emplace_back(callVINI(fruPath, nlohmann::detail::value_t::null));
+    }
+    else
+    {
+        output.emplace_back(parseInvJson(jsObject, flag, fruPath));
+    }
+
     debugger(output);
 }
 
