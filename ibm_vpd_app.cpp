41ae8d7851680f8b34c7984cbacae2136ce29e2b
Alpana Kumari <alpankum@in.ibm.com>
2020-04-02 00:26:07 -0500

    IBM Read VPD support for processor vpd
    This commit adds support to read and parse the processor vpd
    
    Test-
    
    NAME                                          TYPE      SIGNATURE RESULT/VALUE                             FLAGS
    com.ibm.ipzvpd.CP00                           interface -         -                                        -
    .AW                                           property  ay        4 0 0 0 0                                emits-change writable
    xyz.openbmc_project.Inventory.Decorator.Asset interface -         -                                        -
    .BuildDate                                    property  s         ""                                       emits-change writable
    .Manufacturer                                 property  s         ""                                       emits-change writable
    .Model                                        property  s         ""                                       emits-change writable
    .PartNumber                                   property  s         "2345678"                                emits-change writable
    .SerialNumber                                 property  s         "YLAB41010000"                           emits-change writable
    xyz.openbmc_project.Inventory.Item            interface -         -                                        -
    .Present                                      property  b         true                                     emits-change writable
    .PrettyName                                   property  s         "6 WAY  PROC CUOD"                       emits-change writable
    
    Change-Id: I7f1aad29c4413c6db7c2ec2525df427d048658f1
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <iterator>
 #include <nlohmann/json.hpp>
+#include<ctype.h>
 
 using namespace std;
 using namespace openpower::vpd;
@@ -142,6 +143,10 @@ static void populateFruSpecificInterfaces(const T& map,
         if (kw[0] == '#')
         {
             kw = std::string("PD_") + kw[1];
+        }
+        else if(isdigit(kw[0]) )
+        {
+            kw = std::string("N_") + kw;
         }
         prop.emplace(move(kw), move(vec));
     }
@@ -341,11 +346,29 @@ int main(int argc, char** argv)
             return 0;
         }
 
-        // Open the file in binary mode
-        ifstream vpdFile(file, ios::binary);
-        // Read the content of the binary file into a vector
-        Binary vpdVector((istreambuf_iterator<char>(vpdFile)),
-                         istreambuf_iterator<char>());
+        uint32_t offset = 0;
+        // check if offset present?
+        for ( const auto& item : js["frus"][file])
+        {
+            if(item.find("offset") != item.end())
+            {
+                offset = item["offset"];
+            }
+        }
+        char buf[2048];
+        ifstream vpdFile;
+        vpdFile.rdbuf()->pubsetbuf(buf, sizeof(buf));
+        vpdFile.open(file, ios::binary);
+        vpdFile.seekg(offset, std::ios_base::cur);
+
+        // Read 64KB data content of the binary file into a vector
+        Binary tmpVector((istreambuf_iterator<char>(vpdFile)),
+                          istreambuf_iterator<char>());
+
+        vector<unsigned char>::const_iterator first = tmpVector.begin();
+        vector<unsigned char>::const_iterator last = tmpVector.begin() + 65536;
+
+        Binary vpdVector(first, last);
 
         vpdType type = vpdTypeCheck(vpdVector);
 
