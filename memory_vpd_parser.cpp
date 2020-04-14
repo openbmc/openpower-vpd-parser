22559be129a526b44107cc878375867fa61b4de1
Alpana Kumari <alpankum@in.ibm.com>
2020-04-14 07:15:46 -0500

    Memory VPD parser
    This commit supports Implementation part of Memory vpd parser.
    JSON support for DIMMs need to be done, so not tested.
    
    Change-Id: I6bff9c75a335b0267d8f2b9fa4f1905953b24da5
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

---
+++ memory_vpd_parser.cpp
@@ -0,0 +1,60 @@
+#include "memory_vpd_parser.hpp"
+
+#include <iostream>
+#include <numeric>
+#include <string>
+
+namespace openpower
+{
+namespace vpd
+{
+namespace memory
+{
+namespace parser
+{
+using namespace inventory;
+using namespace parser;
+using namespace constants;
+using namespace std;
+
+KeywordVpdMap memoryVpdParser::readKeywords(Binary::const_iterator iterator)
+{
+    KeywordVpdMap map{};
+
+    vector<uint8_t> partNumber(iterator, iterator + PART_NUM_LEN);
+
+    advance(iterator, PART_NUM_LEN);
+    vector<uint8_t> serialNumber(iterator, iterator + SERIAL_NUM_LEN);
+
+    advance(iterator, SERIAL_NUM_LEN);
+    vector<uint8_t> ccin(iterator, iterator + CCIN_LEN);
+
+    map.emplace("PN", move(partNumber));
+    map.emplace("SN", move(serialNumber));
+    map.emplace("CC", move(ccin));
+
+    return map;
+}
+
+KeywordVpdMap memoryVpdParser::parseMemVpd()
+{
+    // check if vpd file is empty
+    if (memVpd.empty())
+    {
+        throw runtime_error("VPD file is empty.");
+    }
+
+    // Read the data and return the map
+    auto iterator = memVpd.cbegin();
+    // point the iterator to DIMM data and skip "11S"
+    advance(iterator, MEMORY_VPD_DATA_START + 3);
+
+    auto vpdDataMap = readKeywords(iterator);
+
+    return vpdDataMap;
+}
+
+} // namespace parser
+} // namespace memory
+} // namespace vpd
+} // namespace openpower
