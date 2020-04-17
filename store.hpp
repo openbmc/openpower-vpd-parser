924429a312b6cb1495f404a3b05993c221fd2167
Joel Stanley <joel@jms.id.au>
2020-04-17 12:19:12 +0930

    store: Provide method to dump out VPD
    This dumps out the contents of all parsed VPDs to stdout.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Change-Id: If5d54c805def4dd33c5ca7aa28bc3ca497a8609b

--- store.hpp
+++ store.hpp
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <iostream>
 
 namespace openpower
 {
@@ -62,6 +63,24 @@ class Store final
         static const std::string record = getRecord<R>();
         static const std::string keyword = record::getKeyword<K>();
         return vpd.count(record) && vpd.at(record).count(keyword);
+    }
+
+    /** @brief Displays all data in the store to stdout
+    */
+    void dump() const
+    {
+        for (auto const & [vpdname, avpd] : vpd ) {
+            std::cout << vpdname << ": " << std::endl;
+
+            for (auto const & [key, val] : avpd ) {
+
+                std::cout << "\t"
+                    << key
+                    << " : "
+                    << val
+                    << std::endl;
+            }
+        }
     }
 
   private:
