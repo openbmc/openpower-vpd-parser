782b5be8ad54da5531b7eaf46598032cc7da4685
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-04-24 08:03:52 -0500

    Get inventory list using expanded location code
    This commit implements api to get inventory list with respect to
    a given location code in expanded format.
    
    In case no inventory is found at the given location or location code
    is not correct, corresponding error is returned to the caller.
    
    Tested on simics.
    This api is under com.ibm.vpd.Manager interface. So vpd-manager app
    needs to be running for this api to work.
    Sample command for simics:
    busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager
    GetFRUsByUnexpandedLocationCode sq <location_code> <node_number>
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: Ifa47d73a64338cbd228b5be1346027c69695612a

--- vpd-manager/reader_impl.cpp
+++ vpd-manager/reader_impl.cpp
@@ -29,9 +29,10 @@ using InvalidArgument =
 using Argument = xyz::openbmc_project::Common::InvalidArgument;
 using LocationNotFound = sdbusplus::com::ibm::VPD::Error::LocationNotFound;
 
-bool ReaderImpl::isValidLocationCode(const std::string& locationCode) const
+bool ReaderImpl::isValidLocationCode(const LocationCode& locationCode) const
 {
     if ((locationCode.length() < UNEXP_LOCATION_CODE_MIN_LENGTH) ||
+        (locationCode[0] != 'U') ||
         ((locationCode.find("fcs", 1, 3) == std::string::npos) &&
          (locationCode.find("mts", 1, 3) == std::string::npos)))
     {
@@ -41,8 +42,8 @@ bool ReaderImpl::isValidLocationCode(const std::string& locationCode) const
     return true;
 }
 
-std::string ReaderImpl::getExpandedLocationCode(
-    const std::string& locationCode, const uint16_t& nodeNumber,
+LocationCode ReaderImpl::getExpandedLocationCode(
+    const LocationCode& locationCode, const Node& nodeNumber,
     const LocationCodeMap& frusLocationCode) const
 {
     if (!isValidLocationCode(locationCode))
@@ -69,10 +70,11 @@ std::string ReaderImpl::getExpandedLocationCode(
 }
 
 ListOfPaths
-    ReaderImpl::getFrusAtLocation(const std::string& locationCode,
-                                  const uint16_t& nodeNumber,
+    ReaderImpl::getFrusAtLocation(const LocationCode& locationCode,
+                                  const Node& nodeNumber,
                                   const LocationCodeMap& frusLocationCode) const
 {
+    // TODO:Implementation related to node number
     if (!isValidLocationCode(locationCode))
     {
         // argument is not valid
@@ -102,6 +104,93 @@ ListOfPaths
     return inventoryPaths;
 }
 
+std::tuple<LocationCode, Node>
+    ReaderImpl::getCollapsedLocationCode(const LocationCode& locationCode) const
+{
+    // Location code should always start with U and fulfil minimum length
+    // criteria.
+    if (locationCode[0] != 'U' ||
+        locationCode.length() < EXP_LOCATIN_CODE_MIN_LENGTH)
+    {
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("LOCATIONCODE"),
+                              Argument::ARGUMENT_VALUE(locationCode.c_str()));
+    }
+
+    std::string fc =
+        readBusProperty(SYSTEM_OBJECT, "com.ibm.ipzvpd.VCEN", "FC");
+
+    // get the first part of expanded location code to check for FC or TM
+    std::string FCorTM = locationCode.substr(1, 4);
+
+    LocationCode unexpandedLocationCode{};
+    Node nodeNummber = INVALID_NODE_NUMBER;
+
+    // check if this value matches the value of FC kwd
+    if (fc.substr(0, 4) == FCorTM) // implies this is Ufcs format location code
+    {
+        // period(.) should be there in expanded location code to seggregate FC,
+        // Node number and SE values.
+        size_t nodeStartPos = locationCode.find('.');
+        if (nodeStartPos == std::string::npos)
+        {
+            elog<InvalidArgument>(
+                Argument::ARGUMENT_NAME("LOCATIONCODE"),
+                Argument::ARGUMENT_VALUE(locationCode.c_str()));
+        }
+
+        // second period(.) should be there to end the node details in non
+        // system location code
+        size_t nodeEndPos = locationCode.find('.', nodeStartPos + 1);
+        if (nodeEndPos == std::string::npos)
+        {
+            elog<InvalidArgument>(
+                Argument::ARGUMENT_NAME("LOCATIONCODE"),
+                Argument::ARGUMENT_VALUE(locationCode.c_str()));
+        }
+
+        // skip 3 for '.ND'
+        nodeNummber = std::stoi(locationCode.substr(
+            nodeStartPos + 3, (nodeEndPos - nodeStartPos - 3)));
+
+        // confirm if there are other details apart FC, Node number and SE in
+        // location code
+        if (locationCode.length() > EXP_LOCATIN_CODE_MIN_LENGTH)
+        {
+            unexpandedLocationCode =
+                locationCode[0] + (std::string) "fcs" +
+                locationCode.substr(nodeEndPos + 1 + SE_KWD_LENGTH,
+                                    std::string::npos);
+        }
+        else
+        {
+            unexpandedLocationCode = "Ufcs";
+        }
+    }
+    else
+    {
+        // read TM kwd value
+        std::string tm =
+            readBusProperty(SYSTEM_OBJECT, "com.ibm.ipzvpd.VSYS", "TM");
+
+        // check if the substr matches to TM kwd
+        if (tm.substr(0, 4) ==
+            FCorTM) // implies this is Umts format of location code
+        {
+            // system location code will not have any other details and node
+            // number
+            unexpandedLocationCode = "Umts";
+        }
+        // it does not belong to either "fcs" or "mts"
+        else
+        {
+            elog<InvalidArgument>(
+                Argument::ARGUMENT_NAME("LOCATIONCODE"),
+                Argument::ARGUMENT_VALUE(locationCode.c_str()));
+        }
+    }
+
+    return std::make_tuple(unexpandedLocationCode, nodeNummber);
+}
 } // namespace reader
 } // namespace manager
 } // namespace vpd
