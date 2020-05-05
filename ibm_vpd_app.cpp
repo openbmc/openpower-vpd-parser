c8804ed71be949cf5dbea8c74908a05e060a17ec
Alpana Kumari <alpankum@in.ibm.com>
2020-05-05 00:22:12 -0500

    Handling FRU and Module in vpd read code
    Change-Id: Ia8143dec680bbfeecb1e63505650428103192157
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -86,10 +86,10 @@ static auto expandLocationCode(const string& unexpanded, const Parsed& vpdMap,
             }
         }
     }
-    catch (std::exception& e)
+    catch (exception& e)
     {
-        std::cerr << "Failed to expand location code with exception: "
-                  << e.what() << "\n";
+        cerr << "Failed to expand location code with exception: " << e.what()
+             << "\n";
     }
     return expanded;
 }
@@ -112,17 +112,17 @@ static void populateFruSpecificInterfaces(const T& map,
 
     for (const auto& kwVal : map)
     {
-        std::vector<uint8_t> vec(kwVal.second.begin(), kwVal.second.end());
+        vector<uint8_t> vec(kwVal.second.begin(), kwVal.second.end());
 
         auto kw = kwVal.first;
 
         if (kw[0] == '#')
         {
-            kw = std::string("PD_") + kw[1];
+            kw = string("PD_") + kw[1];
         }
         else if (isdigit(kw[0]))
         {
-            kw = std::string("N_") + kw;
+            kw = string("N_") + kw;
         }
         prop.emplace(move(kw), move(vec));
     }
@@ -159,7 +159,7 @@ static void populateInterfaces(const nlohmann::json& js,
             }
             else if (itr.value().is_string())
             {
-                if constexpr (std::is_same<T, Parsed>::value)
+                if constexpr (is_same<T, Parsed>::value)
                 {
                     if (busProp == "LocationCode" &&
                         inf == "com.ibm.ipzvpd.Location")
@@ -184,7 +184,7 @@ static void populateInterfaces(const nlohmann::json& js,
                 const string& kw = itr.value().value("keywordName", "");
                 const string& encoding = itr.value().value("encoding", "");
 
-                if constexpr (std::is_same<T, Parsed>::value)
+                if constexpr (is_same<T, Parsed>::value)
                 {
                     if (!rec.empty() && !kw.empty() && vpdMap.count(rec) &&
                         vpdMap.at(rec).count(kw))
@@ -194,7 +194,7 @@ static void populateInterfaces(const nlohmann::json& js,
                         props.emplace(busProp, encoded);
                     }
                 }
-                else if constexpr (std::is_same<T, KeywordVpdMap>::value)
+                else if constexpr (is_same<T, KeywordVpdMap>::value)
                 {
                     if (!kw.empty() && vpdMap.count(kw))
                     {
@@ -208,6 +208,39 @@ static void populateInterfaces(const nlohmann::json& js,
         }
         interfaces.emplace(inf, move(props));
     }
+}
+
+Binary getVpdDataInVector(nlohmann::json& js, const string& filePath)
+{
+    uint32_t offset = 0;
+    // check if offset present?
+    for (const auto& item : js["frus"][filePath])
+    {
+        if (item.find("offset") != item.end())
+        {
+            offset = item["offset"];
+        }
+    }
+    char buf[2048];
+    ifstream vpdFile;
+    vpdFile.rdbuf()->pubsetbuf(buf, sizeof(buf));
+    vpdFile.open(filePath, ios::binary);
+    vpdFile.seekg(offset, ios_base::cur);
+
+    // Read 64KB data content of the binary file into a vector
+    Binary tmpVector((istreambuf_iterator<char>(vpdFile)),
+                     istreambuf_iterator<char>());
+
+    vector<unsigned char>::const_iterator first = tmpVector.begin();
+    vector<unsigned char>::const_iterator last = tmpVector.begin() + NEXT_64_KB;
+
+    if (distance(first, last) < tmpVector.size())
+    {
+        Binary vpdVector(first, last);
+        return vpdVector;
+    }
+
+    return tmpVector;
 }
 
 /**
@@ -242,7 +275,7 @@ inventory::ObjectMap primeInventory(const nlohmann::json& jsObject,
                     inventory::PropertyMap props;
                     if (eI.key() == LOCATION_CODE_INF)
                     {
-                        if constexpr (std::is_same<T, Parsed>::value)
+                        if constexpr (is_same<T, Parsed>::value)
                         {
                             for (auto& lC : eI.value().items())
                             {
@@ -296,7 +329,7 @@ static void populateDbus(const T& vpdMap, nlohmann::json& js,
         // extraInterfaces.
         if (item.value("inherit", true))
         {
-            if constexpr (std::is_same<T, Parsed>::value)
+            if constexpr (is_same<T, Parsed>::value)
             {
                 // Each record in the VPD becomes an interface and all
                 // keyword within the record are properties under that
@@ -307,7 +340,7 @@ static void populateDbus(const T& vpdMap, nlohmann::json& js,
                         record.second, preIntrStr + record.first, interfaces);
                 }
             }
-            else if constexpr (std::is_same<T, KeywordVpdMap>::value)
+            else if constexpr (is_same<T, KeywordVpdMap>::value)
             {
                 populateFruSpecificInterfaces(vpdMap, preIntrStr, interfaces);
             }
@@ -315,7 +348,7 @@ static void populateDbus(const T& vpdMap, nlohmann::json& js,
         else
         {
             // Check if we have been asked to inherit specific record(s)
-            if constexpr (std::is_same<T, Parsed>::value)
+            if constexpr (is_same<T, Parsed>::value)
             {
                 if (item.find("copyRecords") != item.end())
                 {
@@ -333,18 +366,21 @@ static void populateDbus(const T& vpdMap, nlohmann::json& js,
             }
         }
 
-        // Populate interfaces and properties that are common to every FRU
-        // and additional interface that might be defined on a per-FRU
-        // basis.
-        if (item.find("extraInterfaces") != item.end())
+        if (item.value("inheritEI", true))
         {
-            populateInterfaces(item["extraInterfaces"], interfaces, vpdMap,
-                               isSystemVpd);
+            // Populate interfaces and properties that are common to every FRU
+            // and additional interface that might be defined on a per-FRU
+            // basis.
+            if (item.find("extraInterfaces") != item.end())
+            {
+                populateInterfaces(item["extraInterfaces"], interfaces, vpdMap,
+                                   isSystemVpd);
+            }
         }
 
         /*add Common interface to all the fru except the one having "mts" in
          their location code as they will not inherit CI*/
-        const std::string& LocationCode =
+        const string& LocationCode =
             item["extraInterfaces"][LOCATION_CODE_INF]["LocationCode"]
                 .get_ref<const nlohmann::json::string_t&>();
         if (LocationCode.substr(1, 3) != "mts")
@@ -442,34 +478,11 @@ int main(int argc, char** argv)
         if ((js.find("frus") == js.end()) ||
             (js["frus"].find(file) == js["frus"].end()))
         {
-            cout << "Device path not in JSON, ignoring" << std::endl;
+            cout << "Device path not in JSON, ignoring" << endl;
             return 0;
         }
 
-        uint32_t offset = 0;
-        // check if offset present?
-        for (const auto& item : js["frus"][file])
-        {
-            if (item.find("offset") != item.end())
-            {
-                offset = item["offset"];
-            }
-        }
-        char buf[2048];
-        ifstream vpdFile;
-        vpdFile.rdbuf()->pubsetbuf(buf, sizeof(buf));
-        vpdFile.open(file, ios::binary);
-        vpdFile.seekg(offset, std::ios_base::cur);
-
-        // Read 64KB data content of the binary file into a vector
-        Binary tmpVector((istreambuf_iterator<char>(vpdFile)),
-                         istreambuf_iterator<char>());
-
-        vector<unsigned char>::const_iterator first = tmpVector.begin();
-        vector<unsigned char>::const_iterator last = tmpVector.begin() + 65536;
-
-        Binary vpdVector(first, last);
-
+        Binary vpdVector(getVpdDataInVector(js, file));
         vpdType type = vpdTypeCheck(vpdVector);
 
         switch (type)
@@ -509,7 +522,7 @@ int main(int argc, char** argv)
             break;
 
             default:
-                throw std::runtime_error("Invalid VPD format");
+                throw runtime_error("Invalid VPD format");
         }
     }
     catch (exception& e)
