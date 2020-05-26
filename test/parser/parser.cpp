3853dc69e9d06e938cf68a805287036e9d09ae6b
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-05-26 02:23:11 -0500

    Interface & Factory implementation for VPD Parsers
    This commit abstracts the implementation logic of different parser.
    A parser factory is implemented to provide instance of required
    parser based on the type of vpd file needed to be parsed.
    
    The interface should be derived to implement any further parser logic
    related to vpd.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I3ce1a2d6b7e8d8984fd7800132e78ab8a9a21e56

--- test/parser/parser.cpp
+++ test/parser/parser.cpp
@@ -2,30 +2,29 @@
 #include <defines.hpp>
 #include <fstream>
 #include <iterator>
-#include <parser.hpp>
 #include <store.hpp>
+#include <vpd-parser/ipz_parser.hpp>
 
-void runTests()
-{
-    using namespace openpower::vpd;
+void runTests() {
+  using namespace openpower::vpd;
+  using namespace openpower::vpd::ipz::parser;
+  // Test parse() API
+  {
+    std::ifstream vpdFile("test.vpd", std::ios::binary);
+    Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
+               std::istreambuf_iterator<char>());
 
-    // Test parse() API
-    {
-        std::ifstream vpdFile("test.vpd", std::ios::binary);
-        Binary vpd((std::istreambuf_iterator<char>(vpdFile)),
-                   std::istreambuf_iterator<char>());
+    IpzVpdParser ipzParser(std::move(vpd));
+    auto vpdStore = std::move(std::get<Store>(ipzParser.parse()));
 
-        auto vpdStore = parse(std::move(vpd));
-
-        assert(("P012" == vpdStore.get<Record::VINI, record::Keyword::CC>()));
-        assert(("2019-01-01-08:30:00" ==
-                vpdStore.get<Record::VINI, record::Keyword::MB>()));
-    }
+    assert(("P012" == vpdStore.get<Record::VINI, record::Keyword::CC>()));
+    assert(("2019-01-01-08:30:00" ==
+            vpdStore.get<Record::VINI, record::Keyword::MB>()));
+  }
 }
 
-int main()
-{
-    runTests();
+int main() {
+  runTests();
 
-    return 0;
+  return 0;
 }
