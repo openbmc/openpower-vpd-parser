7dce19e64fc0e6c72ee5d6f0e6d05cb8377c2a40
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-05-30 12:05:53 -0500

    Test cases for VPD-Manager read functionalities
    Implementation of test cases for read functionalities provided
    by VPD-Manager app.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I83ad6757934a4c28ae26eb11f0b1f380e2c82aa8

---
+++ test/vpd-manager-test/reader_test.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "utilInterface.hpp"
+
+#include <gmock/gmock.h>
+
+using namespace openpower::vpd::utils::interface;
+
+class MockUtilCalls : public UtilityInterface
+{
+  public:
+    MOCK_METHOD(std::string, readBusProperty,
+                (const string& obj, const string& inf, const string& prop),
+                (override));
+};
