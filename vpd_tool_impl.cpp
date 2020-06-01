a53ad82b10a216e4721b862bfab2307139243c09
Alpana Kumari <alpankum@in.ibm.com>
2020-06-01 00:32:21 -0500

    Support FRU Type in vpd tool
    Test:
    ./vpd-tool --dumpObject  --object  /system/chassis/motherboard/cpu0
    [
        {
            "/system/chassis/motherboard/cpu0": {
                "CC": "CB41",
                "DR": "A WAY  PROC CUOD",
                "FN": "1234567",
                "LocationCode": "U78DA.ND1.1234567-P0-C15",
                "PN": "1345678",
                "SN": "YLAB41010000",
                "TYPE": "fruAndModule",  <=================================
                "type": "xyz.openbmc_project.Inventory.Item.Cpu"
            }
        }
    ]
    
    ./vpd-tool --dumpObject  --object  /system/chassis/motherboard/cpu1
    [
        {
            "/system/chassis/motherboard/cpu1": {
                "CC": "BB41",
                "DR": "A WAY  PROC CUOD",
                "FN": "1234567",
                "LocationCode": "U78DA.ND1.1234567-P0-C15",
                "PN": "1345678",
                "SN": "YLAB41010000",
                "TYPE": "moduleOnly",    <==================================
                "type": "xyz.openbmc_project.Inventory.Item.Cpu"
            }
        }
    ]
    
     ./vpd-tool --dumpObject  --object  /system/chassis/motherboard/ebmc_card_bmc
    [
        {
            "/system/chassis/motherboard/ebmc_card_bmc": {
                "CC": "6B58",
                "DR": "EBMC            ",
                "FN": "F191014",
                "LocationCode": "U78DA.ND1.1234567-P0-C5",
                "PN": "PN12345",
                "SN": "YL6B58010000",
                "TYPE": "FRU",             <=================================
                "type": "xyz.openbmc_project.Inventory.Item.Bmc"
            }
        }
    ]
    
    Change-Id: Ie36631913481514e9f06bd50415a3d6b057f8510
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

--- vpd_tool_impl.cpp
+++ vpd_tool_impl.cpp
@@ -144,6 +144,8 @@ json VpdTool::getVINIProperties(string invPath, json exIntf)
     }
 
     addFruTypeAndLocation(exIntf, objectName, kwVal);
+    kwVal.emplace("TYPE", fruType);
+
     output.emplace(invPath, kwVal);
     return output;
 }
@@ -189,6 +191,13 @@ json VpdTool::interfaceDecider(json& itemEEPROM)
 
     bool exIntfCheck = false;
     json output = json::object({});
+    fruType = "FRU";
+
+    // check type and add FRU Type in object
+    if (itemEEPROM.find("type") != itemEEPROM.end())
+    {
+        fruType = itemEEPROM.at("type");
+    }
 
     if (itemEEPROM.value("inherit", true))
     {
@@ -211,6 +220,7 @@ json VpdTool::interfaceDecider(json& itemEEPROM)
         }
         output.emplace(itemEEPROM.at("inventoryPath"), js);
     }
+
     return output;
 }
 
