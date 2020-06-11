7a425a9437094552c70340804634c58fdf95fb8c
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-06-11 06:19:47 -0500

    Merge openpower_read_vpd and ibm_read_vpd.
    This commit merges the implementation of source code required to
    build openpower_read_vpd_exe ibm_read_vpd exe.
    Main objective is to have single exe irrespective of the type of
    parser.
    
    Major modification includes:
    1)Introduction of a new parser class for openpower.
    2)Modification in the code to populate dbus in case of openpower.
    3)Get rid of auto generated files.
    4)Removal of app.cpp to have a single main when having a single
    exe.
    5)Removal of write.cpp and write.hpp.
    
    Status: Not Tested, Compilation ok.
    
    Signed-off-by: SunnySrivastava1984 <sunnsr25@in.ibm.com>
    Change-Id: I647a4e335abd0f20c02f78b5d58108c974088e92

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -1,5 +1,6 @@
 #include "config.h"
 
+#include "args.hpp"
 #include "const.hpp"
 #include "defines.hpp"
 #include "ipz_parser.hpp"
@@ -345,22 +346,27 @@ static void populateDbus(const T& vpdMap, nlohmann::json& js,
         {
             populateInterfaces(item["extraInterfaces"], interfaces, vpdMap,
                                isSystemVpd);
-        }
 
-        /*add Common interface to all the fru except the one having "mts" in
-         their location code as they will not inherit CI*/
-        const std::string& LocationCode =
-            item["extraInterfaces"][LOCATION_CODE_INF]["LocationCode"]
-                .get_ref<const nlohmann::json::string_t&>();
-        if (LocationCode.substr(1, 3) != "mts")
-        {
-            if (js.find("commonInterfaces") != js.end())
+            //this condition is needed as openpower json will not have location
+            //code interface
+            if(item["extraInterfaces"].find("LOCATION_CODE_INF") != item.end())
             {
-                populateInterfaces(js["commonInterfaces"], interfaces, vpdMap,
-                                   isSystemVpd);
+                /*add Common interface to all the fru except the one having "mts" in
+                their location code as they will not inherit CI*/
+                const std::string& LocationCode =
+                item["extraInterfaces"][LOCATION_CODE_INF]["LocationCode"]
+                    .get_ref<const nlohmann::json::string_t&>();
+                if (LocationCode.substr(1, 3) != "mts")
+                {
+                    if (js.find("commonInterfaces") != js.end())
+                    {
+                        populateInterfaces(js["commonInterfaces"], interfaces, vpdMap,
+                                        isSystemVpd);
+                    }
+                }
             }
         }
-
+        
         objects.emplace(move(object), move(interfaces));
     }
 
@@ -430,14 +436,17 @@ int main(int argc, char** argv)
 
     try
     {
-        App app{"ibm-read-vpd - App to read IPZ format VPD, parse it and store "
+        App app{"ibm-read-vpd - App to read VPD, parse it and store "
                 "in DBUS"};
         string file{};
+        bool doDump = false;
 
-        app.add_option("-f, --file", file, "File containing VPD (IPZ/KEYWORD)")
+        app.add_flag("--dump", doDump, "Optional argument to dump vpd");
+
+        app.add_option("-f, --file", file, "File containing VPD")
             ->required()
             ->check(ExistingFile);
-
+        
         CLI11_PARSE(app, argc, argv);
 
         // Make sure that the file path we get is for a supported EEPROM
@@ -477,18 +486,22 @@ int main(int argc, char** argv)
 
         ParserInterface* parser =
             ParserFactory::getParser(std::move(vpdVector));
-        // string preIntrStr = parser->getInterfaceName();
-
+        
         variant<KeywordVpdMap, Store> parseResult;
         parseResult = parser->parse();
 
         if (auto pVal = get_if<Store>(&parseResult))
         {
-            populateDbus(pVal->getVpdMap(), js, file); //, preIntrStr);
+            if(doDump)
+            {
+                pVal->dump();
+                return 0;
+            }
+            populateDbus(pVal->getVpdMap(), js, file);
         }
         else if (auto pVal = get_if<KeywordVpdMap>(&parseResult))
         {
-            populateDbus(*pVal, js, file); //, preIntrStr);
+            populateDbus(*pVal, js, file);
         }
 
         // release the parser object
@@ -499,6 +512,5 @@ int main(int argc, char** argv)
         cerr << e.what() << "\n";
         rc = -1;
     }
-
     return rc;
 }
