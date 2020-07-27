84d18850ea5f37202d5b8677107fa0ea7fa6cff4
Priyanga Ramasamy <priyanga24@in.ibm.com>
2020-07-27 11:59:38 +0530

    JSON Schema for VPD inventory json.
    This schema validates the vpd inventory JSON document.
    
    The JSON document should incorporate the changes from the following commits:
    
    	< https://gerrit.openbmc-project.xyz/c/openbmc/meta-ibm/+/31821/1/meta-witherspoon/recipes-phosphor/vpd/ibm-vpd-parser/vpd_inventory.json >
    
    	< https://gerrit.openbmc-project.xyz/c/openbmc/meta-ibm/+/30856/7/meta-witherspoon/recipes-phosphor/vpd/ibm-vpd-parser/vpd_inventory.json >
    
    This schema will validate both 2U and 4U system inventory jsons.
    
    ( pending: runtime validation of json schema using valyjson library. )
    
    Command to test:
    python main.py -d <JSON_DOCUMENT> -s <SCHEMA_DOCUMENT>
    
    Result:
    Validation Success message!
    
    Signed-off-by: Priyanga Ramasamy <priyanga24@in.ibm.com>
    Change-Id: Iab89bc5f6b1df71f441800d77fae7418627fa03d

---
+++ json/schema/main.py
@@ -0,0 +1,32 @@
+#!usr/bin/env python3
+
+import argparse
+import sys
+import jsonschema
+import json
+def handle_validation_error():
+    sys.exit("validation failed");
+def validate_schema(data,schema):
+    with open(data) as data_handle:
+        data_json=json.load(data_handle)
+        with open(schema) as schema_handle:
+            schema_json=json.load(schema_handle)
+            try:
+               jsonschema.validate(data_json, schema_json)
+            except jsonschema.ValidationError as e:
+                print(e)
+                handle_validation_error()
+    return data_json
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser(description='Vpd inventory JSON validator')
+    parser.add_argument('-s','--json_schema',dest='json_schema',help='JSON schema for vpd inventory JSONs.')
+    parser.add_argument('-d','--json_doc',dest='json_doc',help='Vpd inventory JSON document')
+    args=parser.parse_args()
+    if not args.json_schema:
+        parser.print_help()
+        sys.exit("Error: Vpd inventory JSON schema is required.")
+    if not args.json_doc:
+        parser.print_help()
+        sys.exit("Error: Vpd inventory JSON document is required.")
+    data_json = validate_schema(args.json_doc, args.json_schema)
+    print("validation success")
