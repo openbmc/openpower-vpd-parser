b6db6b240c8c4aee366ab4b2887c21550c57ed10
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-09-07 06:04:50 -0500

    VPD recollection at standby
    This commit implement changes to recollect VPD at standby.
    In case there is a replacement of hardware at standby, this commit takes
    care of recollecting VPD for that hardware, re-publishing those data, if
    required on Dbus and binding of driver with those hardware(s).
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: Ia0d377b554299faac3b46a4dc8dd96f964f07bd2

--- vpd-manager/manager.cpp
+++ vpd-manager/manager.cpp
@@ -6,11 +6,18 @@
 #include "ibm_vpd_utils.hpp"
 #include "ipz_parser.hpp"
 #include "reader_impl.hpp"
+#include "vpd_exceptions.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
 
 using namespace openpower::vpd::constants;
 using namespace openpower::vpd::inventory;
 using namespace openpower::vpd::manager::editor;
 using namespace openpower::vpd::manager::reader;
+using namespace std;
+using namespace openpower::vpd::parser;
+using namespace openpower::vpd::exceptions;
+using namespace phosphor::logging;
 
 namespace openpower
 {
@@ -90,6 +97,11 @@ void Manager::processJSON()
                     itemEEPROM["inventoryPath"]
                         .get_ref<const nlohmann::json::string_t&>());
             }
+
+            if (itemEEPROM.value("isReplacable", false))
+            {
+                replacableFrus.emplace_back(itemFRUS.key());
+            }
         }
     }
 }
@@ -154,6 +166,112 @@ LocationCode Manager::getExpandedLocationCode(const LocationCode locationCode,
     ReaderImpl read;
     return read.getExpandedLocationCode(locationCode, nodeNumber,
                                         fruLocationCode);
+}
+
+void Manager::performVPDRecollection()
+{
+    try
+    {
+        // get list of FRUs replacable at standby
+        for (const auto& item : replacableFrus)
+        {
+            bool isRecollected = false;
+            string inventoryPath{};
+            const vector<nlohmann::json>& groupEEPROM = jsonFile["frus"][item];
+            for (const auto& singleFru : groupEEPROM)
+            {
+                // Check if the FRU can be replaces at standby
+                if (!singleFru.value("isReplacable", false))
+                {
+                    continue;
+                }
+
+                inventoryPath = singleFru["inventoryPath"]
+                                    .get_ref<const nlohmann::json::string_t&>();
+
+                if ((singleFru.find("devAddress") == singleFru.end()) ||
+                    (singleFru.find("driverType") == singleFru.end()) ||
+                    (singleFru.find("busType") == singleFru.end()))
+                {
+                    // The FRUs is marked for replacement but missing mandatory
+                    // fields for recollection
+                    break;
+                }
+
+                string str = "echo ";
+                string deviceAddress = singleFru["devAddress"];
+                const string& driverType = singleFru["driverType"];
+                const string& busType = singleFru["busType"];
+                string cmd = str + deviceAddress + " > /sys/bus/" + busType +
+                             "/drivers/" + driverType;
+
+                // devTreeStatus flag is present in json as false to mention
+                // that the EEPROM is not mentioned in device tree. If this flag
+                // is absent consider the value to be true, i.e EEPROM is
+                // mentioned in device tree
+                if (!singleFru.value("devTreeStatus", true))
+                {
+                    auto pos = deviceAddress.find('-');
+                    if (pos != string::npos)
+                    {
+                        executeCmd(cmd + "/unbind");
+
+                        string busNum = deviceAddress.substr(0, pos);
+                        deviceAddress =
+                            "0x" + deviceAddress.substr(pos + 1, string::npos);
+
+                        string bindCmd = str + driverType + " " +
+                                         deviceAddress + " > /sys/bus/" +
+                                         busType + "/devices/" + busType + "-" +
+                                         busNum + "/new_device";
+
+                        executeCmd(bindCmd);
+
+                        isRecollected = true;
+
+                        // do not process other FRUs under the EEPROM path once
+                        // data has been recollected.
+                        break;
+                    }
+                    else
+                    {
+                        throw VpdJsonException("Wrong format of data in "
+                                               "device address field of Json.",
+                                               INVENTORY_JSON_SYM_LINK);
+                    }
+                }
+                // if present it should be true, if absent then consider as true
+                else
+                {
+                    executeCmd(cmd + "/unbind");
+                    executeCmd(cmd + "/bind");
+
+                    isRecollected = true;
+                    // do not process other FRUs under the EEPROM path once data
+                    // has been recollected.
+                    break;
+                }
+            }
+
+            // if the VPD has not been recollected till this point implies entry
+            // is missing in JSON
+            if (isRecollected == false)
+            {
+                // if FRU is marked as replacable and mandatory fields are
+                // missing log error
+                throw VpdJsonException(
+                    "Mandatory field(s) devAddress/driverType/busType required "
+                    "for recollection is missing in JSON for fru " +
+                        inventoryPath,
+                    INVENTORY_JSON_SYM_LINK);
+            }
+        }
+    }
+    catch (const VpdJsonException& ex)
+    {
+        // map to hold additional data in case of logging pel
+        log<level::ERR>("Recoolection Failed", entry("ERROR=%s", ex.what()));
+    }
 }
 
 } // namespace manager
