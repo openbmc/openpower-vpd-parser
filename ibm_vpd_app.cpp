647868edf10761db109d8b51a2013ebed6f86463
PriyangaRamasamy <priyanga24@in.ibm.com>
2020-09-08 17:03:19 +0530

    Translate Udev Event path to a generic path
    Given Udev event generated path is translated into sys bus path - as the json will
    have the sys/bus path.
    
    Test:
    -> Translation successful for all frus including frus behind muxes.
    -> If the system vpd is triggered by udev event, then the execution stops as
    the system vpd will already be parsed via system-vpd.service.
    
    Few Reasons why we translate the path to a generic path:
    	-> Each i2c bus is memory mapped to a certain address in ASPEED.
    	   In future if there are any architectural changes in the kernel, we need to
    	   make changes to the JSON accordingly.
    	-> Also in future if the system runs on a different ASPEED, we need to
    	   update the JSON accordingly.
    	-> This generic /sys/bus path represents the system wiring and doesnot rely on
    	   how the busses are memory mapped.
    	-> Also for call out based events, generic path is expected than the udev event path.
    
    Sample Udev Event i2c path : "/sys/devices/platform/ahb/ahb:apb/ahb:apb:bus@1e78a000/1e78a480.i2c-bus/i2c-8/8-0050/8-00500/nvmem"
    Which gets translated to /sys/bus/ path as : "/sys/bus/i2c/drivers/at24/8-0050/eeprom"
    
    Sample Udev Event spi path : "/sys/devices/platform/ahb/ahb:apb/1e79b000.fsi/fsi-master/fsi0/slave@00:00/00:00:00:0a/fsi-master/fsi1/slave@03:00/01:03:00:04/spi_master/spi6/spi6.0/spi6.00/nvmem"
    Which gets translated to /sys/bus/ path as : "/sys/bus/spi/drivers/at25/spi6.0/eeprom"
    
    Sample Udev Event i2c path for frus behind mux : "/sys/devices/platform/ahb/ahb:apb/ahb:apb:bus@1e78a000/1e78a300.i2c-bus/i2c-5/i2c-24/24-0051/24-00510/nvmem"
    Which gets translated to generic path as /sys/bus/i2c/drivers/at24/24-0051/eeprom
    
    Tested on simics:
    
    <the inventory json should have generic eeprom paths for all frus>
    
    root@rainier:/tmp# ./ibm-read-vpd-UG -f /sys/devices/platform/ahb/ahb:apb/1e79b000.fsi/fsi-master/fsi0/slave@00:00/00:00:00:04/spi_master/spi12/spi12.0/spi12.00/nvmem
    the path after translation : /sys/bus/spi/drivers/at25/spi12.0/eeprom
    
    root@rainier:/tmp# ./ibm-read-vpd-UG -f /sys/devices/platform/ahb/ahb:apb/ahb:apb:bus@1e78a000/1e78a480.i2c-bus/i2c-8/8-0051/8-00510/nvmem
    the path after translation : /sys/bus/i2c/drivers/at24/8-0051/eeprom
    
    root@rainier:/tmp# ./ibm-read-vpd-UG -f /sys/bus/i2c/drivers/at24/8-0051/8-00510/nvmem
    the path after translation : /sys/bus/i2c/drivers/at24/8-0051/eeprom
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I6d5995a85ef15e63d2d0b6f054fb0bf14a2b756c

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -887,7 +887,6 @@ static void populateDbus(T& vpdMap, nlohmann::json& js, const string& filePath)
 int main(int argc, char** argv)
 {
     int rc = 0;
-    string file{};
     json js{};
 
     // map to hold additional data in case of logging pel
@@ -902,9 +901,9 @@ int main(int argc, char** argv)
 
     try
     {
+        string file{};
         App app{"ibm-read-vpd - App to read IPZ format VPD, parse it and store "
                 "in DBUS"};
-        string file{};
 
         app.add_option("-f, --file", file, "File containing VPD (IPZ/KEYWORD)")
             ->required();
@@ -942,6 +941,22 @@ int main(int argc, char** argv)
             throw(VpdJsonException("Json parsing failed", jsonToParse));
         }
 
+        // Check if it's a udev path - patterned as(/ahb/ahb:apb/ahb:apb:bus@)
+        if (file.find("/ahb:apb") != string::npos)
+        {
+            // Translate udev path to a generic /sys/bus/.. file path.
+            udevToGenericPath(file);
+            if (js["frus"][file].at(0).value("isSystemVpd", false))
+            {
+                return 0;
+            }
+        }
+
+        if (file.empty())
+        {
+            cerr << "The EEPROM path <" << file << "> is not valid.";
+            return 0;
+        }
         if ((js.find("frus") == js.end()) ||
             (js["frus"].find(file) == js["frus"].end()))
         {
@@ -970,13 +985,6 @@ int main(int argc, char** argv)
 
         try
         {
-            // check if vpd file is empty
-            if (file.empty())
-            {
-                throw(VpdDataException(
-                    "VPD file is empty. Can't process with blank file."));
-            }
-
             Binary vpdVector = getVpdDataInVector(js, file);
             ParserInterface* parser = ParserFactory::getParser(move(vpdVector));
 
