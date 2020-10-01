8d8be3b7d618a582983dcb0baaff794920dd3196
Alpana Kumari <alpankum@in.ibm.com>
2020-10-01 00:18:10 -0500

    Enable Dynamic Presence Detect of op-panel
    This commit enables support for runtime presence detect
    of op-panel LCD. Anytime this fru gets attached OR de-attached,
    this code will detect it and enables/disables it's I2C pin respectively.
    
    Test- Tested on Simics:
    root@rainier:/tmp# ./vpd-manager
    Init Oppanel GpioInfos
    Event_handler running
    First time: Save Current Presence State
    Event checking every 5 seconds
    Did event Occur?
        check Current Presence State
        No Event occurred.
    
    Did event Occur?
        check Current Presence State
        No Event occurred.
    
    ...
    ...
    Did event Occur?
        check Current Presence State
        Yes. Event occurred.
        Toggle the Oppanel I2C Gpio
        Save Current Presence State
    
    ...
    ...
    
    Effects on i2c-
    root@rainier:/tmp# i2cdetect -y 7
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:          -- -- -- -- -- -- -- -- -- -- -- -- --
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: 20 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: -- -- -- -- -- -- -- -- UU -- -- -- -- -- -- --
    50: UU UU UU -- -- -- -- -- -- -- 5a -- -- -- -- --
    60: UU UU UU -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- UU --
    
    root@rainier:/tmp# i2cdetect -y 7
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:          -- -- -- -- -- -- -- -- -- -- -- -- --
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: 20 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: -- -- -- -- -- -- -- -- UU -- -- -- -- -- -- --
    50: UU 51 UU -- -- -- -- -- -- -- 5a -- -- -- -- --
    60: UU UU UU -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- UU --
    
    Also tested for write functionality, shouldn't be affected due to this change
    It looks fine.
    busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager WriteKeyword ossay  "/system/chassis/motherboard/ebmc_card_bmc" "VINI" "PN" 1 66
    .PartNumber                                   property  s         "BN12345"
    
    Change-Id: If7d311d36bf56ece751afe393a9ba2d83be5df11
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

---
+++ vpd-manager/gpioMonitor.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include "manager.hpp"
+
+#include <gpiod.hpp>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <sdeventplus/clock.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/utility/timer.hpp>
+
+namespace openpower
+{
+namespace vpd
+{
+namespace gpioMonitor
+{
+using namespace std;
+using sdeventplus::ClockId;
+using sdeventplus::Event;
+constexpr auto clockId = ClockId::RealTime;
+using Timer = sdeventplus::utility::Timer<clockId>;
+
+/** @class GpioMonitor
+ *  @brief Responsible for collect and init gpio infos
+ */
+class GpioMonitor
+{
+  public:
+    GpioMonitor() = delete;
+    ~GpioMonitor() = default;
+    GpioMonitor(const GpioMonitor&) = delete;
+    GpioMonitor& operator=(const GpioMonitor&) = delete;
+    GpioMonitor(GpioMonitor&&) = delete;
+    GpioMonitor& operator=(GpioMonitor&&) = delete;
+
+    GpioMonitor(nlohmann::json& js, sdbusplus::bus::bus& bus) :
+        jsonFile(js), _bus(bus)
+    {
+        initOppanelGpioInfos();
+    };
+
+  private:
+    nlohmann::json& jsonFile;
+    sdbusplus::bus::bus& _bus;
+
+    /*@brief This is to extract the gpio informations from vpd json and store*/
+    void initOppanelGpioInfos();
+};
+
+/** @class GpioEventHandler
+ *  @brief Responsible for catching event and handle it
+ */
+class GpioEventHandler
+{
+  public:
+    GpioEventHandler() = delete;
+    ~GpioEventHandler() = default;
+    GpioEventHandler(const GpioEventHandler&) = delete;
+    GpioEventHandler& operator=(const GpioEventHandler&) = delete;
+    GpioEventHandler(GpioEventHandler&&) = delete;
+    GpioEventHandler& operator=(GpioEventHandler&&) = delete;
+
+    GpioEventHandler(const string& presPin, const uint8_t& presValue,
+                     const string& outPin, const uint8_t& outValue,
+                     const string& devAddr, const string& driver,
+                     const string& bus, sdbusplus::bus::bus& busConn) :
+        presencePin(presPin),
+        presenceValue(presValue), outputPin(outPin), outputValue(outValue),
+        devNameAddr(devAddr), driverType(driver), busType(bus), _dbus(busConn)
+    {
+        eventHandler();
+    };
+
+    /*Preserve the previous GPIO value*/
+    bool prevPresenceState;
+
+  private:
+    sdbusplus::bus::bus& _dbus;
+    /*op-panel GPIO informations to get initialised from vpd json*/
+    string presencePin;
+    uint8_t presenceValue;
+    string outputPin;
+    uint8_t outputValue;
+
+    string devNameAddr;
+    string driverType;
+    string busType;
+
+    /* @brief This function will toggle the gpio as per the presence state.
+     */
+    void toggleOppanelGpio();
+
+    /* @brief This function checks for an event occurred on GPIO presence pin
+     *
+     * @returns true if event occurred
+     *          false otherwise
+     */
+    bool eventOccurred();
+
+    /* @brief This is a helper function to read the
+     *        current value of Presence GPIO
+     *
+     * @returns The GPIO value
+     */
+    bool getCurrentPresenceState();
+
+    /*@brief This function keeps checking for if an event happened
+     *       if event occured then take care of action
+     */
+    void eventHandler();
+};
+
+} // namespace gpioMonitor
+} // namespace vpd
+} // namespace openpower
