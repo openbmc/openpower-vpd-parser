f2357f72e5d98afb2d017b31794d739e80d6eeb1
Alpana Kumari <alpankum@in.ibm.com>
2020-10-01 00:18:10 -0500

    Enable Dynamic Presence Detect of op-panel
    This commit enables support for runtime presence detect
    of op-panel LCD. Anytime this fru gets attached OR de-attached,
    this code will detect it and enables/disables it's I2C pin respectively.
    
    Test- Tested on Simics:
    root@rainier:/tmp# ./vpd-manager
    Init Oppanel GpioInfos
    Event_handler running
    First time: Save Current Presence State
    Event checking every 5 seconds
    Did event Occur?
        check Current Presence State
        No Event occurred.
    
    Did event Occur?
        check Current Presence State
        No Event occurred.
    
    ...
    ...
    Did event Occur?
        check Current Presence State
        Yes. Event occurred.
        Toggle the Oppanel I2C Gpio
        Save Current Presence State
    
    ...
    ...
    
    Effects on i2c-
    root@rainier:/tmp# i2cdetect -y 7
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:          -- -- -- -- -- -- -- -- -- -- -- -- --
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: 20 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: -- -- -- -- -- -- -- -- UU -- -- -- -- -- -- --
    50: UU UU UU -- -- -- -- -- -- -- 5a -- -- -- -- --
    60: UU UU UU -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- UU --
    
    root@rainier:/tmp# i2cdetect -y 7
         0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
    00:          -- -- -- -- -- -- -- -- -- -- -- -- --
    10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    20: 20 -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
    40: -- -- -- -- -- -- -- -- UU -- -- -- -- -- -- --
    50: UU 51 UU -- -- -- -- -- -- -- 5a -- -- -- -- --
    60: UU UU UU -- -- -- -- -- -- -- -- -- -- -- -- --
    70: -- -- -- -- -- -- UU --
    
    Also tested for write functionality, shouldn't be affected due to this change
    It looks fine.
    busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager WriteKeyword ossay  "/system/chassis/motherboard/ebmc_card_bmc" "VINI" "PN" 1 66
    .PartNumber                                   property  s         "BN12345"
    
    Change-Id: If7d311d36bf56ece751afe393a9ba2d83be5df11
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

---
+++ vpd-manager/gpioMonitor.cpp
@@ -0,0 +1,160 @@
+#include "config.h"
+
+#include "gpioMonitor.hpp"
+
+#include "utils.hpp"
+
+#include <systemd/sd-event.h>
+
+using namespace std;
+
+namespace openpower
+{
+namespace vpd
+{
+namespace gpioMonitor
+{
+
+bool GpioEventHandler::getCurrentPresenceState()
+{
+    uint8_t gpioData = 0;
+    gpiod::line presenceLine = gpiod::find_line(presencePin);
+    if (!presenceLine)
+    {
+        cout << "Error getCurrentPresenceState: couldn't find presence line:"
+             << presencePin << " on GPIO \n";
+        // return previous state as we couldn't read current state
+        return prevPresenceState;
+    }
+
+    presenceLine.request(
+        {"Op-panel presence line", gpiod::line_request::DIRECTION_INPUT, 0});
+
+    gpioData = presenceLine.get_value();
+    if (!(gpioData ^ presenceValue))
+    {
+        // it's present
+        return true;
+    }
+    return false;
+}
+
+void GpioMonitor::initOppanelGpioInfos()
+{
+    uint8_t setOrReset = 0;
+    uint8_t value = 0;
+    string presencePinName, outputPinName;
+
+    for (const auto& eachFRU : jsonFile["frus"].items())
+    {
+        for (const auto& eachInventory : eachFRU.value())
+        {
+            if (eachInventory.find("preAction") != eachInventory.end())
+            {
+                for (const auto& presStatus : eachInventory["presence"].items())
+                {
+                    if (presStatus.key() == "pin")
+                    {
+                        presencePinName = presStatus.value();
+                    }
+                    else if (presStatus.key() == "value")
+                    {
+                        value = presStatus.value();
+                    }
+                }
+
+                for (const auto& preAction : eachInventory["preAction"].items())
+                {
+                    if (preAction.key() == "pin")
+                    {
+                        outputPinName = preAction.value();
+                    }
+                    else if (preAction.key() == "value")
+                    {
+                        setOrReset = preAction.value();
+                    }
+                }
+
+                string devNameAddr = eachInventory["devAddress"];
+                string driverType = eachInventory["driverType"];
+                string busType = eachInventory["busType"];
+
+                // Init all Gpio info variable
+                GpioEventHandler eventHndlr(
+                    presencePinName, value, outputPinName, setOrReset,
+                    devNameAddr, driverType, busType, _bus);
+            }
+        }
+    }
+}
+
+void GpioEventHandler::toggleOppanelGpio()
+{
+    bool isPresent = getCurrentPresenceState();
+    bool newValue = false;
+
+    if (!(isPresent ^ outputValue))
+    {
+        // set the gpio pin
+        newValue = true;
+    }
+
+    gpiod::line outputLine = gpiod::find_line(outputPin);
+    if (!outputLine)
+    {
+        cout << "Error: toggleOppanelGpio: couldn't find output line:"
+             << outputPin << ". Skipping update\n";
+
+        return;
+    }
+
+    outputLine.request({"Op-panel LCD present: set the output pin",
+                        gpiod::line_request::DIRECTION_OUTPUT, 0},
+                       newValue);
+
+    string str = "echo ";
+    if (isPresent)
+    {
+        // bind the driver
+        str = str + devNameAddr + " > /sys/bus/" + busType + "/drivers/" +
+              driverType + "/bind";
+    }
+    else
+    {
+        // unbind the driver
+        str = str + devNameAddr + " > /sys/bus/" + busType + "/drivers/" +
+              driverType + "/unbind";
+    }
+    system(str.c_str());
+
+    prevPresenceState = isPresent;
+}
+
+inline bool GpioEventHandler::eventOccurred()
+{
+    return getCurrentPresenceState() != prevPresenceState;
+}
+
+void GpioEventHandler::eventHandler()
+{
+    prevPresenceState = getCurrentPresenceState();
+    unsigned interval = 5;
+
+    auto event = Event::get_default();
+    Timer timer(
+        event,
+        [this](Timer&) {
+            if (eventOccurred())
+            {
+                toggleOppanelGpio();
+            }
+        },
+        std::chrono::seconds{interval});
+
+    _dbus.attach_event(event.get(), SD_EVENT_PRIORITY_IMPORTANT);
+    event.loop();
+}
+
+} // namespace gpioMonitor
+} // namespace vpd
+} // namespace openpower
