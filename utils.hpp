06e1f772c25fd24176c5b2f173495cf5331b3b73
PriyangaRamasamy <priyanga24@in.ibm.com>
2020-11-20 00:00:09 +0530

    Handle PASS 2 Planar in ibm-read-vpd
    The system has two planar passes - Pass1 & Pass2 Planars.
    Pass1 Planar a buffer connects all pcie cards, Whereas
    Pass2 Planar has mux connection established for pcie card selection.
    
    ibm-read-vpd picks up right json(either 2UPass1/2UPass2/4UPass1/4UPass2)
    based on the PN and IM kw of the planar.
    
    Test-
    tested on Rainier 2U simics
    
    root@rainier:/tmp# ./ibm-read-vpd  -f /tmp/systemVpd
    imKeyword- 50001001, partNumber- PN12345
    Processing with this JSON - /usr/share/vpd/50001001_P1.json
    
    root@rainier:/tmp# ./ibm-read-vpd  -f /tmp/systemVpd
    imKeyword- 50001001, partNumber- PN11345
    Processing with this JSON - /usr/share/vpd/50001001_P2.json
    
    root@rainier:/tmp# ./ibm-read-vpd  -f /tmp/systemVpd
    imKeyword- 50001000, partNumber- PN11345
    Processing with this JSON - /usr/share/vpd/50001000_P2.json
    
    root@rainier:/tmp# ./ibm-read-vpd  -f /tmp/systemVpd
    imKeyword- 50001000, partNumber- PN12345
    Processing with this JSON - /usr/share/vpd/50001000_P1.json
    root@rainier:/tmp#
    
    Signed-off-by: PriyangaRamasamy <priyanga24@in.ibm.com>
    Change-Id: I96fba736cebddbbefad291c1c60b6fae818252ec

--- utils.hpp
+++ utils.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "const.hpp"
+#include "store.hpp"
 #include "types.hpp"
 
 #include <iostream>
 
 using namespace std;
+
 namespace openpower
 {
 namespace vpd
@@ -80,12 +82,24 @@ string encodeKeyword(const string& kw, const string& encoding);
 string readBusProperty(const string& obj, const string& inf,
                        const string& prop);
 
-/**
- * @brief API to create PEL entry
- * @param[in] Map holding the additional data
- * @param[in] error interface
+/** @brief This API checks for IM and PN keywords, and based
+ *         on these values decides which system json to be used.
+ *  @param[in] vpdMap  parsed vpd
+ *  @returns System json path
  */
-void createPEL(const std::map<std::string, std::string>& additionalData,
-               const std::string& errIntf);
+string getSystemsJson(const Parsed& vpdMap);
+
+/** @brief Reads PN Keyword from the vpd
+ *  @param[in] vpdMap  parsed vpd
+ *  @returns   value of PN Keyword
+ */
+const string getPN(const Parsed& vpdMap);
+
+/** @brief Reads IM Keyword from the vpd
+ *  @param[in] vpdMap  parsed vpd
+ *  @returns   value of IM Keyword
+ */
+const string getIM(const Parsed& vpdMap);
+
 } // namespace vpd
 } // namespace openpower
