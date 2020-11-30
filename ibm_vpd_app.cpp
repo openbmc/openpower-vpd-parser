32c99db4b7522bd4e590e7f5cfd2fd377324b178
Santosh Puranik <santosh.puranik@in.ibm.com>
2020-11-30 20:26:44 +0530

    Fix u-boot env var name for Rainier 2U
    Change the value of the fitconfig u-boot environment
    variable to 'conf@aspeed-bmc-ibm-rainier.dtb'.
    This is to match the value used by u-boot to switch to the right
    device tree for Rainier 2U
    
    Signed-off-by: Santosh Puranik <santosh.puranik@in.ibm.com>
    Change-Id: Ie52bffb1e50c0fd7d081038732e3c5676a69de03

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -34,7 +34,7 @@ using namespace openpower::vpd::parser::interface;
 using namespace openpower::vpd::exceptions;
 
 static const deviceTreeMap deviceTreeSystemTypeMap = {
-    {RAINIER_2U, "conf@aspeed-bmc-ibm-rainier-2u.dtb"},
+    {RAINIER_2U, "conf@aspeed-bmc-ibm-rainier.dtb"},
     {RAINIER_4U, "conf@aspeed-bmc-ibm-rainier-4u.dtb"}};
 
 /**
