85a96a6b84a162a62578eb51e7035a4d4e601043
Alpana Kumari <alpankum@in.ibm.com>
2021-02-02 06:10:43 -0600

    This commit enables check for BR keyword for system VPD
    systemRestore checks for BR keyword to get the BRAND of the system.
    Curently supported Brands are S0 and D0.
    As per the Brand,
    we have some set of keywords to verify it's not blank in the VPD,
    also should have valid value only.
    
    Test Result-
    
    root@rainier:/tmp# ./ibm-read-vpd -f /sys/bus/i2c/drivers/at24/8-0050/eeprom
    Read BR: S0
    RBkwVal[0]:
    Found keyword value invalid OR not supported...breaking
    
    as RB data is blank in the VPD, we are breaking here.
    Also, forced to continue with blank RB value-
    
    root@rainier:/tmp# ./ibm-read-vpd -f /sys/bus/i2c/drivers/at24/8-0050/eeprom
    
    Read BR: S0
    RBkwVal[0]:
    isKwValid: 1
    sys Brand: S0_BRAND
    Setting GPIO: SLOT0_PRSNT_EN_RSVD to 1
    Binding device 4-0050
    
    Change-Id: Id349357f06f72486b5fac4c53bf024827e152d8d
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

--- types.hpp
+++ types.hpp
@@ -20,43 +20,45 @@ using Binary = std::vector<Byte>;
 
 namespace inventory
 {
-
+using namespace std::string_literals;
 using Path = std::string;
 using Property = std::string;
 using Value = std::variant<bool, size_t, int64_t, std::string, Binary>;
-using PropertyMap = std::map<Property, Value>;
 using kwdVpdValueTypes = std::variant<size_t, Binary>;
 
 using Interface = std::string;
-using InterfaceMap = std::map<Interface, PropertyMap>;
-
 using Object = sdbusplus::message::object_path;
-using ObjectMap = std::map<Object, InterfaceMap>;
 
 using VPDfilepath = std::string;
 using FruIsMotherboard = bool;
-using FrusMap = std::multimap<Path, std::pair<VPDfilepath, FruIsMotherboard>>;
 using LocationCode = std::string;
-using LocationCodeMap = std::unordered_multimap<LocationCode, Path>;
-using ListOfPaths = std::vector<sdbusplus::message::object_path>;
 using NodeNumber = uint16_t;
-using namespace std::string_literals;
-using KeywordVpdMap = std::unordered_map<std::string, kwdVpdValueTypes>;
 
 using systemType = std::string;
 using deviceTree = std::string;
+using Keyword = std::string;
+using RecordName = std::string;
+using KeywordData = std::string;
+using Service = std::string;
+using BrandType = std::string;
+
+using PropertyMap = std::map<Property, Value>;
+using InterfaceMap = std::map<Interface, PropertyMap>;
+using ObjectMap = std::map<Object, InterfaceMap>;
+using FrusMap = std::multimap<Path, std::pair<VPDfilepath, FruIsMotherboard>>;
+using LocationCodeMap = std::unordered_multimap<LocationCode, Path>;
+using ListOfPaths = std::vector<sdbusplus::message::object_path>;
+using KeywordVpdMap = std::unordered_map<std::string, Binary>;
 using deviceTreeMap = std::unordered_map<systemType, deviceTree>;
 using PelAdditionalData = std::map<std::string, std::string>;
-using Keyword = std::string;
-using KeywordData = std::string;
 using DbusPropertyMap = std::unordered_map<Keyword, KeywordData>;
-using PelAdditionalData = std::map<std::string, std::string>;
-using Service = std::string;
 using MapperResponse =
     std::map<Path, std::map<Service, std::vector<Interface>>>;
 using RestoredEeproms = std::tuple<Path, std::string, Keyword, Binary>;
 using ReplaceableFrus = std::vector<VPDfilepath>;
-} // namespace inventory
+using svpdBrandRecKwdMap =
+    std::map<BrandType, std::map<RecordName, std::vector<Keyword>>>;
 
+} // namespace inventory
 } // namespace vpd
 } // namespace openpower
