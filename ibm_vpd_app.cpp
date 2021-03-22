2d1beb73404b6e8cbb059dcb5d5a2036f38ccd27
Sunny Srivastava <sunnsr25@in.ibm.com>
2021-03-22 13:36:54 -0500

    Set severity for PELs
    This commit adds an option to pass severity while logging PEL.
    This will help to classify PELs based on their severity.
    By default severity of all the PELs logged for system VPD failure
    is set to be unrecoverable where as other failures are flagged as
    predictive errors.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I9cd10a29252a42f5031b91b9c7ad2e284ed5b861

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -622,7 +622,8 @@ void updateHardware(const string& objectName, const string& recName,
         PelAdditionalData additionalData{};
         additionalData.emplace("CALLOUT_INVENTORY_PATH", objectName);
         additionalData.emplace("DESCRIPTION", what);
-        createPEL(additionalData, errIntfForBusFailure);
+        createPEL(additionalData, severity::PelSeverity::WARNING,
+                  errIntfForBusFailure);
     }
 }
 
@@ -688,7 +689,9 @@ std::vector<RestoredEeproms> restoreSystemVPD(Parsed& vpdMap,
 
                                 additionalData.emplace("DESCRIPTION", errMsg);
 
-                                createPEL(additionalData, errIntfForInvalidVPD);
+                                createPEL(additionalData,
+                                          severity::PelSeverity::WARNING,
+                                          errIntfForInvalidVPD);
                             }
                         }
                         else
@@ -724,7 +727,8 @@ std::vector<RestoredEeproms> restoreSystemVPD(Parsed& vpdMap,
                         additionalData.emplace("DESCRIPTION", errMsg);
 
                         // log PEL TODO: Block IPL
-                        createPEL(additionalData, errIntfForBlankSystemVPD);
+                        createPEL(additionalData, severity::PelSeverity::ERROR,
+                                  errIntfForBlankSystemVPD);
                         continue;
                     }
                 }
@@ -930,6 +934,9 @@ int main(int argc, char** argv)
     // vpd exception while parsing the file
     std::string baseFruInventoryPath = {};
 
+    // severity for PEL
+    severity::PelSeverity pelSeverity = severity::PelSeverity::WARNING;
+
     try
     {
         App app{"ibm-read-vpd - App to read IPZ format VPD, parse it and store "
@@ -940,6 +947,12 @@ int main(int argc, char** argv)
             ->required();
 
         CLI11_PARSE(app, argc, argv);
+
+        // PEL severity should be ERROR in case of any system VPD failure
+        if (file == systemVpdFilePath)
+        {
+            pelSeverity = severity::PelSeverity::ERROR;
+        }
 
         auto jsonToParse = INVENTORY_JSON_DEFAULT;
 
@@ -954,8 +967,7 @@ int main(int argc, char** argv)
         ifstream inventoryJson(jsonToParse);
         if (!inventoryJson)
         {
-            throw(
-                (VpdJsonException("Failed to access Json path", jsonToParse)));
+            throw(VpdJsonException("Failed to access Json path", jsonToParse));
         }
 
         try
@@ -964,7 +976,7 @@ int main(int argc, char** argv)
         }
         catch (json::parse_error& ex)
         {
-            throw((VpdJsonException("Json parsing failed", jsonToParse)));
+            throw(VpdJsonException("Json parsing failed", jsonToParse));
         }
 
         if ((js.find("frus") == js.end()) ||
@@ -1029,7 +1041,7 @@ int main(int argc, char** argv)
     {
         additionalData.emplace("JSON_PATH", ex.getJsonPath());
         additionalData.emplace("DESCRIPTION", ex.what());
-        createPEL(additionalData, errIntfForJsonFailure);
+        createPEL(additionalData, pelSeverity, errIntfForJsonFailure);
 
         cerr << ex.what() << "\n";
         rc = -1;
@@ -1039,7 +1051,7 @@ int main(int argc, char** argv)
         additionalData.emplace("DESCRIPTION", "ECC check failed");
         additionalData.emplace("CALLOUT_INVENTORY_PATH",
                                INVENTORY_PATH + baseFruInventoryPath);
-        createPEL(additionalData, errIntfForEccCheckFail);
+        createPEL(additionalData, pelSeverity, errIntfForEccCheckFail);
 
         cerr << ex.what() << "\n";
         rc = -1;
@@ -1049,7 +1061,7 @@ int main(int argc, char** argv)
         additionalData.emplace("DESCRIPTION", "Invalid VPD data");
         additionalData.emplace("CALLOUT_INVENTORY_PATH",
                                INVENTORY_PATH + baseFruInventoryPath);
-        createPEL(additionalData, errIntfForInvalidVPD);
+        createPEL(additionalData, pelSeverity, errIntfForInvalidVPD);
 
         cerr << ex.what() << "\n";
         rc = -1;
