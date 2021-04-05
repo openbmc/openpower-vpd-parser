043a80a464049a975d1c110c19df06f732289017
Alpana Kumari <alpankum@in.ibm.com>
2021-04-05 19:09:19 +0000

    MemorySize support
    This Commit will add support for MemorySizeinKB
    Memory size gets calculated from the DIMM VPD and then
    populated it on dbus.
    
    Test- Tested on simics 2s2u
    
    Set the property with dummy value-
    busctl call xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory xyz.openbmc_project.Inventory.Manager Notify a{oa{sa{sv}}} 1 "/system/chassis/motherboard/tpm_wilson" 1 "xyz.openbmc_project.Inventory.Item.Dimm" 1 "MemorySizeInKB" u 10022
    
    busctl introspect xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard/tpm_wilson|grep MemorySizeInKB
    .MemorySizeInKB                               property  u         10022                                    emits-change writable
    
    Set the value via code-
    ./ibm-read-vpd -f /tmp/p10_dimmspd.dat
    Parser launched with file: /tmp/p10_dimmspd.dat
    dimmSize- 65536
    dimmSize in hex- 00010000
    
    busctl introspect xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system/chassis/motherboard/tpm_wilson|grep MemorySizeInKB
    .MemorySizeInKB                               property  u         65536                                    emits-change writable
    
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>
    Change-Id: I70e5bc3f538379f9891ae0ede6cdf09a585520a4

--- vpd-parser/memory_vpd_parser.cpp
+++ vpd-parser/memory_vpd_parser.cpp
@@ -17,10 +17,98 @@ using namespace constants;
 using namespace std;
 using namespace openpower::vpd::parser;
 
+auto memoryVpdParser::getDimmSize(Binary::const_iterator iterator,
+                                  size_t& dimmSize)
+{
+    size_t rc = 0, tmp = 0;
+    dimmSize = 0;
+
+    size_t sdram_cap = 1, pri_bus_wid = 1, sdram_wid = 1,
+           logical_ranks_per_dimm = 1;
+
+    Byte primaryBusWidthByteInSPD = SVPD_MEM_BYTE_13;
+    Byte sdramCapacityByteInSPD = SVPD_MEM_BYTE_4;
+    Byte sdramDeviceWidthByteInSPD = SVPD_MEM_BYTE_12;
+    Byte packageRanksPerDimmByteInSPD = SVPD_MEM_BYTE_12;
+    Byte dieCount = 1;
+
+    // NOTE: This calculation is Only for DDR4
+
+    // Calculate SDRAM  capacity
+    tmp = iterator[sdramCapacityByteInSPD] & SVPD_JEDEC_SDRAM_CAP_MASK;
+    /* Make sure the bits are not Reserved */
+    if (tmp > SVPD_JEDEC_SDRAMCAP_RESERVED)
+    {
+        rc = ERR_MEM_VPD_BYTE_4;
+        return rc;
+    }
+
+    sdram_cap = (sdram_cap << tmp) * SVPD_JEDEC_SDRAMCAP_MULTIPLIER;
+
+    /* Calculate Primary bus width */
+    tmp = iterator[primaryBusWidthByteInSPD] & SVPD_JEDEC_PRI_BUS_WIDTH_MASK;
+    if (tmp > SVPD_JEDEC_RESERVED_BITS)
+    {
+        rc = ERR_MEM_VPD_BYTE_13;
+        return rc;
+    }
+    pri_bus_wid = (pri_bus_wid << tmp) * SVPD_JEDEC_PRI_BUS_WIDTH_MULTIPLIER;
+
+    /* Calculate SDRAM width */
+    tmp = iterator[sdramDeviceWidthByteInSPD] & SVPD_JEDEC_SDRAM_WIDTH_MASK;
+    if (tmp > SVPD_JEDEC_RESERVED_BITS)
+    {
+        rc = ERR_MEM_VPD_BYTE_12;
+        return rc;
+    }
+    sdram_wid = (sdram_wid << tmp) * SVPD_JEDEC_SDRAM_WIDTH_MULTIPLIER;
+
+    tmp = iterator[SVPD_MEM_BYTE_6] & SVPD_JEDEC_SIGNAL_LOADING_MASK;
+
+    if (tmp == SVPD_JEDEC_SINGLE_LOAD_STACK)
+    {
+        // Fetch die count
+        tmp = iterator[SVPD_MEM_BYTE_6] & SVPD_JEDEC_DIE_COUNT_MASK;
+        tmp >>= SVPD_JEDEC_DIE_COUNT_RIGHT_SHIFT;
+        dieCount = tmp + 1;
+    }
+
+    /* Calculate Number of ranks */
+    tmp = iterator[packageRanksPerDimmByteInSPD] & SVPD_JEDEC_NUM_RANKS_MASK;
+    tmp >>= SVPD_JEDEC_RESERVED_BITS;
+
+    if (tmp > SVPD_JEDEC_RESERVED_BITS)
+    {
+        rc = ERR_PACKAGE_RANKS_MEM_VPD_BYTE_12;
+        return rc;
+    }
+    logical_ranks_per_dimm = (tmp + 1) * dieCount;
+
+    dimmSize = (sdram_cap / SVPD_JEDEC_PRI_BUS_WIDTH_MULTIPLIER) *
+               (pri_bus_wid / sdram_wid) * logical_ranks_per_dimm;
+
+    return rc;
+}
+
 kwdVpdMap memoryVpdParser::readKeywords(Binary::const_iterator iterator)
 {
     KeywordVpdMap map{};
 
+    size_t dimmSize = 0;
+
+    // collect Dimm size value
+    auto rc = getDimmSize(iterator, dimmSize);
+    if (rc)
+    {
+        throw runtime_error(
+            "Couldn't calculate the dimm size. One of the parameters has "
+            "reserved value in the VPD. Failed with reason code - " +
+            rc);
+    }
+
+    map.emplace("MemorySizeInKB", dimmSize);
+    // point the iterator to DIMM data and skip "11S"
+    advance(iterator, MEMORY_VPD_DATA_START + 3);
     Binary partNumber(iterator, iterator + PART_NUM_LEN);
 
     advance(iterator, PART_NUM_LEN);
@@ -40,9 +128,6 @@ variant<kwdVpdMap, Store> memoryVpdParser::parse()
 {
     // Read the data and return the map
     auto iterator = memVpd.cbegin();
-    // point the iterator to DIMM data and skip "11S"
-    advance(iterator, MEMORY_VPD_DATA_START + 3);
-
     auto vpdDataMap = readKeywords(iterator);
 
     return vpdDataMap;
--- vpd-parser/memory_vpd_parser.hpp
+++ vpd-parser/memory_vpd_parser.hpp
@@ -4,6 +4,8 @@
 #include "parser_interface.hpp"
 #include "types.hpp"
 
+#include <iomanip>
+
 namespace openpower
 {
 namespace vpd
@@ -14,6 +16,14 @@ namespace parser
 {
 using ParserInterface = openpower::vpd::parser::interface::ParserInterface;
 using kwdVpdMap = openpower::vpd::inventory::KeywordVpdMap;
+
+enum ErrorCodes
+{
+    ERR_MEM_VPD_BYTE_4 = -1,
+    ERR_MEM_VPD_BYTE_13 = -2,
+    ERR_MEM_VPD_BYTE_12 = -3,
+    ERR_PACKAGE_RANKS_MEM_VPD_BYTE_12 = -4
+}; // enum ErrorCodes
 
 class memoryVpdParser : public ParserInterface
 {
@@ -57,6 +67,18 @@ class memoryVpdParser : public ParserInterface
      * @return- map of kwd:value
      */
     kwdVpdMap readKeywords(Binary::const_iterator iterator);
+
+    /**
+     * @brief This function calculates dimm size from DIMM VPD
+     *
+     * @param[in] iterator - iterator to buffer containing VPD
+     * @param[in] errorByte - this will tell which BYTE in VPD is bad,
+     *                        and caused to return the error code.
+     *                        In case of Success, it will have 0.
+     *
+     * @return either calculated data or error code.
+     */
+    auto getDimmSize(Binary::const_iterator iterator, size_t& errorByte);
 
     // vdp file to be parsed
     const Binary& memVpd;
