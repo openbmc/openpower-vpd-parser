d510653007dc8bdb65321c554b19a21d94b12eae
Sunny Srivastava <sunnsr25@in.ibm.com>
2021-04-15 04:43:54 -0500

    OPenpower fix
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I95f2be27dc0c391a45beb1654a99506317aaa52b

--- utils.hpp
+++ utils.hpp
@@ -4,7 +4,6 @@
 #include "types.hpp"
 
 #include <iostream>
-#include <nlohmann/json.hpp>
 
 using namespace std;
 
@@ -12,56 +11,20 @@ namespace openpower
 {
 namespace vpd
 {
-/**
- * @brief Types of VPD
- */
-enum vpdType
-{
-    IPZ_VPD,           /**< IPZ VPD type */
-    KEYWORD_VPD,       /**< Keyword VPD type */
-    MEMORY_VPD,        /**< Memory VPD type */
-    INVALID_VPD_FORMAT /**< Invalid VPD type */
-};
-
-/**
- * @brief Check the type of VPD.
- *
- * Checks the type of vpd based on the start tag.
- * @param[in] vector - Vpd data in vector format
- *
- * @return enum of type vpdType
- */
-vpdType vpdTypeCheck(const Binary& vector);
 
 /** @brief Return the hex representation of the incoming byte
  *
  * @param [in] c - The input byte
  * @returns The hex representation of the byte as a character.
  */
-constexpr auto toHex(size_t c)
+/*constexpr auto toHex(size_t c)
 {
     constexpr auto map = "0123456789abcdef";
     return map[c];
-}
+}*/
 
 namespace inventory
 {
-
-/** @brief Api to Get d-bus service for given interface
- *  @param[in] - Bus object
- *  @param[in] - object path of the service
- *  @param[in] - interface under the object path
- *  @return service name
- */
-std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
-                       const std::string& interface);
-
-/** @brief Call inventory-manager to add objects
- *
- *  @param [in] objects - Map of inventory object paths
- */
-void callPIM(ObjectMap&& objects);
-
 /** @brief Api to obtain a dictionary of path -> services
  * where path is in subtree and services is of the type
  * returned by the GetObject method.
@@ -72,9 +35,10 @@ void callPIM(ObjectMap&& objects);
  * result is required.
  * @return A dictionary of Path -> services
  */
-MapperResponse
-    getObjectSubtreeForInterfaces(const std::string& root, const int32_t depth,
-                                  const std::vector<std::string>& interfaces);
+// MapperResponse
+//    getObjectSubtreeForInterfaces(const std::string& root, const int32_t
+//    depth,
+//                                  const std::vector<std::string>& interfaces);
 
 } // namespace inventory
 
@@ -82,14 +46,14 @@ MapperResponse
  * @param[in] iterator- Pointer pointing to the data to be read
  * @return returns 2 Byte data read at the given pointer
  */
-openpower::vpd::constants::LE2ByteData
-    readUInt16LE(Binary::const_iterator iterator);
+// openpower::vpd::constants::LE2ByteData
+//    readUInt16LE(Binary::const_iterator iterator);
 
 /** @brief Encodes a keyword for D-Bus.
  *  @param[in] kw - kwd data in string format
  *  @param[in] encoding - required for kwd data
  */
-string encodeKeyword(const string& kw, const string& encoding);
+// string encodeKeyword(const string& kw, const string& encoding);
 
 /** @brief Reads a property from the inventory manager given object path,
  *         intreface and property.
@@ -98,16 +62,16 @@ string encodeKeyword(const string& kw, const string& encoding);
  *  @param[in] prop - property whose value is fetched
  *  @return [out] - value of the property
  */
-string readBusProperty(const string& obj, const string& inf,
-                       const string& prop);
+// string readBusProperty(const string& obj, const string& inf,
+//                       const string& prop);
 
 /**
  * @brief API to create PEL entry
  * @param[in] Map holding the additional data
  * @param[in] error interface
  */
-void createPEL(const std::map<std::string, std::string>& additionalData,
-               const std::string& errIntf);
+// void createPEL(const std::map<std::string, std::string>& additionalData,
+//               const std::string& errIntf);
 
 /**
  * @brief getVpdFilePath
@@ -116,8 +80,8 @@ void createPEL(const std::map<std::string, std::string>& additionalData,
  * @param[in] - Object path
  * @return - Vpd file path
  */
-inventory::VPDfilepath getVpdFilePath(const string& jsonFile,
-                                      const std::string& ObjPath);
+// inventory::VPDfilepath getVpdFilePath(const string& jsonFile,
+//                                      const std::string& ObjPath);
 
 /**
  * @brief isPathInJson
@@ -125,7 +89,7 @@ inventory::VPDfilepath getVpdFilePath(const string& jsonFile,
  * @param[in] - eepromPath
  * @return - true if the eeprom is present in the json; false otherwise
  */
-bool isPathInJson(const std::string& eepromPath);
+// bool isPathInJson(const std::string& eepromPath);
 
 /**
  * @brief isRecKwInDbusJson
@@ -137,7 +101,8 @@ bool isPathInJson(const std::string& eepromPath);
  * @return - true if the record-keyword pair is present in dbus_property.json;
  * false otherwise.
  */
-bool isRecKwInDbusJson(const std::string& record, const std::string& keyword);
+// bool isRecKwInDbusJson(const std::string& record, const std::string&
+// keyword);
 
 } // namespace vpd
 } // namespace openpower
