c8b166f13fcf94513ad713ab2d6573f389968670
Alpana Kumari <alpankum@in.ibm.com>
2021-09-16 00:29:12 -0500

    CM: Bind the PCIe devices with LED driver
    bind the LED driver to the device if vpd collection done successfully
    on adding the FRU to the system concurrently and unbind if FRU removed.
    Inventory Json change- https://github.ibm.com/openbmc/openbmc/pull/1424
    
    Test:
    Tested on Rainier
    
    root@rain104bmc:~# busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager  CollectFRUVPD  o  "/system/chassis/motherboard/pcieslot10/pcie_card10"
    
    root@rain104bmc:~# busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager  deleteFRUVPD  o  "/system/chassis/motherboard/pcieslot10/pcie_card10"
    
    Sep 27 03:49:49 rain104bmc vpd-manager[781]: Binding device- echo "11-0060" > /sys/bus/i2c/drivers/leds-pca955x/bind
    Sep 27 03:49:50 rain104bmc kernel: at24 11-0050: 8192 byte 24c64 EEPROM, writable, 1 bytes/write
    Sep 27 03:49:50 rain104bmc systemd[1]: Started IPZ format VPD Parser service for FRU sys/devices/platform/ahb/ahb:apb/ahb:apb:bus@1e78a000/1e78a600.i2c-bus/i2c-11/11-0050/11-00508.
    Sep 27 03:49:50 rain104bmc ibm-vpd-parser[836]: Parser launched with file: /sys/devices/platform/ahb/ahb:apb/ahb:apb:bus@1e78a000/1e78a600.i2c-bus/i2c-11/11-0050/11-00508/nvmem
    Sep 27 03:49:50 rain104bmc ibm-vpd-parser[836]: Path after translation: /sys/bus/i2c/drivers/at24/11-0050/eeprom
    
    Sep 27 03:53:00 rain104bmc vpd-manager[781]: Unbinding device- echo "11-0050" > /sys/bus/i2c/drivers/leds-pca9551/unbind
    
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>
    Change-Id: I1d886d31e0e36c21d8b739c4b3d3e054ec876b6d

--- ibm_vpd_utils.hpp
+++ ibm_vpd_utils.hpp
@@ -290,7 +290,7 @@ bool executePreAction(const nlohmann::json& json, const string& file);
 
 /**
  * @brief This API will be called at the end of VPD collection to perform any
- * post actions.
+ *        post actions.
  *
  * @param[in] json - json object
  * @param[in] file - eeprom file path
@@ -314,6 +314,14 @@ void executePostFailAction(const nlohmann::json& json, const string& file);
 void insertOrMerge(inventory::InterfaceMap& map,
                    const inventory::Interface& interface,
                    inventory::PropertyMap&& property);
+
+/** @brief This API will be called at the end of VPD collection to perform
+ *         success post actions.
+ *
+ * @param[in] json - json object
+ * @param[in] file - eeprom file path
+ */
+void executePostSuccessAction(const nlohmann::json& json, const string& file);
 
 } // namespace vpd
 } // namespace openpower
