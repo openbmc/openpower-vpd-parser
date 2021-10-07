bf1245afceec4e010e4fb1e577fb24f98459f8a1
Priyanga Ramasamy <priyanga24@in.ibm.com>
2021-10-07 16:30:51 -0500

    Api to read a keyword from vpd
    This commit adds up a new method in Impl class
    to read a given keyword directly from vpd.
    
    Vpd-tool is one such user of this method.
    
    Signed-off-by: Priyanga Ramasamy <priyanga24@in.ibm.com>
    Change-Id: I2bb3d3522a958d0970b20c078b7132ace7ebf692

--- impl.hpp
+++ impl.hpp
@@ -82,6 +82,16 @@ class Impl
      */
     void checkVPDHeader();
 
+    /** @brief Read a specific keyword from hardware.
+     * This api is used by the vpd-tool to read a specific keyword directly from
+     * the vpd.
+     * @param[in] record - record name.
+     * @param[in] keyword - keyword name.
+     * @return keyword value.
+     */
+    std::string readKwFromHw(const std::string& record,
+                             const std::string& keyword);
+
   private:
     /** @brief Process the table of contents record
      *
