286c109571bd0d1d43a75930f8c84331644ffe54
Priyanga Ramasamy <priyanga24@in.ibm.com>
2022-02-01 11:58:42 +0000

    Replace std::emplace with std::insert_or_assign
    With std::emplace, the code logic behaves like all values of inventory
    interfaces and inventory objects should be inserted together at once,
    as the std::emplace doesn't allow adding elements to the already
    existing key.
    
    This commit replaces std::emplace with std::insert_or_assign which will
    allow the developer to insert any values of inventory interfaces and
    inventory objects at any point of execution(variable being within the
    scope), even if the key already exist.
    
    Thus even if the developer tries to insert a new value to an existing
    key, the new value gets updated in the objects map.
    
    Signed-off-by: Priyanga Ramasamy <priyanga24@in.ibm.com>
    Change-Id: I1e2ac0e0867543a6d37470ef66d30273a1880489

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -189,7 +189,7 @@ static void populateFruSpecificInterfaces(const T& map,
                     {
                         memProp.emplace(move(kw),
                                         ((*memVal) * CONVERT_MB_TO_KB));
-                        interfaces.emplace(
+                        interfaces.insert_or_assign(
                             "xyz.openbmc_project.Inventory.Item.Dimm",
                             move(memProp));
                     }
@@ -207,7 +207,7 @@ static void populateFruSpecificInterfaces(const T& map,
         }
     }
 
-    interfaces.emplace(preIntrStr, move(prop));
+    interfaces.insert_or_assign(preIntrStr, move(prop));
 }
 
 /**
@@ -248,7 +248,10 @@ static void populateInterfaces(const nlohmann::json& js,
                         auto prop = expandLocationCode(
                             itr.value().get<string>(), vpdMap, isSystemVpd);
                         props.emplace(busProp, prop);
-                        interfaces.emplace(XYZ_LOCATION_CODE_INF, props);
+                        interfaces.insert_or_assign(XYZ_LOCATION_CODE_INF,
+                                                    props);
+                        interfaces.insert_or_assign(IBM_LOCATION_CODE_INF,
+                                                    props);
                     }
                     else
                     {
@@ -503,7 +506,7 @@ static void setOneTimeProperties(const std::string& object,
         // Treat as property unavailable
         inventory::PropertyMap prop;
         prop.emplace("Functional", true);
-        interfaces.emplace(
+        interfaces.insert_or_assign(
             "xyz.openbmc_project.State.Decorator.OperationalStatus",
             move(prop));
     }
@@ -521,7 +524,8 @@ static void setOneTimeProperties(const std::string& object,
         // Treat as property unavailable
         inventory::PropertyMap prop;
         prop.emplace("Enabled", false);
-        interfaces.emplace("xyz.openbmc_project.Object.Enable", move(prop));
+        interfaces.insert_or_assign("xyz.openbmc_project.Object.Enable",
+                                    move(prop));
     }
 }
 
@@ -556,8 +560,8 @@ inventory::ObjectMap primeInventory(const nlohmann::json& jsObject,
             {
                 inventory::PropertyMap presProp;
                 presProp.emplace("Present", false);
-                interfaces.emplace("xyz.openbmc_project.Inventory.Item",
-                                   presProp);
+                interfaces.insert_or_assign(
+                    "xyz.openbmc_project.Inventory.Item", presProp);
                 setOneTimeProperties(object, interfaces);
                 if (itemEEPROM.find("extraInterfaces") != itemEEPROM.end())
                 {
@@ -575,17 +579,18 @@ inventory::ObjectMap primeInventory(const nlohmann::json& jsObject,
 
                                     props.emplace(move(lC.key()),
                                                   move(propVal));
-                                    interfaces.emplace(XYZ_LOCATION_CODE_INF,
-                                                       props);
-                                    interfaces.emplace(move(eI.key()),
-                                                       move(props));
+                                    interfaces.insert_or_assign(
+                                        XYZ_LOCATION_CODE_INF, props);
+                                    interfaces.insert_or_assign(move(eI.key()),
+                                                                move(props));
                                 }
                             }
                         }
                         else if (eI.key().find("Inventory.Item.") !=
                                  string::npos)
                         {
-                            interfaces.emplace(move(eI.key()), move(props));
+                            interfaces.insert_or_assign(move(eI.key()),
+                                                        move(props));
                         }
                         else if (eI.key() ==
                                  "xyz.openbmc_project.Inventory.Item")
@@ -606,7 +611,7 @@ inventory::ObjectMap primeInventory(const nlohmann::json& jsObject,
                         }
                     }
                 }
-                objects.emplace(move(object), move(interfaces));
+                objects.insert_or_assign(move(object), move(interfaces));
             }
         }
     }
@@ -1097,7 +1102,7 @@ static void populateDbus(T& vpdMap, nlohmann::json& js, const string& filePath)
                                    isSystemVpd);
             }
         }
-        objects.emplace(move(object), move(interfaces));
+        objects.insert_or_assign(move(object), move(interfaces));
     }
 
     if (isSystemVpd)
