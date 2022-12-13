80ea1b99f0e884eabbf0ce49e42e027db462ff59
Kantesh Nagaradder <kantesh.nagaradder@ibm.com>
2022-12-13 21:07:00 +0530

    Addressed warnings reported by CI test run
    To make sure no warnings and to avoid unexpected behavior due to those warnings.
    
    Below are the list of warnings addressed as part of this commit
    1)noExplicitConstructor
    2)missingOverride
    
    Signed-off-by: Kantesh Nagaradder <kantesh.nagaradder@ibm.com>
    Change-Id: I3d4a768e4f9ae224ec14bef8068f013a3b6243e6

--- vpd-parser/memory_vpd_parser.hpp
+++ vpd-parser/memory_vpd_parser.hpp
@@ -30,7 +30,7 @@ class memoryVpdParser : public ParserInterface
      *
      * Move memVpdVector to parser object's memVpdVector
      */
-    memoryVpdParser(const Binary& VpdVector) : memVpd(VpdVector)
+    explicit memoryVpdParser(const Binary& VpdVector) : memVpd(VpdVector)
     {
     }
 
@@ -40,7 +40,7 @@ class memoryVpdParser : public ParserInterface
      *
      * @return map of keyword:value
      */
-    std::variant<kwdVpdMap, Store> parse();
+    std::variant<kwdVpdMap, Store> parse() override;
 
     /**
      * @brief An api to return interface name with respect to
@@ -48,7 +48,7 @@ class memoryVpdParser : public ParserInterface
      *
      * @return - Interface name for that vpd type.
      */
-    std::string getInterfaceName() const;
+    std::string getInterfaceName() const override;
 
   private:
     /**
