02b28165634bfb4b1c30e77227df76964da4d7a0
Priyanga Ramasamy <priyanga24@in.ibm.com>
2023-01-05 04:54:59 -0600

    Rewordings required in logging PEL
    This commit rewords some of the comments and variables
    in restore VPD PEL code and in vpd-tool restore function.
    
    Eg:
    >>Cache value -> Backup value
        [As backup need not be in cache only]
    >>Blank data -> Default data
        [As default data need not be blank only]
    The VPD backup values and backup memory are machine specific.
    
    Thus providing generic terms makes the code statements/comments
    more reliable w.r.t any machine.
    
    Signed-off-by: Priyanga Ramasamy <priyanga24@in.ibm.com>
    Change-Id: I34fcd44e94b928db0fc30cda94eb8f03e6e87987

--- ibm_vpd_app.cpp
+++ ibm_vpd_app.cpp
@@ -877,8 +877,8 @@ void restoreSystemVPD(Parsed& vpdMap, const string& objectPath)
                             // both the data are present, check for mismatch
                             if (busValue != kwdValue)
                             {
-                                string errMsg = "VPD data mismatch on cache "
-                                                "and hardware for record: ";
+                                string errMsg = "Mismatch found between backup "
+                                                "and primary VPD for record: ";
                                 errMsg += (*it).first;
                                 errMsg += " and keyword: ";
                                 errMsg += keyword;
@@ -906,23 +906,23 @@ void restoreSystemVPD(Parsed& vpdMap, const string& objectPath)
                                                            objectPath);
 
                                 additionalData.emplace("DESCRIPTION", errMsg);
-                                additionalData.emplace("Value on Cache: ",
-                                                       busStream.str());
+                                additionalData.emplace(
+                                    "Value read from Backup: ",
+                                    busStream.str());
                                 additionalData.emplace(
                                     "Value read from EEPROM: ",
                                     vpdStream.str());
 
                                 createPEL(additionalData, PelSeverity::WARNING,
-                                          errIntfForSysVPDMismatch, nullptr);
+                                          errIntfForVPDMismatch, nullptr);
                             }
                         }
 
-                        // If cache data is not blank, then irrespective of
-                        // hardware data(blank or other than cache), copy the
-                        // cache data to vpd map as we don't need to change the
-                        // cache data in either case in the process of
+                        // If backup data is not default, then irrespective of
+                        // hardware data(default or other than backup), copy the
+                        // backup data to vpd map as we don't need to change the
+                        // backup data in either case in the process of
                         // restoring system vpd.
-                        Binary busData(busValue.begin(), busValue.end());
                         kwdValue = busValue;
                     }
                     else if (kwdValue.find_first_not_of(defaultValue) ==
@@ -937,14 +937,14 @@ void restoreSystemVPD(Parsed& vpdMap, const string& objectPath)
                             // upgrading BMC to the minimum required version.
                             continue;
                         }
-                        string errMsg = "VPD is blank on both cache and "
-                                        "hardware for record: ";
+                        string errMsg = "Found default value on both backup "
+                                        "and primary VPD for record: ";
                         errMsg += (*it).first;
                         errMsg += " and keyword: ";
                         errMsg += keyword;
-                        errMsg += ". SSR need to update hardware VPD.";
+                        errMsg += ". SSR need to update primary VPD.";
 
-                        // both the data are blanks, log PEL
+                        // mfg default on both backup and hardware, log PEL
                         PelAdditionalData additionalData;
                         additionalData.emplace("CALLOUT_INVENTORY_PATH",
                                                INVENTORY_PATH + objectPath);
@@ -953,7 +953,7 @@ void restoreSystemVPD(Parsed& vpdMap, const string& objectPath)
 
                         // log PEL TODO: Block IPL
                         createPEL(additionalData, PelSeverity::ERROR,
-                                  errIntfForBlankSystemVPD, nullptr);
+                                  errIntfForVPDDefault, nullptr);
                         continue;
                     }
                 }
