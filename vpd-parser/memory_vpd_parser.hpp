9fca3a53560e6947296880ec924e78ca4b0e0c17
Kantesh Nagaradder <Kantesh.Nagaradder@ibm.com>
2023-02-14 23:22:45 -0600

    VPD:DDIMM DDR5 support for eBMC based systems
    The commit implement the changes to find the DDIMM DDR5 memory
    modules capacity as per the DDR5 SPD Spec.
    
    Change-Id: I61a4ad4c29bcc5511f56c7ab8448dfafdc32180d
    Signed-off-by: Kantesh Nagaradder <kantesh.nagaradder@ibm.com>

--- vpd-parser/memory_vpd_parser.hpp
+++ vpd-parser/memory_vpd_parser.hpp
@@ -68,6 +68,22 @@ class memoryVpdParser : public ParserInterface
 
     // vdp file to be parsed
     const Binary& memVpd;
+
+    /**
+     * @brief This function calculates DDR5 dimm size from DIMM VPD
+     *
+     * @param[in] iterator - iterator to buffer containing VPD
+     * @return calculated data or 0 in case of any error.
+     */
+    auto getDDR5DimmSize(Binary::const_iterator& iterator);
+
+    /**
+     * @brief This API calls the relevant APIs based on SDRAM type
+     *
+     * @param[in] iterator - iterator to buffer containing VPD
+     * @return dimm size.
+     */
+    auto getDDRXDimmSize(Binary::const_iterator& iterator);
 };
 } // namespace parser
 } // namespace memory
