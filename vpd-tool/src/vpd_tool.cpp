97712702031e4de4b2a8be715313ba466378cea6
Anupama B R <anupama.b.r1@ibm.com>
2025-01-23 02:59:27 -0600

    vpd-tool force reset command
    This commit implements showing force reset command to user in vpd-tool.
    The command clears inventory cache data and restarts the VPD manager
    service which collects the FRU’s VPD listed in the system config JSON.
    
    Command will only be processed if chassis is powered off.
    
    The stub API is added in VpdTool class to implement this command.
    
    Output:
    ```
    When chassis is powered off:
    root@p10bmc:/tmp# ./vpd-tool -f
    The chassis power state is not Off. Force reset operation is not allowed.
    ```
    
    Change-Id: I58ed0f11cf182d718ae957c4fbe803dc24f83d09
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-tool/src/vpd_tool.cpp
+++ vpd-tool/src/vpd_tool.cpp
@@ -1210,4 +1210,10 @@ int VpdTool::handleMoreOption(
     return l_rc;
 }
 
+int VpdTool::forceReset()
+{
+    // ToDo: Implementation needs to be added
+    return constants::SUCCESS;
+}
+
 } // namespace vpd
