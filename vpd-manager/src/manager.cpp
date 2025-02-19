4cf3acb3d4ab4e584bf5e3fd27a659fb6766ad22
Sunny Srivastava <sunnsr25@in.ibm.com>
2025-02-20 00:01:02 +0530

    Check and update powerVS VPD
    The commit adds implementation to check for existing VPD in the system
    and if it is different from the VPD required in case of poewrVS
    configuration, it updates it with the powerVS VPD.
    
    Change-Id: I6092531936bf12d7a19df6acdf4dc73eb962c60e
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

--- vpd-manager/src/manager.cpp
+++ vpd-manager/src/manager.cpp
@@ -327,9 +327,61 @@ void Manager::checkAndUpdatePowerVsVpd(
     const nlohmann::json& i_powerVsJsonObj,
     std::vector<std::string>& io_failedPathList)
 {
-    (void)i_powerVsJsonObj;
-    (void)io_failedPathList;
-    // TODO: Check and update powerVS VPD
+    for (const auto& [l_fruPath, l_recJson] : i_powerVsJsonObj.items())
+    {
+        // TODO add special handling for PROC CCIN check.
+        for (const auto& [l_recordName, l_kwdJson] : l_recJson.items())
+        {
+            for (const auto& [l_kwdName, l_kwdValue] : l_kwdJson.items())
+            {
+                // Is value of type array.
+                if (!l_kwdValue.is_array())
+                {
+                    io_failedPathList.push_back(l_fruPath);
+                    continue;
+                }
+
+                auto l_inventoryPath =
+                    jsonUtility::getInventoryObjPathFromJson(l_fruPath);
+
+                // Mark it as failed if inventory path not found in JSON.
+                if (l_inventoryPath.empty())
+                {
+                    io_failedPathList.push_back(l_fruPath);
+                    continue;
+                }
+
+                // Get current Part number.
+                auto l_retVal = dbusUtility::readDbusProperty(
+                    constants::pimServiceName, l_inventoryPath,
+                    constants::viniInf, constants::kwdPN);
+
+                auto l_ptrToPn = std::get_if<types::BinaryVector>(&l_retVal);
+
+                if (!l_ptrToPn)
+                {
+                    io_failedPathList.push_back(l_fruPath);
+                    continue;
+                }
+
+                types::BinaryVector l_binaryKwdValue =
+                    l_kwdValue.get<types::BinaryVector>();
+                if (l_binaryKwdValue == (*l_ptrToPn))
+                {
+                    continue;
+                }
+
+                // Update part number only if required.
+                if (updateKeyword(
+                        l_path,
+                        std::make_tuple(l_recordName, l_kwdName, l_kwdValue)) ==
+                    constants::FAILURE)
+                {
+                    io_failedPathList.push_back(l_path);
+                }
+            }
+        }
+    }
 }
 
 void Manager::ConfigurePowerVsSystem()
