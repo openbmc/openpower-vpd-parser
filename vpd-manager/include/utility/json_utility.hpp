08fa59eaaa6ece75330788ae17b3f78f54fd55b6
Anupama B R <anupama.b.r1@ibm.com>
2025-03-06 22:55:11 -0600

    Move single FAB API under the separate class
    SingleFab class provides functionalities to support single FAB feature.
    This commit moves the single FAB related API under SingleFab class.
    
    Change-Id: I8d55cd2134f5812f83f5fc5033df097ab1b1f8f2
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/include/utility/json_utility.hpp
+++ vpd-manager/include/utility/json_utility.hpp
@@ -1113,46 +1113,5 @@ inline nlohmann::json getPowerVsJson(const types::BinaryVector& i_imValue)
         return nlohmann::json{};
     }
 }
-
-/** @brief API to get IM value from persisted data location.
- *
- * @return IM value on success, empty string otherwise.
- */
-inline std::string getImFromPersistedLocation() noexcept
-{
-    std::string l_imFilePath{constants::pimPersistVsbpPath};
-
-    try
-    {
-        auto l_parsedVsbpJsonObj = getParsedJson(l_imFilePath);
-        if (!l_parsedVsbpJsonObj.contains("value0") ||
-            !l_parsedVsbpJsonObj["value0"].contains(constants::kwdIM) ||
-            !l_parsedVsbpJsonObj["value0"][constants::kwdIM].is_array())
-        {
-            throw std::runtime_error(
-                "Json is empty or mandatory tag(s) missing from JSON");
-        }
-
-        types::BinaryVector l_imValue =
-            l_parsedVsbpJsonObj["value0"][constants::kwdIM]
-                .get<types::BinaryVector>();
-
-        std::ostringstream l_imData;
-        for (auto& l_byte : l_imValue)
-        {
-            l_imData << std::setw(2) << std::setfill('0') << std::hex
-                     << static_cast<int>(l_byte);
-        }
-        return l_imData.str();
-    }
-    catch (const std::exception& l_ex)
-    {
-        logging::logMessage(
-            "Error while getting IM value from PIM persisted file: " +
-            l_imFilePath + ", reason: " + std::string(l_ex.what()));
-    }
-
-    return std::string();
-}
 } // namespace jsonUtility
 } // namespace vpd
