78567a838e1c7dbec3612e7dcf85b240d431c746
Sunny Srivastava <sunnsr25@in.ibm.com>
2025-04-15 12:24:23 +0530

    Add specific handler class
    The commit adds folder structure and specific handler class stub.
    It is required to encapsulate any specific handling based on OEM and
    keep it separate from the generic features offered by the repository.
    
    Change-Id: Idff4df024597f6087e70b7b7f10fc05b00b3f664
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

---
+++ vpd-manager/oem-handler/ibm_handler.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace vpd
+{
+/**
+ * @brief Class to handle specific use case.
+ *
+ * Few pre-requisites needs to be taken case specifically, which will be
+ * encapsulated by this class.
+ */
+class IbmHandler
+{
+  public:
+    /**
+     * List of deleted methods.
+     */
+    IbmHandler(const IbmHandler&) = delete;
+    IbmHandler& operator=(const IbmHandler&) = delete;
+    IbmHandler(IbmHandler&&) = delete;
+
+    /**
+     * @brief Constructor.
+     */
+    IbmHandler() {}
+};
+} // namespace vpd
