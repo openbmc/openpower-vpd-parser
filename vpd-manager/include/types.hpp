c4dec97a295219c2e13d9de89cf39a62cb0a2a83
Souvik Roy <souvikroyofficial10@gmail.com>
2025-04-16 02:22:33 -0500

    Log PEL and dump VPD if invalid record(s) found
    This commit adds changes in ipz parser to log predictive PEL if any
    invalid record(s) is found while parsing. Processing of records still
    continues even if any invalid record is found. At the end of parsing the
    VPD:
    1. A predictive PEL is logged which specifies FRU path and list of
       invalid records.
    2. The bad VPD binary data is dumped to filesystem so that it can be
       collected by FFDC.
    
    Test:
    ```
    1. Using dd command, zero out VINI DR and VMPU VZ  keyword values of Op
       Panel EEPROM(/sys/bus/i2c/drivers/at24/7-0051/eeprom).
    2. Restart vpd-manager service.
    3. Check PEL list using peltool and observe that a predictive PEL is
       logged with the Op Panel EEPROM path in the description and a list of
       bad record names [VINI,VMPU] in the UserData1 section.
    4. Check /var/lib/vpd/ directory and see dumps directory is created with
       a dump file which contains the Op Panel EEPROM binary data
    5. Restore the Op Panel EEPROM with backed up correct data
    6. Clear all PELs using peltool
    7. Restart vpd-manager
    8. Observe no new PELs logged for invalid records
    ```
    
    Change-Id: Iebb526bae3c467b1cd0a256c93d57a56fb87e9f6
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/types.hpp
+++ vpd-manager/include/types.hpp
@@ -197,5 +197,31 @@ using I2cBusCalloutData = std::tuple<std::string, std::string, std::string>;
 using ExceptionInfoVariant = std::variant<std::monostate, ErrorType, std::string>;
 /* Error info map of format <Error format, Value> */
 using ExceptionDataMap = std::map<std::string, ExceptionInfoVariant>;
+
+using InvalidRecordEntry = std::pair<Record,ErrorType>;
+using InvalidRecordList = std::vector<InvalidRecordEntry>;
+
+inline const std::string toString(const ErrorType& i_errorType) noexcept
+{
+    const std::unordered_map<ErrorType,std::string> l_errorTypeToStringMap{{InvalidVpdMessage,"Invalid VPD Message"},{EccCheckFailed,"ECC check failed"}};
+    const auto l_result = l_errorTypeToStringMap.find(i_errorType);
+    return (l_result != l_errorTypeToStringMap.end() ? l_result->second : "Invalid VPD");
+}
+
+inline const std::string toString(const InvalidRecordEntry& i_recordEntry) noexcept
+{
+    return std::string{"{" + i_recordEntry.first + "," + toString(i_recordEntry.second) + "}"};
+}
+
+inline const std::string toString(const InvalidRecordList& i_list) noexcept
+{
+    std::string l_result{"["};
+    for(const auto& l_entry : i_list)
+    {
+        l_result += toString(l_entry) + ",";
+    }
+    l_result += "]";
+    return l_result;
+}
 } // namespace types
 } // namespace vpd
