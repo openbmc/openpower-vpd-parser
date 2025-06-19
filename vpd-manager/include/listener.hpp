65cfbb034e79d06273af8bc1327b95f812f8aca4
Anupama B R <anupama.b.r1@ibm.com>
2025-06-19 12:54:44 -0500

    Moving AssetTag and HostState under Listener
    This commit moves the AssetTag and HostState change property signal and
    its callback implementation from ibm-handler to Listener class.
    AssetTag and HostState property change signal will be registered
    through Listener class object.
    
    Change-Id: I8679466283cbc70fc07121a0a57243572f80b483
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/include/listener.hpp
+++ vpd-manager/include/listener.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "worker.hpp"
+
 #include <sdbusplus/asio/object_server.hpp>
 
 #include <memory>
@@ -24,13 +26,42 @@ class Listener
 
     /**
      * @brief Constructor
-     *
+     * @param[in] i_worker - Reference to worker class object.
      * @param[in] i_asioConnection - Dbus Connection.
      */
     Listener(
+        const std::shared_ptr<Worker>& i_worker,
         const std::shared_ptr<sdbusplus::asio::connection>& i_asioConnection);
 
+    /**
+     * @brief API to register callback for Host state change.
+     *
+     */
+    void registerHostStateChangeCallback() noexcept;
+
+    /**
+     * @brief API to register callback for "AssetTag" property change.
+     */
+    void registerAssetTagChangeCallback() const noexcept;
+
   private:
+    /**
+     * @brief API to process host state change callback.
+     *
+     * @param[in] i_msg - Callback message.
+     */
+    void hostStateChangeCallBack(sdbusplus::message_t& i_msg) const noexcept;
+
+    /**
+     * @brief Callback API to be triggered on "AssetTag" property change.
+     *
+     * @param[in] i_msg - Callback message.
+     */
+    void assetTagChangeCallback(sdbusplus::message_t& i_msg) const noexcept;
+
+    // Shared pointer to worker class
+    const std::shared_ptr<Worker>& m_worker;
+
     // Shared pointer to bus connection.
     const std::shared_ptr<sdbusplus::asio::connection>& m_asioConnection;
 };
--- vpd-manager/src/listener.cpp
+++ vpd-manager/src/listener.cpp
@@ -1,9 +1,168 @@
 #include "listener.hpp"
 
+#include "event_logger.hpp"
+#include "utility/dbus_utility.hpp"
+
 namespace vpd
 {
 Listener::Listener(
+    const std::shared_ptr<Worker>& i_worker,
     const std::shared_ptr<sdbusplus::asio::connection>& i_asioConnection) :
-    m_asioConnection(i_asioConnection)
+    m_worker(i_worker), m_asioConnection(i_asioConnection)
 {}
+
+void Listener::registerHostStateChangeCallback() noexcept
+{
+    try
+    {
+        static std::shared_ptr<sdbusplus::bus::match_t> l_hostState =
+            std::make_shared<sdbusplus::bus::match_t>(
+                *m_asioConnection,
+                sdbusplus::bus::match::rules::propertiesChanged(
+                    constants::hostObjectPath, constants::hostInterface),
+                [this](sdbusplus::message_t& i_msg) {
+                    hostStateChangeCallBack(i_msg);
+                });
+    }
+    catch (const std::exception& l_ex)
+    {
+        logging::logMessage(l_ex.what());
+    }
+}
+
+void Listener::hostStateChangeCallBack(
+    sdbusplus::message_t& i_msg) const noexcept
+{
+    try
+    {
+        if (i_msg.is_method_error())
+        {
+            throw std::runtime_error(
+                "Error reading callback message for host state");
+        }
+
+        std::string l_objectPath;
+        types::PropertyMap l_propMap;
+        i_msg.read(l_objectPath, l_propMap);
+
+        const auto l_itr = l_propMap.find("CurrentHostState");
+
+        if (l_itr == l_propMap.end())
+        {
+            // CurrentHostState is not found in the callback message
+            return;
+        }
+
+        if (auto l_hostState = std::get_if<std::string>(&(l_itr->second)))
+        {
+            // implies system is moving from standby to power on state
+            if (*l_hostState == "xyz.openbmc_project.State.Host.HostState."
+                                "TransitioningToRunning")
+            {
+                // TODO: check for all the essential FRUs in the system.
+
+                if (m_worker.get() != nullptr)
+                {
+                    // Perform recollection.
+                    m_worker->performVpdRecollection();
+                }
+                else
+                {
+                    logging::logMessage(
+                        "Failed to get worker object, Abort re-collection");
+                }
+            }
+        }
+        else
+        {
+            throw std::runtime_error(
+                "Invalid type recieved in variant for host state.");
+        }
+    }
+    catch (const std::exception& l_ex)
+    {
+        logging::logMessage(l_ex.what());
+
+        EventLogger::createSyncPel(
+            EventLogger::getErrorType(l_ex), types::SeverityType::Informational,
+            __FILE__, __FUNCTION__, 0,
+            "Host state change callback update failed, reason: " +
+                std::string(l_ex.what()),
+            std::nullopt, std::nullopt, std::nullopt, std::nullopt);
+    }
+}
+
+void Listener::registerAssetTagChangeCallback() const noexcept
+{
+    try
+    {
+        static std::shared_ptr<sdbusplus::bus::match_t> l_assetMatch =
+            std::make_shared<sdbusplus::bus::match_t>(
+                *m_asioConnection,
+                sdbusplus::bus::match::rules::propertiesChanged(
+                    constants::systemInvPath, constants::assetTagInf),
+                [this](sdbusplus::message_t& l_msg) {
+                    assetTagChangeCallback(l_msg);
+                });
+    }
+    catch (const std::exception& l_ex)
+    {
+        logging::logMessage(l_ex.what());
+    }
+}
+
+void Listener::assetTagChangeCallback(
+    sdbusplus::message_t& i_msg) const noexcept
+{
+    try
+    {
+        if (i_msg.is_method_error())
+        {
+            throw std::runtime_error(
+                "Error reading callback msg for asset tag.");
+        }
+
+        std::string l_objectPath;
+        types::PropertyMap l_propMap;
+        i_msg.read(l_objectPath, l_propMap);
+
+        const auto& l_itrToAssetTag = l_propMap.find("AssetTag");
+        if (l_itrToAssetTag != l_propMap.end())
+        {
+            if (auto l_assetTag =
+                    std::get_if<std::string>(&(l_itrToAssetTag->second)))
+            {
+                // Call Notify to persist the AssetTag
+                types::ObjectMap l_objectMap = {
+                    {sdbusplus::message::object_path(constants::systemInvPath),
+                     {{constants::assetTagInf, {{"AssetTag", *l_assetTag}}}}}};
+
+                // Notify PIM
+                if (!dbusUtility::callPIM(move(l_objectMap)))
+                {
+                    throw std::runtime_error(
+                        "Call to PIM failed for asset tag update.");
+                }
+            }
+        }
+        else
+        {
+            throw std::runtime_error(
+                "Could not find asset tag in callback message.");
+        }
+    }
+    catch (const std::exception& l_ex)
+    {
+        logging::logMessage("Asset tag callback update failed with error: " +
+                            std::string(l_ex.what()));
+
+        EventLogger::createSyncPel(
+            EventLogger::getErrorType(l_ex), types::SeverityType::Informational,
+            __FILE__, __FUNCTION__, 0,
+            "AssetTag change callback update failed, reason: " +
+                std::string(l_ex.what()),
+            std::nullopt, std::nullopt, std::nullopt, std::nullopt);
+    }
+}
+
 } // namespace vpd
