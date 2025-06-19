585b64931152c3883b480b5becb70847c1878fcf
Anupama B R <anupama.b.r1@ibm.com>
2025-06-19 02:08:39 -0500

    Stub for Listener class
    This commit has stub for Listener class, which can be used to register
    the events and handle the callback functionality for those events.
    This commit only adds stub class for Listener.
    
    Change-Id: I8731abc212239823e7062e3ee23f46a150b942a8
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

---
+++ vpd-manager/include/listener.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <sdbusplus/asio/object_server.hpp>
+
+namespace vpd
+{
+/**
+ * @brief Class to listen on events
+ *
+ * This class used for registering and handling events on the system.
+ */
+class Listener
+{
+  public:
+    /**
+     * Deleted methods for Listener
+     */
+    Listener(const Listener&) = delete;
+    Listener& operator=(const Listener&) = delete;
+    Listener& operator=(Listener&&) = delete;
+    Listener(Listener&&) = delete;
+
+    /**
+     * @brief Constructor
+     *
+     * @param[in] i_asioConnection - Dbus Connection.
+     */
+    Listener(
+        const std::shared_ptr<sdbusplus::asio::connection>& i_asioConnection);
+
+  private:
+    // Shared pointer to bus connection.
+    const std::shared_ptr<sdbusplus::asio::connection>& m_asioConnection;
+};
+} // namespace vpd
