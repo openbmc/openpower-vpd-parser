ca276af49e5b756659e964f82a68ca93af9c0c01
Souvik Roy <souvikroyofficial10@gmail.com>
2025-06-20 00:23:17 -0500

    Listener : Register callback API stub
    This commit implements a stub API in Listener class to register callback
    for all interfaces in correlated_properties.json. This commit also
    implements a stub callback API which will be triggered whenever a
    property change signal is detected.
    
    Change-Id: Ifdaa2310b397ae9616d921ba2ee657f61dce753e
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/listener.cpp
+++ vpd-manager/src/listener.cpp
@@ -6,4 +6,39 @@ Listener::Listener(
     const std::shared_ptr<sdbusplus::asio::connection>& i_asioConnection) :
     m_asioConnection(i_asioConnection)
 {}
+
+bool Listener::registerCallBack() const noexcept
+{
+    bool l_rc{true};
+    try
+    {
+        /* TODO:
+        Parse correlated_properties JSON, and register callback for all
+        interfaces */
+    }
+    catch (const std::exception& l_ex)
+    {
+        l_rc = false;
+        logging::logMessage(
+            "Failed to register call back for correlated interfaces. Error : " +
+            std::string(l_ex.what()));
+    }
+
+    return l_rc;
+}
+
+void Listener::propertiesChangedCallBack(
+    const sdbusplus::message_t& i_msg) const noexcept
+{
+    if (i_msg.is_method_error())
+    {
+        logging::logMessage("Error in reading property change signal.");
+        return;
+    }
+
+    /*TODO:
+    1. Extract interface, object path and property name from the message
+    2. Use correlated JSON to find target {object path, interface,
+    property/properties} to update*/
+}
 } // namespace vpd
