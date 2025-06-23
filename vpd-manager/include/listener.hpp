3f464b5f706b25a638dd89d40334afde7e2a0407
Souvik Roy <souvikroyofficial10@gmail.com>
2025-06-23 04:44:31 -0500

    Listener: Parse correlated properties JSON
    This commit adds changes in Listener class to parse correlated
    properties JSON file. Listener class needs to have the parsed correlated
    properties JSON in memory to listen for property changes on specific
    object path, interface and update correlated property/properties.
    
    Change-Id: I8f6881fc106edf995814fa16218f680757be079f
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/listener.hpp
+++ vpd-manager/include/listener.hpp
@@ -3,6 +3,7 @@
 #include "constants.hpp"
 #include "worker.hpp"
 
+#include <nlohmann/json.hpp>
 #include <sdbusplus/asio/object_server.hpp>
 
 #include <memory>
@@ -96,6 +97,9 @@ class Listener
 
     // Shared pointer to bus connection.
     const std::shared_ptr<sdbusplus::asio::connection>& m_asioConnection;
+
+    // Parsed correlated properties JSON.
+    nlohmann::json m_correlatedPropJson{};
 
     /**
      * @brief API which is called when correlated property change is detected
