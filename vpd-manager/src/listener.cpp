4550de891e6a533b17edb4c19ec9efc39f6c607f
Souvik Roy <souvikroyofficial10@gmail.com>
2025-06-25 03:39:38 -0500

    Listener:register correlated properties callback
    This commit implements API to register callback for all interfaces in
    correlated_properties.json.
    
    ```
    Test:
    1. Patch debug traces in the callback function.
    2. On rainier 2s2u simics, do a busctl set-property on Decorator.Asset
       PartNumber, com.ibm.ipzvpd.VINI PN of LCD Op Panel.
    3. Observe vpd-manager logs and observe traces indicating callback is
       getting triggered for above property changes.
    ```
    
    Change-Id: I1ca2ae65724d8aa27424775fb1290a4139518644
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/listener.cpp
+++ vpd-manager/src/listener.cpp
@@ -13,7 +13,7 @@ Listener::Listener(
 {}
 
 void Listener::registerAllCorrPropCallBack(
-    [[maybe_unused]] const std::string& i_correlatedPropJsonPath) noexcept
+    const std::string& i_correlatedPropJsonPath) noexcept
 {
     try
     {
@@ -24,9 +24,30 @@ void Listener::registerAllCorrPropCallBack(
             throw JsonException("Failed to parse correlated properties JSON",
                                 i_correlatedPropJsonPath);
         }
-        /* TODO:
-        Parse correlated_properties JSON, and register callback for all
-        interfaces under all services */
+
+        const nlohmann::json& l_serviceObj =
+            m_correlatedPropJson.get_ref<const nlohmann::json::object_t&>();
+
+        // Iterate through all services in the correlated properties json
+        for (const auto& l_service : l_serviceObj.items())
+        {
+            const auto& l_serviceName = l_service.key();
+
+            const nlohmann::json& l_invServiceObj =
+                m_correlatedPropJson[l_serviceName]
+                    .get_ref<const nlohmann::json::object_t&>();
+
+            // register properties changed D-Bus signal callback
+            // for all interfaces under this service.
+            std::for_each(l_invServiceObj.items().begin(),
+                          l_invServiceObj.items().end(),
+                          [this, &l_serviceName = std::as_const(l_serviceName)](
+                              const auto& i_invServiceObj) {
+                              registerCorrPropCallBack(
+                                  l_serviceName, i_invServiceObj.key(),
+                                  correlatedPropChangedCallBack);
+                          });
+        } // service loop
     }
     catch (const std::exception& l_ex)
     {
@@ -38,17 +59,22 @@ void Listener::registerAllCorrPropCallBack(
 }
 
 void Listener::registerCorrPropCallBack(
-    [[maybe_unused]] const std::string& i_service,
-    [[maybe_unused]] const std::string& i_interface,
-    [[maybe_unused]] std::function<void(sdbusplus::message_t& i_msg)>
-        i_callBackFunction)
+    const std::string& i_service, const std::string& i_interface,
+    std::function<void(sdbusplus::message_t& i_msg)> i_callBackFunction)
 {
     try
     {
-        /*TODO:
-        Create match object based on service name, interface and callback
-        function.
-        */
+        types::MatchObjectPtr l_matchObj =
+            std::make_unique<sdbusplus::bus::match::match>(
+                static_cast<sdbusplus::bus_t&>(*m_asioConnection),
+                "type='signal',member='PropertiesChanged',"
+                "interface='org.freedesktop.DBus.Properties',"
+                "arg0='" +
+                    i_interface + "'",
+                i_callBackFunction);
+
+        // save the match object in map
+        m_matchObjectMap[i_service][i_interface] = l_matchObj;
     }
     catch (const std::exception& l_ex)
     {
@@ -64,12 +90,18 @@ void Listener::correlatedPropChangedCallBack(
         if (i_msg.is_method_error())
         {
             throw DbusException("Error in reading property change signal.");
-            return;
         }
 
+        const std::string l_interface{i_msg.get_interface()};
+        const std::string l_objectPath{i_msg.get_path()};
+        const std::string l_signature{i_msg.get_signature()};
+
+        (void)l_interface;
+        (void)l_objectPath;
+        (void)l_signature;
+
         /*TODO:
-        1. Extract interface, object path and property name from the message
-        2. Use correlated JSON to find target {object path, interface,
+        Use correlated JSON to find target {object path, interface,
         property/properties} to update*/
     }
     catch (const std::exception& l_ex)
