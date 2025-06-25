184ca174b1fd6581f565f9cb1e47732dccc459d7
Souvik Roy <souvikroyofficial10@gmail.com>
2025-06-25 03:39:38 -0500

    Listener: Register correlated properties callback
    This commit implements API to register callback for all interfaces in
    correlated_properties.json.
    
    ```
    Test:
    1. Patch debug traces in the callback function.
    2. On rainier 2s2u simics, do a busctl set-property on Decorator.Asset
       PartNumber, com.ibm.ipzvpd.VINI PN of LCD Op Panel.
    3. Observe vpd-manager logs and observe traces indicating callback is
       getting triggered for above property changes.
    ```
    
    Change-Id: I1ca2ae65724d8aa27424775fb1290a4139518644
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/listener.hpp
+++ vpd-manager/include/listener.hpp
@@ -64,15 +64,17 @@ class Listener
      * @param[in] i_correlatedPropJsonFile - File path of correlated properties
      * JSON.
      */
+
     void registerCorrPropCallBack(
-        [[maybe_unused]] const std::string& i_correlatedPropJsonFile =
+        const std::string& i_correlatedPropJsonFile =
             constants::correlatedPropJsonFile) noexcept;
 
     /**
      * @brief API to register properties changed callback.
      *
      * This API registers a properties changed callback for a specific interface
-     * under a service.
+     * under a service by constructing a match object. This API also saves the
+     * constructed match object into map object map data member.
      *
      * @param[in] i_service - Service name.
      * @param[in] i_interface - Interface name.
@@ -81,10 +83,8 @@ class Listener
      * @throw FirmwareException
      */
     void registerPropChangeCallBack(
-        [[maybe_unused]] const std::string& i_service,
-        [[maybe_unused]] const std::string& i_interface,
-        [[maybe_unused]] std::function<void(sdbusplus::message_t& i_msg)>
-            i_callBackFunction);
+        const std::string& i_service, const std::string& i_interface,
+        std::function<void(sdbusplus::message_t& i_msg)> i_callBackFunction);
 
   private:
     /**
@@ -120,6 +120,9 @@ class Listener
 
     // Parsed correlated properties JSON.
     nlohmann::json m_correlatedPropJson{};
+
+    // A map of {service name,{interface,match object}}
+    types::MatchObjectMap m_matchObjectMap;
 
     /**
      * @brief API which is called when correlated property change is detected
