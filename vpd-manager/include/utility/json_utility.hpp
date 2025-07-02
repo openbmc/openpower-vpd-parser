85ccad63e2429b7e1311a2ff2e270d7350809141
Souvik Roy <souvikroyofficial10@gmail.com>
2025-07-02 08:24:38 -0500

    DeleteFruVpd:conditional skip "Present" check
    This commit adds a check in deleteFruVpd API to skip reading "Present"
    property before trying to delete a FRU if the FRU's presence is handled
    by external application and its presence is monitored by vpd-manager.
    
    ```
    Test:
    - On Everest system, see fan0 VPD is present on PIM
    - Set "Present" property of fan0 to false using busctl
    - See fan0 VPD is cleared from PIM
    ```
    
    Change-Id: I69e3f805e3170d86befd95b2efc1a59069d762d6
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/utility/json_utility.hpp
+++ vpd-manager/include/utility/json_utility.hpp
@@ -1198,5 +1198,44 @@ inline std::vector<types::Path> getFrusWithPresenceMonitoring(
     }
     return l_frusWithPresenceMonitoring;
 }
+
+/**
+ * @brief API which tells if the FRU's presence should be monitored
+ *
+ * For a given FRU, this API checks if it's presence should be monitored by
+ * checking the "monitorPresence" tag.
+ *
+ * @param[in] i_sysCfgJsonObj - System config JSON object.
+ * @param[in] i_vpdFruPath - EEPROM path.
+ *
+ * @return true if FRU is replaceable at standby. false otherwise.
+ */
+inline bool isPresenceMonitoredFru(const nlohmann::json& i_sysCfgJsonObj,
+                                   const std::string& i_vpdFruPath)
+{
+    try
+    {
+        if (i_vpdFruPath.empty())
+        {
+            throw std::runtime_error("Given FRU path is empty.");
+        }
+
+        if (i_sysCfgJsonObj.empty() || (!i_sysCfgJsonObj.contains("frus")))
+        {
+            throw JsonException("Invalid system config JSON object.");
+        }
+
+        return i_sysCfgJsonObj["frus"][i_vpdFruPath].at(0).value(
+            "monitorPresence", false);
+    }
+    catch (const std::exception& l_ex)
+    {
+        logging::logMessage(
+            "Failed to check if FRU's presence should be monitored, reason:" +
+            std::string(l_ex.what()));
+    }
+
+    return false;
+}
 } // namespace jsonUtility
 } // namespace vpd
