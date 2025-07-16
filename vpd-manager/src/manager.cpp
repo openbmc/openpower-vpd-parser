f1900f7f5d6254eb7650d38c0903974afd95275b
Anupama B R <anupama.b.r1@ibm.com>
2025-07-16 01:36:53 -0500

    Moving OEM handler API under IBM tag
    OEM handler API to collect all FRUs VPD is moved under IBM_SYSTEM tag.
    
    Change-Id: Iad0150fa00337b8493eb27d1b647a266bebf6efc
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/src/manager.cpp
+++ vpd-manager/src/manager.cpp
@@ -634,6 +634,8 @@ bool Manager::collectAllFruVpd() const noexcept
             __FUNCTION__, 0, "Collect all FRUs VPD is requested.", std::nullopt,
             std::nullopt, std::nullopt, std::nullopt);
 
+// ToDo: Handle with OEM interface
+#ifdef IBM_SYSTEM
         if (m_ibmHandler.get() != nullptr)
         {
             m_ibmHandler->collectAllFruVpd();
@@ -644,6 +646,7 @@ bool Manager::collectAllFruVpd() const noexcept
             throw std::runtime_error(
                 "Not found any OEM handler to collect all FRUs VPD.");
         }
+#endif
     }
     catch (const std::exception& l_ex)
     {
