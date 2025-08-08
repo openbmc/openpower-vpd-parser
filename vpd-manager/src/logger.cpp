7cf17f7ce0d1b7e19791ff6ac2018ed2efbca6e0
Sunny Srivastava <sunnsr25@in.ibm.com>
2025-08-08 01:45:23 -0500

    Base framework for log management
    The commit implements stub of logger class and log file handler class,
    which together will be further extended to implement methods required
    for abstracting log management from the caller and will log message at
    appropriate place.
    
    Change-Id: Ieb53ec5d0377fbad062f1150bcbf24c335a7ce80
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

--- vpd-manager/src/logger.cpp
+++ vpd-manager/src/logger.cpp
@@ -4,6 +4,41 @@
 
 namespace vpd
 {
+std::shared_ptr<Logger> Logger::m_loggerInstance;
+
+void Logger::logMessage(std::string_view i_message,
+                        const PlaceHolder& i_placeHolder,
+                        const types::PelInfoTuple* i_pelTuple,
+                        const std::source_location& i_location)
+{
+    std::ostringstream l_log;
+    l_log << "FileName: " << i_location.file_name() << ","
+        << " Line: " << i_location.line() << " " << i_message;
+
+    if (i_placeHolder == PlaceHolder::COLLECTION)
+    {
+        // Log it to a specific place.
+        m_logFileHandler->writeLogToFile(i_placeHolder);
+    }
+    else if (i_placeHolder == PlaceHolder::PEL)
+    {
+        if (i_pelTuple)
+        {
+            // LOG PEL
+            // This should call create PEL API from the event logger.
+            return;
+        }
+        std::cout << "Pel info tuple required to log PEL for message <" +
+                         l_log.str() + ">"
+                  << std::endl;
+    }
+    else
+    {
+        // Default case, let it go to journal.
+        std::cout << l_log.str() << std::endl;
+    }
+}
+
 namespace logging
 {
 void logMessage(std::string_view message, const std::source_location& location)
@@ -11,11 +46,6 @@ void logMessage(std::string_view message, const std::source_location& location)
     std::ostringstream log;
     log << "FileName: " << location.file_name() << ","
         << " Line: " << location.line() << " " << message;
-
-    /* TODO: Check on this later.
-    log << "FileName: " << location.file_name() << ","
-        << " Line: " << location.line() << ","
-        << " Func: " << location.function_name() << ", " << message;*/
 
     std::cout << log.str() << std::endl;
 }
