268c96b1163e4df445a65df4e65a7410620d82b5
Rekha Aparna <vrekhaaparna@ibm.com>
2025-08-12 10:51:23 -0500

    Define error codes for json_utility API's
    This commit defines error codes for the errors that can be thrown by
    json_utility APIs.
    
    Change-Id: If783c6389d14fcba45662facfc98574643d7fe5c
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

---
+++ vpd-manager/include/error_codes.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace vpd
+{
+namespace error_code
+{
+// File exceptions
+static constexpr auto FILE_NOT_FOUND = 1001;
+static constexpr auto FILE_ACCESS_ERROR = 1002;
+static constexpr auto EMPTY_FILE = 1003;
+
+// JSON exceptions
+static constexpr auto MISSING_FRU_TAG = 2001;
+static constexpr auto MISSING_FLAG = 2002;
+static constexpr auto MISSING_ACTION_TAG = 2003;
+static constexpr auto FRU_PATH_NOT_FOUND = 2004;
+
+// Generic errors.
+static constexpr auto EMPTY_INPUT_PARAMATER = 3001;
+} // namespace error_code
+} // namespace vpd
