a5cd66ffa6470ed3bd4d3f6a1f8b80ce71e68e07
Anupama B R <anupama.b.r1@ibm.com>
2025-08-29 02:41:10 -0500

    Update overall VPD collection status
    This commit adds code to use xyz.openbmc_project.Common.Progress
    interface instead of the com.ibm.VPD.Manager to update overall VPD
    collection status of the system.
    
    Output:
    ```
    Collection status updates when FRUs VPD gets collected
    
    root@p11bmc:~# busctl get-property com.ibm.VPD.Manager /com/ibm/VPD/Manager xyz.openbmc_project.Common.Progress Status
    s "xyz.openbmc_project.Common.Progress.OperationStatus.NotStarted"
    
    root@p11bmc:~# busctl get-property com.ibm.VPD.Manager /com/ibm/VPD/Manager xyz.openbmc_project.Common.Progress Status
    s "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress"
    
    root@p11bmc:~# busctl get-property com.ibm.VPD.Manager /com/ibm/VPD/Manager xyz.openbmc_project.Common.Progress Status
    s "xyz.openbmc_project.Common.Progress.OperationStatus.Completed"
    
    ```
    
    Change-Id: I8fc12e5c3ee89092405b6a270cf3eaafa70d41d2
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/src/manager_main.cpp
+++ vpd-manager/src/manager_main.cpp
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #include "bios_handler.hpp"
+#include "constants.hpp"
 #include "event_logger.hpp"
 #include "exceptions.hpp"
 #include "logger.hpp"
@@ -27,8 +28,12 @@ int main(int, char**)
         std::shared_ptr<sdbusplus::asio::dbus_interface> interface =
             server.add_interface(OBJPATH, IFACE);
 
-        auto vpdManager =
-            std::make_shared<vpd::Manager>(io_con, interface, connection);
+        std::shared_ptr<sdbusplus::asio::dbus_interface> collInterface =
+            server.add_interface(OBJPATH,
+                                 vpd::constants::vpdCollectionInterface);
+
+        auto vpdManager = std::make_shared<vpd::Manager>(
+            io_con, interface, collInterface, connection);
 
         // TODO: Take this under conditional compilation for IBM
         auto biosHandler =
@@ -36,6 +41,7 @@ int main(int, char**)
                 connection, vpdManager);
 
         interface->initialize();
+        collInterface->initialize();
 
         vpd::logging::logMessage("Start VPD-Manager event loop");
 
