ad0db9ee4c47d3b5ae8fd3dfca822063e715aa26
Rekha Aparna <vrekhaaparna@ibm.com>
2025-09-01 20:29:18 -0500

    Set error code for isFruReplaceableAtRuntime API
    This commit updates isFruReplaceableAtRuntime API to set error code in
    case of error. This helps caller of API to take action based on the
    error code returned from the API.
    
    Change-Id: Ibf07dcbce8aaf25a21e851a562914807cec35228
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- vpd-manager/include/utility/json_utility.hpp
+++ vpd-manager/include/utility/json_utility.hpp
@@ -1005,34 +1005,30 @@ inline bool isFruPowerOffOnly(const nlohmann::json& i_sysCfgJsonObj,
  *
  * @param[in] i_sysCfgJsonObj - System config JSON object.
  * @param[in] i_vpdFruPath - EEPROM path.
+ * @param[out] o_errCode - to set error code in case of error.
  *
  * @return true if FRU is replaceable at runtime. false otherwise.
  */
 inline bool isFruReplaceableAtRuntime(const nlohmann::json& i_sysCfgJsonObj,
-                                      const std::string& i_vpdFruPath)
+                                      const std::string& i_vpdFruPath,
+                                      uint16_t& o_errCode)
 {
-    try
+    if (i_vpdFruPath.empty())
     {
-        if (i_vpdFruPath.empty())
-        {
-            throw std::runtime_error("Given FRU path is empty.");
-        }
-
-        if (i_sysCfgJsonObj.empty() || (!i_sysCfgJsonObj.contains("frus")))
-        {
-            throw std::runtime_error("Invalid system config JSON object.");
-        }
-
-        return ((i_sysCfgJsonObj["frus"][i_vpdFruPath].at(0))
-                    .contains("replaceableAtRuntime") &&
-                (i_sysCfgJsonObj["frus"][i_vpdFruPath].at(
-                    0)["replaceableAtRuntime"]));
+        o_errCode = error_code::INVALID_INPUT_PARAMETER;
+        return false;
     }
-    catch (const std::exception& l_error)
+
+    if (i_sysCfgJsonObj.empty() || (!i_sysCfgJsonObj.contains("frus")))
     {
-        // TODO: Log PEL
-        logging::logMessage(l_error.what());
+        o_errCode = error_code::INVALID_JSON;
+        return false;
     }
+
+    return (
+        (i_sysCfgJsonObj["frus"][i_vpdFruPath].at(0))
+            .contains("replaceableAtRuntime") &&
+        (i_sysCfgJsonObj["frus"][i_vpdFruPath].at(0)["replaceableAtRuntime"]));
 
     return false;
 }
