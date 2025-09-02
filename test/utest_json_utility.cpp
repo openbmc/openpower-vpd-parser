233bf37f5c1fd5ffdfc9830034be1cc42f91fe39
Rekha Aparna <vrekhaaparna@ibm.com>
2025-09-01 20:48:07 -0500

    Set error code for isFruPowerOffOnly API
    This commit updates isFruPowerOffOnly API to set error code in case of
    error. This helps caller of API to take action based on the error code
    returned from the API.
    
    Change-Id: Ieb5b0a649ed802cc08ad836bc7399d3dacec9429
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- test/utest_json_utility.cpp
+++ test/utest_json_utility.cpp
@@ -12,6 +12,7 @@ using namespace vpd;
 TEST(IsFruPowerOffOnlyTest, PositiveTestCase)
 {
     uint16_t l_errCode = 0;
+    uint16_t l_errCode1 = 0;
     const std::string l_jsonPath{"/usr/local/share/vpd/50001001.json"};
     const std::string l_vpdPath{"/sys/bus/spi/drivers/at25/spi12.0/eeprom"};
     const nlohmann::json l_parsedJson =
@@ -25,13 +26,22 @@ TEST(IsFruPowerOffOnlyTest, PositiveTestCase)
     }
 
     const bool l_result =
-        jsonUtility::isFruPowerOffOnly(l_parsedJson, l_vpdPath);
+        jsonUtility::isFruPowerOffOnly(l_parsedJson, l_vpdPath, l_errCode1);
+
+    if (l_errCode1)
+    {
+        logging::logMessage(
+            "Failed to check if FRU is power off only for FRU [" + l_vpdPath +
+            "], error : " + vpdSpecificUtility::getErrCodeMsg(l_errCode1));
+    }
+
     EXPECT_TRUE(l_result);
 }
 
 TEST(IsFruPowerOffOnlyTest, NegativeTestCase)
 {
     uint16_t l_errCode = 0;
+    uint16_t l_errCode1 = 0;
     const std::string l_jsonPath{"/usr/local/share/vpd/50001001.json"};
     const std::string l_vpdPath{"/sys/bus/i2c/drivers/at24/4-0050/eeprom"};
     const nlohmann::json l_parsedJson =
@@ -45,6 +55,14 @@ TEST(IsFruPowerOffOnlyTest, NegativeTestCase)
     }
 
     const bool l_result =
-        jsonUtility::isFruPowerOffOnly(l_parsedJson, l_vpdPath);
+        jsonUtility::isFruPowerOffOnly(l_parsedJson, l_vpdPath, l_errCode1);
+
+    if (l_errCode1)
+    {
+        logging::logMessage(
+            "Failed to check if FRU is power off only for FRU [" + l_vpdPath +
+            "], error : " + vpdSpecificUtility::getErrCodeMsg(l_errCode1));
+    }
+
     EXPECT_FALSE(l_result);
 }
