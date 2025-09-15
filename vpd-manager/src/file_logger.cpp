4681edb9b299dedf0fde5ea03fc498b68bd75c92
Souvik Roy <souvikroyofficial10@gmail.com>
2025-09-15 08:18:25 +0000

    FileLogger: Implement async file logging
    This commit implements a class to handle asynchronous file logging. It
    extends the synchronous FileLogger class. Logging messages to file from
    multiple VPD collection threads synchronously affects the overall VPD
    collection time, hence these logs need to be handled asynchronously.
    
    Change-Id: I81342e20a10015364d7de5b77531688aeabe6000
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/file_logger.cpp
+++ vpd-manager/src/file_logger.cpp
@@ -1,5 +1,6 @@
 #include "file_logger.hpp"
 
+#include <iostream>
 namespace vpd
 {
 const std::unordered_map<LogLevel, std::string>
@@ -14,7 +15,7 @@ void FileLogger::logMessage(const std::string& i_message,
     try
     {
         // acquire lock on file
-        std::lock_guard<std::mutex> l_lock(m_fileMutex);
+        std::lock_guard<std::mutex> l_lock(m_mutex);
         if (++m_currentNumEntries > m_maxEntries)
         {
             // TODO: implement log rotation
@@ -28,6 +29,63 @@ void FileLogger::logMessage(const std::string& i_message,
     {
         throw;
     }
+}
+
+void AsyncFileLogger::logMessage(const std::string& i_message,
+                                 const LogLevel i_logLevel)
+{
+    try
+    {
+        // acquire lock on queue
+        std::lock_guard<std::mutex> l_lock(m_mutex);
+        // push message to queue
+        m_messageQueue.emplace(
+            timestamp() + "[" + m_logLevelToStringMap.at(i_logLevel) + "]" +
+            i_message);
+    }
+    catch (const std::exception& l_ex)
+    {
+        throw;
+    }
+}
+
+void AsyncFileLogger::fileWorker() noexcept
+{
+    while (m_shouldWorkerThreadRun)
+    {
+        // acquire lock on queue
+        m_mutex.lock();
+
+        if (!m_messageQueue.empty())
+        {
+            // read the first message in queue
+            const auto l_logMessage = m_messageQueue.front();
+            try
+            {
+                // pop the message from queue
+                m_messageQueue.pop();
+
+                if (++m_currentNumEntries > m_maxEntries)
+                {
+                    // TODO: implement log rotation
+                    m_currentNumEntries = 0;
+                }
+
+                // flush the message to file
+                m_fileStream << l_logMessage << std::endl;
+            }
+            catch (const std::exception& l_ex)
+            {
+                // redirect log to journal
+                std::cout << l_logMessage << std::endl;
+            }
+        }
+
+        // release lock on queue before going to sleep
+        m_mutex.unlock();
+
+        std::this_thread::sleep_for(std::chrono::seconds(m_flushTimeInSecs));
+    } // thread loop
 }
 
 }; // namespace vpd
