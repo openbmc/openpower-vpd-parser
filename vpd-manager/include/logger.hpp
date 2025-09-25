2412425eca0c98a657e061fe5c15ec78c4429ebf
Souvik Roy <souvikroyofficial10@gmail.com>
2025-09-25 09:56:20 +0000

    Implement async file logger
    This commit implements asynchronous, thread safe file logging.
    
    Change-Id: I1a7c28cc37f9e88f04e6af89cf4042d032c8cf90
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/logger.hpp
+++ vpd-manager/include/logger.hpp
@@ -2,6 +2,7 @@
 
 #include "types.hpp"
 
+#include <condition_variable>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -197,8 +198,11 @@ class AsyncFileLogger final : public ILogFileHandler
     // queue for log messages
     std::queue<std::string> m_messageQueue;
 
-    // flag which controls if the logger worker thread should be running
-    std::atomic_bool m_shouldStopLogging{true};
+    // flag which indicates log worker thread if logging is finished
+    std::atomic_bool m_shouldStopLogging{false};
+
+    // conditional variable to signal log worker thread
+    std::condition_variable m_cv;
 
     /**
      * @brief Constructor
@@ -244,15 +248,16 @@ class AsyncFileLogger final : public ILogFileHandler
     // destructor
     ~AsyncFileLogger()
     {
-        /* TODO
-            - acquire lock
-            - set log stop flag to true
-            - notify log worker thread
-        */
+        std::unique_lock<std::mutex> l_lock(m_mutex);
+
+        m_shouldStopLogging = true;
+
         if (m_fileStream.is_open())
         {
             m_fileStream.close();
         }
+
+        m_cv.notify_one();
     }
 };
 
