40a7a1c7b897bf5c01dc1a48a3b3af677ef0c0f4
Rekha Aparna <vrekhaaparna@ibm.com>
2025-10-09 20:45:53 -0500

    Error code for encodeKeyword API
    This commit updates encodeKeyword APIs to set error code in case of
    error. This helps the caller of API to take action based on the error
    code returned from the API.
    
    Change-Id: I0cba6191d2ec6ded8e793514086f3e62218cf893
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- test/utest_utils.cpp
+++ test/utest_utils.cpp
@@ -1,3 +1,5 @@
+#include "utility/common_utility.hpp"
+
 #include <utility/vpd_specific_utility.hpp>
 
 #include <cassert>
@@ -9,10 +11,18 @@ using namespace vpd;
 
 TEST(UtilsTest, TestValidValue)
 {
+    uint16_t l_errCode = 0;
     std::string key = "VINI";
     std::string encoding = "MAC";
     std::string expected = "56:49:4e:49";
-    EXPECT_EQ(expected, vpdSpecificUtility::encodeKeyword(key, encoding));
+    EXPECT_EQ(expected,
+              vpdSpecificUtility::encodeKeyword(key, encoding, l_errCode));
+    if (l_errCode)
+    {
+        logging::logMessage(
+            "Failed to get encoded keyword value for : " + key +
+            ", error : " + commonUtility::getErrCodeMsg(l_errCode));
+    }
 }
 
 int main(int argc, char** argv)
