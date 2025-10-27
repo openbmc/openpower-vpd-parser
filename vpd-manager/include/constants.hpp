efc3eb75a956754a09500cac11b4535c006de72a
Anupama B R <anupama.b.r1@ibm.com>
2025-10-27 04:54:58 -0500

    Update BMC position for RBMC prototype system
    This commit adds a stub API to determine whether the system is an RBMC
    prototype. If it is, the BMC position is updated based on motherboard
    access.
    
    Change-Id: I38b9f6b5d1ba4e840ad98208c7983fc650d0735b
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/include/constants.hpp
+++ vpd-manager/include/constants.hpp
@@ -239,5 +239,8 @@ static constexpr auto power_vs_50001_json =
     "/usr/share/vpd/50001_power_vs.json";
 static constexpr auto correlatedPropJsonFile =
     "/usr/share/vpd/correlated_properties.json";
+
+static constexpr auto positionInterface =
+    "xyz.openbmc_project.Inventory.Decorator.Position";
 } // namespace constants
 } // namespace vpd
