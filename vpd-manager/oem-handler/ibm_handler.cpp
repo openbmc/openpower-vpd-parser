12f6c34fbbdc0e1d9908984748032f5a6b245d88
Alpana Kumari <alpu8007@gmail.com>
2025-11-10 08:59:20 -0600

    Flag based logger functions
    Moved initiation and termination of FRU VPD collection logging under
    compile time flag. User can enable/disable the use of these functions
    while building the bmc image by setting/unsetting the particular flag.
    
    Change-Id: I63238d63bea065f6b1c0fca6d66c87307a713f94
    Signed-off-by: Alpana Kumari <alpu8007@gmail.com>

--- vpd-manager/oem-handler/ibm_handler.cpp
+++ vpd-manager/oem-handler/ibm_handler.cpp
@@ -144,9 +144,10 @@ void IbmHandler::SetTimerToDetectVpdCollectionStatus()
             {
                 m_eventListener->registerCorrPropCallBack();
             }
-
+#ifdef ENABLE_FILE_LOGGING
             // terminate collection logger
             m_logger->terminateVpdCollectionLogging();
+#endif
         }
         else
         {
@@ -156,9 +157,10 @@ void IbmHandler::SetTimerToDetectVpdCollectionStatus()
                 l_timer.cancel();
                 logging::logMessage("Taking too long. Active thread = " +
                                     std::to_string(l_threadCount));
-
+#ifdef ENABLE_FILE_LOGGING
                 // terminate collection logger
                 m_logger->terminateVpdCollectionLogging();
+#endif
             }
             else
             {
@@ -488,8 +490,10 @@ void IbmHandler::performInitialSetup()
 
 void IbmHandler::collectAllFruVpd()
 {
+#ifdef ENABLE_FILE_LOGGING
     // initialize VPD collection logger
     m_logger->initiateVpdCollectionLogging();
+#endif
 
     // Setting status to "InProgress", before trigeering VPD collection.
     m_progressInterface->set_property(
