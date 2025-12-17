371ab6acb8a29bbaa5749c0f196afdf7e3f92b07
Souvik Roy <souvikroyofficial10@gmail.com>
2025-12-17 15:54:39 +0000

    Inventory backup data restoration stub
    This commit implements stub changes in wait-vpd-parsers service to check
    for inventory backup data and restore it if found. On systems with
    redundant BMCs, inventory data will be synced from Active BMC to Passive
    BMC by other service at a specific file path. When the old Passive BMC
    is transitioning to Active, it needs to check for the backup data,
    restore it and restart the inventory manager service. This commit only
    includes stub changes, and actual changes will be handled in future
    commits.
    
    Change-Id: I1d22a4069614bed441b1b31fc02bc17c5279a5c8
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

---
+++ wait-vpd-parser/include/inventory_backup_handler.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include "constants.hpp"
+#include "logger.hpp"
+
+#include <filesystem>
+
+/**
+ * @brief Class to handle backup inventory data.
+ *
+ * This class is used for handling inventory backup data. This contains methods
+ * to handle checking inventory backed up data, restoring said data and
+ * restarting inventory manager service if needed
+ */
+class InventoryBackupHandler
+{
+  public:
+    /*
+     * Deleted methods
+     */
+    InventoryBackupHandler(const InventoryBackupHandler&) = delete;
+    InventoryBackupHandler operator=(const InventoryBackupHandler&) = delete;
+    InventoryBackupHandler(const InventoryBackupHandler&&) = delete;
+    InventoryBackupHandler operator=(const InventoryBackupHandler&&) = delete;
+
+    /**
+     * @brief Parameterized Constructor
+     *
+     * @param[in] i_inventoryServiceName - Inventory manager service name
+     * @param[in] i_inventoryBackupPath - Inventory backup path
+     */
+    InventoryBackupHandler(const std::string_view i_inventoryServiceName,
+                           const std::filesystem::path i_inventoryBackupPath) :
+        m_inventoryManagerServiceName{i_inventoryServiceName},
+        m_inventoryBackupPath{i_inventoryBackupPath},
+        m_logger{vpd::Logger::getLoggerInstance()}
+    {}
+
+    /**
+     * @brief Default constructor
+     *
+     */
+    InventoryBackupHandler() :
+        InventoryBackupHandler{vpd::constants::pimServiceName,
+                               vpd::constants::pimBackupPath}
+    {}
+
+    /**
+     * @brief API to check and restore inventory backup data
+     *
+     * This API checks if there is any backed up inventory data. If backup data
+     * is found at the specified file path, the back up data is restored, the
+     * inventory manager service is restarted and the backup data is deleted.
+     *
+     * @return true if inventory backup data is found and successfully restored,
+     * false otherwise
+     */
+    bool checkAndRestoreInventoryBackupData() const noexcept;
+
+  private:
+    /**
+     * @brief API to check if inventory backup path has data
+     *
+     * @param[out] o_errCode - To set error code in case of error.
+     *
+     * @return true if inventory backup data is found, false otherwise
+     *
+     * @throw std::runtime_error
+     */
+    inline bool checkInventoryBackupPath(uint16_t& o_errCode) const;
+
+    /**
+     * @brief API to restore inventory data from backup file path to inventory
+     * persisted path
+     *
+     * @param[out] o_errCode - To set error code in case of error.
+     *
+     * @return true if the restoration is successful, false otherwise
+     *
+     * @throw std::runtime_error
+     */
+    inline bool restoreInventoryBackupData(uint16_t& o_errCode) const;
+
+    /**
+     * @brief API to clear inventory backup data from backup file path
+     *
+     * @param[out] o_errCode - To set error code in case of error.
+     * @return true if backup data has been cleared, false otherwise
+     *
+     * @throw std::runtime_error
+     */
+    inline bool clearInventoryBackupData(uint16_t& o_errCode) const;
+
+    /**
+     * @brief API to restart inventory manager service
+     *
+     * @param[out] o_errCode - To set error code in case of error.
+     *
+     * @return true if inventory manager service is successfully restarted,
+     * false otherwise
+     *
+     * @throw std::runtime_error
+     */
+    inline bool restartInventoryManagerService(uint16_t& o_errCode) const;
+
+    /* Members */
+    std::string m_inventoryManagerServiceName{vpd::constants::pimServiceName};
+    std::filesystem::path m_inventoryBackupPath{vpd::constants::pimBackupPath};
+
+    std::shared_ptr<vpd::Logger> m_logger{nullptr};
+};
