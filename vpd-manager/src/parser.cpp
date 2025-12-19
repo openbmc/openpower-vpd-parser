3e3ff7954d59f576d8f8110c520c4cad7e83b64c
Alpana Kumari <alpu8007@gmail.com>
2025-12-19 01:01:21 -0600

    updateVpdKeyword
    Replaced callPIM to call generic method which will decide at runtime
    which method to call for dbus update.
    
    Test:
    ```
    ```
    
    Change-Id: I8488f33b271204d6cb001a7639838a7dfcaf541e
    Signed-off-by: Alpana Kumari <alpu8007@gmail.com>

--- vpd-manager/src/parser.cpp
+++ vpd-manager/src/parser.cpp
@@ -219,8 +219,9 @@ int Parser::updateVpdKeyword(const types::WriteVpdParams& i_paramsToWriteData,
                     l_interfaceName, types::PropertyMap{std::make_pair(
                                          l_propertyName, l_keywordValue)})})};
 
-            // Call PIM's Notify method to perform update
-            if (!dbusUtility::callPIM(std::move(l_dbusObjMap)))
+	    logging::logMessage("DBG: updateVpdKeyword:update Dbus data via callDbusMethod");
+            // Call method for dbus update
+            if (!dbusUtility::callDbusMethod(std::move(l_dbusObjMap)))
             {
                 // Call to PIM's Notify method failed.
                 std::string l_errMsg("Notify PIM is failed for object path: " +
