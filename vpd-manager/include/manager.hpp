39bdab35e963f5613ba2ec03eacc5ce67056689a
Anupama B R <anupama.b.r1@ibm.com>
2026-01-05 01:14:32 -0600

    Check primary&redundant EEPROMs VPD are identical
    Some FRUs contain a redundant VPD copy on an alternate EEPROM.
    To ensure data consistency, the VPD manager must be able to compare the
    primary and redundant VPD contents and detect mismatches. This commit
    adds the necessary support for that comparison.
    
    A new D-Bus method, ValidateEEPROMs is introduced in the VPD Manager.
    It allows callers to verify whether the VPD of a given EEPROM matches
    that of its redundant EEPROM.
    
    The actual implementation will be added in subsequent commits.
    
    Change-Id: I3f6588d320bc1b3d0d3cfc85192a3624b2143e28
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/include/manager.hpp
+++ vpd-manager/include/manager.hpp
@@ -249,6 +249,24 @@ class Manager
         return m_worker;
     }
 
+    /**
+     * @brief Checks whether the primary and its redundant EEPROMs are
+     * identical.
+     *
+     * This API checks whether the given primary EEPROM and its corresponding
+     * redundant EEPROM (as defined in the system configuration JSON) contain
+     * the same VPD.
+     *
+     * Note:
+     * - If there is no corresponding redundant EEPROM path, or
+     * - If the given EEPROM path is not part of the system configuration JSON,
+     *   the API returns false.
+     *
+     * @return true if the VPD on given EEPROM and its redundant EEPROM are the
+     * same; false otherwise.
+     */
+    bool validateRedundantEeprom(const types::Path& i_fruPath) const noexcept;
+
   private:
     /**
      * @brief An api to check validity of unexpanded location code.
