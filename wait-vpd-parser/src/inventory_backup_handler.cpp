19a82f4a785ef0a10383be3af189b0558b41695b
Souvik Roy <souvikroyofficial10@gmail.com>
2026-01-06 09:43:49 +0000

    InventoryBackupHandler implementation
    This commit implements APIs in InventoryBackupHandler class.
    
    Change-Id: Id179738b484f6f036a31d9a9f892cc7f91f8f979
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- wait-vpd-parser/src/inventory_backup_handler.cpp
+++ wait-vpd-parser/src/inventory_backup_handler.cpp
@@ -2,6 +2,7 @@
 
 #include "error_codes.hpp"
 #include "utility/common_utility.hpp"
+#include "utility/dbus_utility.hpp"
 
 bool InventoryBackupHandler::checkInventoryBackupPath(
     uint16_t& o_errCode) const noexcept
@@ -10,10 +11,25 @@ bool InventoryBackupHandler::checkInventoryBackupPath(
     o_errCode = 0;
     try
     {
-        /* TODO:
-            Check inventory backup path to see if it
-           has sub directories with inventory backup data and return true if so.
-        */
+        const auto l_systemInventoryBackupPath{
+            m_inventoryBackupPath /
+            std::filesystem::path(vpd::constants::systemInvPath)
+                .relative_path()};
+
+        if (std::filesystem::exists(l_systemInventoryBackupPath) &&
+            std::filesystem::is_directory(l_systemInventoryBackupPath) &&
+            !std::filesystem::is_empty(l_systemInventoryBackupPath))
+        {
+            // check number of directories under system inventory
+            auto l_dirIt = std::filesystem::directory_iterator{
+                l_systemInventoryBackupPath};
+
+            const auto l_numSubDirectories = std::count_if(
+                std::filesystem::begin(l_dirIt), std::filesystem::end(l_dirIt),
+                [](const auto& l_entry) { return l_entry.is_directory(); });
+
+            l_rc = (l_numSubDirectories != 0);
+        }
     }
     catch (const std::exception& l_ex)
     {
@@ -49,6 +65,36 @@ bool InventoryBackupHandler::restoreInventoryBackupData(
             2. Extract the object path and interface information
             3. Restore the data to inventory manager persisted path.
         */
+        const auto l_systemInventoryPrimaryPath{
+            m_inventoryPrimaryPath /
+            std::filesystem::path(vpd::constants::systemVpdInvPath)
+                .relative_path()};
+
+        const auto l_systemInventoryBackupPath{
+            m_inventoryBackupPath /
+            std::filesystem::path(vpd::constants::systemVpdInvPath)
+                .relative_path()};
+
+        if (std::filesystem::exists(l_systemInventoryPrimaryPath) &&
+            std::filesystem::is_directory(l_systemInventoryPrimaryPath))
+        {
+            // TODO: copy all sub directories under system from backup to
+            // primary
+            auto l_dirIt = std::filesystem::directory_iterator{
+                l_systemInventoryBackupPath};
+
+            std::for_each(
+                std::filesystem::begin(l_dirIt), std::filesystem::end(l_dirIt),
+                [this](const auto& l_entry) {
+                    if (l_entry.is_directory())
+                    {
+                        m_logger->logMessage(
+                            "_SR dir: " + l_entry.path().filename().string());
+                    }
+                });
+
+            l_rc = true;
+        }
     }
     catch (const std::exception& l_ex)
     {
@@ -79,9 +125,13 @@ bool InventoryBackupHandler::clearInventoryBackupData(
     o_errCode = 0;
     try
     {
-        /* TODO:
-           Clear all directories under inventory backup path
-        */
+        //     auto l_systemInventoryBackupPath{m_inventoryBackupPath};
+        //     l_systemInventoryBackupPath +=
+        //         std::filesystem::path{vpd::constants::systemInvPath};
+
+        //    std::filesystem::remove_all(l_systemInventoryBackupPath);
+
+        l_rc = true;
     }
     catch (const std::exception& l_ex)
     {
@@ -120,6 +170,38 @@ bool InventoryBackupHandler::restartInventoryManagerService(
            re-collection. If not running then along with critical PEL, fail this
            service as well.
         */
+        auto l_restartInvManager = [this]() -> bool {
+            uint16_t l_errCode{0};
+            const auto l_cmd =
+                "systemctl restart " + m_inventoryManagerServiceName;
+            vpd::commonUtility::executeCmd(l_cmd, l_errCode);
+
+            return l_errCode == vpd::constants::VALUE_0;
+        };
+
+        // Restart inventory manager service, with a re-try limit of 3 attempts
+        constexpr auto l_numRetries{3};
+
+        for (unsigned l_attempt = 0; l_attempt < l_numRetries; ++l_attempt)
+        {
+            if (l_restartInvManager())
+            {
+                // restarting inventory manager service is successful, return
+                // success
+                return true;
+            }
+        }
+
+        // re-try limit crossed
+        m_logger->logMessage(
+            "Failed to restart inventory manager service after " +
+            std::to_string(l_numRetries) + " attempts");
+
+        // check inventory manager service state and set appropriate error code
+        o_errCode =
+            vpd::dbusUtility::isServiceRunning(m_inventoryManagerServiceName)
+                ? vpd::error_code::SERVICE_RUNNING
+                : vpd::error_code::SERVICE_NOT_RUNNING;
     }
     catch (const std::exception& l_ex)
     {
