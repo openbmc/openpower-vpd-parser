6ea584f98245dfadebe22ce4c9c1db3028254bc1
Alpana Kumari <alpu8007@gmail.com>
2026-01-23 06:02:31 -0600

    backup_restore update
    Updated this class APIs, to use logger::logMessage with placeHolder PEL
    instead of using EventLogger::createSyncPel to log a PEL.
    This change is required to stream line everything with logger class.
    
    Test-
    Tested on PST simics
    ```
    ```
    
    Change-Id: Ie708bd11c733e5f3deabd18bbbc38985c0e0d0e2
    Signed-off-by: Alpana Kumari <alpu8007@gmail.com>

--- vpd-manager/src/backup_restore.cpp
+++ vpd-manager/src/backup_restore.cpp
@@ -523,10 +523,12 @@ void BackupAndRestore::backupAndRestoreIpzVpd(types::IPZVpdMap& io_srcVpdMap,
                     " . Value read from destination : " +
                     commonUtility::convertByteVectorToHex(l_dstBinaryValue));
 
-                EventLogger::createSyncPel(
-                    types::ErrorType::VpdMismatch, types::SeverityType::Warning,
-                    __FILE__, __FUNCTION__, 0, l_errorMsg, std::nullopt,
-                    std::nullopt, std::nullopt, std::nullopt);
+                m_logger->logMessage(
+                    l_errorMsg, PlaceHolder::PEL,
+                    types::PelInfoTuple{types::ErrorType::VpdMismatch,
+                                        types::SeverityType::Warning, 0,
+                                        std::nullopt, std::nullopt,
+                                        std::nullopt, std::nullopt});
             }
         }
         else if (l_srcBinaryValue == l_defaultBinaryValue &&
@@ -536,10 +538,11 @@ void BackupAndRestore::backupAndRestoreIpzVpd(types::IPZVpdMap& io_srcVpdMap,
                 "Default value found on both source and destination VPD, for record: " +
                 l_srcRecordName + " and keyword: " + l_srcKeywordName);
 
-            EventLogger::createSyncPel(
-                types::ErrorType::DefaultValue, types::SeverityType::Error,
-                __FILE__, __FUNCTION__, 0, l_errorMsg, std::nullopt,
-                std::nullopt, std::nullopt, std::nullopt);
+            m_logger->logMessage(
+                l_errorMsg, PlaceHolder::PEL,
+                types::PelInfoTuple{
+                    types::ErrorType::VpdMismatch, types::SeverityType::Warning,
+                    0, std::nullopt, std::nullopt, std::nullopt, std::nullopt});
         }
     }
 }
