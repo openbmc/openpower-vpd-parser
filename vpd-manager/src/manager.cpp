556a6d3de474304ee848d866602cec22faa6afbd
Rekha Aparna <vrekhaaparna@ibm.com>
2026-02-12 10:16:20 -0600

    Method to delete all FRU VPD
    Persisted VPD may become stale due to hardware reconfiguration or when
    FRUs are no longer accessible. In such cases, clearing these entries
    ensures the inventory can be repopulated with valid data.
    
    This commit implements a D-Bus method to delete all FRU VPD from PIM
    persisted path under VPD Manager interface.
    
    Change-Id: I41b2653b6f6c8861567f68489e9400f5019369c0
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- vpd-manager/src/manager.cpp
+++ vpd-manager/src/manager.cpp
@@ -126,6 +126,10 @@ Manager::Manager(
             return this->collectAllFruVpd();
         });
 
+        iFace->register_method("DeleteAllFRUVPD", [this]() {
+            this->deleteAllFRUVPD();
+        });
+
         // Indicates FRU VPD collection for the system has not started.
         progressiFace->register_property_rw<std::string>(
             "Status", sdbusplus::vtable::property_::emits_change,
@@ -812,5 +816,62 @@ bool Manager::collectAllFruVpd() const noexcept
                                 std::nullopt, std::nullopt, std::nullopt});
     }
     return false;
+}
+
+void Manager::deleteAllFRUVPD() const noexcept
+{
+    if (m_vpdCollectionStatus == constants::vpdCollectionInProgress)
+    {
+        m_logger->logMessage(
+            "FRU VPD collection is in progress. Cannot perform delete all FRU VPD.");
+        return;
+    }
+
+    try
+    {
+        m_logger->logMessage(
+            std::string("Delete all FRUs VPD is requested."), PlaceHolder::PEL,
+            types::PelInfoTuple{types::ErrorType::FirmwareError,
+                                types::SeverityType::Informational, 0,
+                                std::nullopt, std::nullopt, std::nullopt,
+                                std::nullopt});
+
+        const auto l_inventoryBackupPath{
+            constants::pimPrimaryPath /
+            std::filesystem::path(constants::systemInvPath).relative_path()};
+
+        if (!std::filesystem::exists(l_inventoryBackupPath))
+        {
+            throw std::runtime_error("PIM persist path does not exist.");
+        }
+
+        for (const auto& l_entry :
+             std::filesystem::directory_iterator(l_inventoryBackupPath))
+        {
+            if (std::filesystem::is_directory(l_entry))
+            {
+                std::filesystem::remove_all(l_entry);
+            }
+        }
+
+        uint16_t l_errCode = 0;
+        if (!commonUtility::restartService(constants::pimServiceName,
+                                           l_errCode))
+        {
+            throw std::runtime_error("Failed to restart PIM service, error : " +
+                                     commonUtility::getErrCodeMsg(l_errCode));
+        }
+    }
+    catch (const std::exception& l_ex)
+    {
+        m_logger->logMessage(
+            "Failed to clear inventory backup data from path [" +
+                std::string(constants::pimPrimaryPath) +
+                "]. Error: " + std::string(l_ex.what()),
+            PlaceHolder::PEL,
+            types::PelInfoTuple{types::ErrorType::FirmwareError,
+                                types::SeverityType::Warning, 0, std::nullopt,
+                                std::nullopt, std::nullopt, std::nullopt});
+    }
 }
 } // namespace vpd
