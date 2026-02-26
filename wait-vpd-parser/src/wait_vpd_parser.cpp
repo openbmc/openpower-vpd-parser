00142e22876fc8fe2165f943cb42a1f786d6685f
Souvik Roy <souvikroyofficial10@gmail.com>
2026-02-26 07:18:48 +0000

    wait-vpd-parser: use CollectionOrchestrator
    This commit refactors wait-vpd-parsers to use CollectionOrchestrator
    class to handle triggering of FRU VPD collection and monitoring FRU VPD
    collection status. CollectionOrchestrator encapsulates methods to
    trigger FRU VPD collection and check for FRU VPD collection status in an
    asynchronous manner. This approach eliminates:
    - D-Bus timeout issues
    - Unnecessary CPU usage in polling based design
    
    Test:
    ```
    - Installed BMC image on both BMCs
    - After both BMCs boot, checked active BMC and saw wait-vpd-parsers
      service ran successfully and FRU VPD is collected by vpd-manager
    - On active BMC, stop vpd-manager service
    - Run wait-vpd-parsers and see it exits immediately with rc=1
    - Restarted vpd-manager and then ran wait-vpd-parsers with timeout
      parameter of 5 seconds, checked journal log to see FRU VPD collection
      got triggered but wait-vpd-parsers exited with timeout as expected
    - Rebooted active BMC and after reboot checked active BMC and saw
      wait-vpd-parsers service ran successfully and FRU VPD is collected by
      vpd-manager
    ```
    
    Change-Id: I29ced4c10e3ca1c4d37a0906cac7f0373c2804ec
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- wait-vpd-parser/src/wait_vpd_parser.cpp
+++ wait-vpd-parser/src/wait_vpd_parser.cpp
@@ -1,5 +1,6 @@
 #include "config.h"
 
+#include "collection_orchestrator.hpp"
 #include "constants.hpp"
 #include "inventory_backup_handler.hpp"
 #include "logger.hpp"
@@ -11,99 +12,6 @@
 
 #include <chrono>
 #include <thread>
-
-/**
- * @brief API to check for VPD collection status
- *
- * This API checks for VPD manager collection status by reading the
- * collection "Status" property exposed by vpd-manager on Dbus. The read logic
- * uses a retry loop with a specific number of retries with specific sleep time
- * between each retry.
- *
- * @param[in] i_retryLimit - Maximum number of retries
- * @param[in] i_sleepDurationInSeconds - Sleep time in seconds between each
- * retry
- *
- * @return If "CollectionStatus" property is "Completed", returns 0, otherwise
- * returns 1.
- */
-int checkVpdCollectionStatus(const unsigned i_retryLimit,
-                             const unsigned i_sleepDurationInSeconds) noexcept
-{
-    auto l_logger = vpd::Logger::getLoggerInstance();
-
-    try
-    {
-        l_logger->logMessage(
-            "Checking every " + std::to_string(i_sleepDurationInSeconds) +
-            "s for VPD collection status ....");
-
-        for (unsigned l_retries = i_retryLimit;
-             l_retries != vpd::constants::VALUE_0; --l_retries)
-        {
-            // check at specified time interval
-            std::this_thread::sleep_for(
-                std::chrono::seconds(i_sleepDurationInSeconds));
-
-            const auto l_propValue = vpd::dbusUtility::readDbusProperty(
-                BUSNAME, OBJPATH, vpd::constants::vpdCollectionInterface,
-                "Status");
-
-            if (auto l_val = std::get_if<std::string>(&l_propValue))
-            {
-                if (*l_val == vpd::constants::vpdCollectionCompleted)
-                {
-                    l_logger->logMessage("VPD collection is completed");
-                    return vpd::constants::VALUE_0;
-                }
-            }
-
-            l_logger->logMessage(
-                "Waiting for VPD status update. Retries remaining: " +
-                std::to_string(l_retries));
-        }
-
-        l_logger->logMessage(
-            "Exit wait for VPD services to finish with timeout");
-    }
-    catch (const std::exception& l_ex)
-    {
-        l_logger->logMessage("Error while checking VPD collection status: " +
-                             std::string(l_ex.what()));
-    }
-
-    return vpd::constants::VALUE_1;
-}
-
-/**
- * @brief API to trigger VPD collection for all FRUs.
- *
- * This API triggers VPD collection for all FRUs by calling Dbus API
- * "CollectAllFRUVPD" exposed by vpd-manager
- *
- * @return - On success returns true, otherwise returns false
- */
-inline bool collectAllFruVpd() noexcept
-{
-    bool l_rc{true};
-    try
-    {
-        auto l_bus = sdbusplus::bus::new_default();
-        auto l_method =
-            l_bus.new_method_call(BUSNAME, OBJPATH, IFACE, "CollectAllFRUVPD");
-
-        l_bus.call_noreply(l_method);
-    }
-    catch (const std::exception& l_ex)
-    {
-        auto l_logger = vpd::Logger::getLoggerInstance();
-        l_logger->logMessage(
-            "Failed to trigger all FRU VPD collection. Error: " +
-            std::string(l_ex.what()));
-        l_rc = false;
-    }
-    return l_rc;
-}
 
 /**
  * @brief API to handle inventory backup data
@@ -184,14 +92,12 @@ int main(int argc, char** argv)
     {
         CLI::App l_app{"Wait VPD parser app"};
 
-        // default retry limit and sleep duration values
-        unsigned l_retryLimit{100};
-        unsigned l_sleepDurationInSeconds{2};
+        // default collection status timeout in seconds
+        unsigned l_collectionStatusTimeoutSecs{360};
 
-        l_app.add_option("--retryLimit, -r", l_retryLimit, "Retry limit");
-        l_app.add_option("--sleepDurationInSeconds, -s",
-                         l_sleepDurationInSeconds,
-                         "Sleep duration in seconds between each retry");
+        l_app.add_option("--collectionStatusTimeout, -s",
+                         l_collectionStatusTimeoutSecs,
+                         "VPD collection status timeout");
 
         CLI11_PARSE(l_app, argc, argv);
 
@@ -207,10 +113,14 @@ int main(int argc, char** argv)
         PrimeInventory l_primeObj;
         l_primeObj.primeSystemBlueprint();
 
-        return collectAllFruVpd()
-                   ? checkVpdCollectionStatus(l_retryLimit,
-                                              l_sleepDurationInSeconds)
-                   : vpd::constants::VALUE_1;
+        // trigger FRU VPD collection and check for status
+        CollectionOrchestrator l_collectionOrchestrator{
+            BUSNAME, OBJPATH, IFACE, "CollectAllFRUVPD",
+            l_collectionStatusTimeoutSecs};
+
+        l_collectionOrchestrator.triggerFruVpdCollectionAndCheckStatus();
+
+        return vpd::constants::VALUE_0;
     }
     catch (const std::exception& l_ex)
     {
