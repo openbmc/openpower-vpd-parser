7b3b285ab323e37fbea4fd7c405a3948c4cf877b
Souvik Roy <souvikroyofficial10@gmail.com>
2026-02-26 06:11:26 +0000

    Add class to trigger and monitor FRU VPD collection
    This commit adds a new class to trigger and monitor FRU VPD collection
    from wait-vpd-parsers service. This class will encapsulate all methods
    and data members required to trigger and monitor FRU VPD collection
    using async based approach. Async based approach is intended to
    eliminate following issues encountered on certain systems:
    - D-Bus timeout issues
    - Unnecessary CPU usage in polling based design
    
    This commit only contains stub implementation of the class methods and
    actual implementation will be added in future commits
    
    Change-Id: I3c5924a7ddfeada31bbc18319851cd89f45ffc04
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

---
+++ wait-vpd-parser/src/fru_vpd_collection_manager.cpp
@@ -0,0 +1,123 @@
+#include "config.h"
+
+#include "fru_vpd_collection_manager.hpp"
+
+#include "utility/dbus_utility.hpp"
+
+#include <format>
+
+int FruVpdCollectionManager::triggerFruVpdCollectionAndCheckStatus()
+{
+    int l_rc{vpd::constants::VALUE_1};
+    try
+    {
+        /*TODO:
+            - Do timer based async D-Bus call with callback to trigger FRU VPD
+           collection
+            - Run I/O context loop with a timeout (blocking call)
+            - After I/O context loop exits, check for error encountered, timer
+           expiry
+            - Incase of error encountered, timer expiry, etc, throw exception so
+           that service fails
+            - Else return success
+        */
+    }
+    catch (const std::exception& l_ex)
+    {
+        m_ioContext.stop();
+
+        m_logger->logMessage(
+            std::format("Failed to trigger all FRU VPD collection. Error: {}",
+                        l_ex.what()));
+
+        m_state = State::Failed;
+
+        throw l_ex;
+    }
+    return l_rc;
+}
+
+void FruVpdCollectionManager::collectAllFruVpdCallback(
+    [[maybe_unused]] boost::system::error_code i_ec,
+    [[maybe_unused]] sdbusplus::message_t& i_msg) noexcept
+{
+    try
+    {
+        /* TODO:
+            - Check for timeout or error
+            - If no error or timeout
+                - if message indicates collection was triggered successfully
+                    -  start a timer based listener on collection status
+           property
+        */
+    }
+    catch (const std::exception& l_ex)
+    {
+        m_logger->logMessage(std::format(
+            "Error processing collect all FRU VPD callback: {}", l_ex.what()));
+
+        m_state = State::Failed;
+
+        m_ioContext.stop();
+    }
+}
+
+void FruVpdCollectionManager::registerVpdCollectionStatusListener() noexcept
+{
+    try
+    {
+        m_state = State::Listening;
+
+        /* TODO:
+         - Register a listener on collection status property
+        */
+    }
+    catch (const std::exception& l_ex)
+    {
+        m_logger->logMessage(std::format(
+            "Failed to register listener on FRU VPD collection status property. Error: {}",
+            l_ex.what()));
+
+        m_state = State::Failed;
+    }
+}
+
+void FruVpdCollectionManager::vpdCollectionStatusCallback(
+    [[maybe_unused]] sdbusplus::message_t& i_msg) noexcept
+{
+    try
+    {
+        /* TODO:
+            - Process the message to check if collection status is complete
+            - If yes,
+             - Update state member variable to Completed
+             - Stop timer
+             - Stop I/O context
+        */
+    }
+    catch (const std::exception& l_ex)
+    {
+        m_logger->logMessage(std::format(
+            "Failed to process VPD collection status callback. Error : {}",
+            l_ex.what()));
+    }
+}
+
+void FruVpdCollectionManager::handleTimerExpiry(
+    [[maybe_unused]] const boost::system::error_code& i_errorCode) noexcept
+{
+    try
+    {
+        /* TODO:
+        - check error code to see why timer expired
+        - update state member variable accordingly
+        - stop I/O context loop
+        */
+    }
+    catch (const std::exception& l_ex)
+    {
+        m_logger->logMessage(
+            std::format("Failed to handle collection timer expiry. Error : {}",
+                        l_ex.what()));
+    }
+}
