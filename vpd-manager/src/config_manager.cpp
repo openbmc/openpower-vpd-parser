69a211136ba86c7f9e4206f0d3d3aa135aadd129
Souvik Roy <souvikroyofficial10@gmail.com>
2026-03-12 05:31:36 +0000

    ConfigManager:method stubs to build maps
    This commit implements stub private method in ConfigManager class to
    build maps for a given FRU. This commit also implements
    buildChassisToFruMap method which builds EEPROM to chassis map and
    chassis ID to JSON map for all FRUs in the system configuration JSON.
    
    Change-Id: I046909ab5cd10850bc90bf5f4508a3cfec159d99
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/config_manager.cpp
+++ vpd-manager/src/config_manager.cpp
@@ -1,7 +1,10 @@
 #include "config_manager.hpp"
 
 #include "constants.hpp"
+#include "exceptions.hpp"
+#include "utility/common_utility.hpp"
 
+#include <format>
 namespace vpd
 {
 const nlohmann::json& ConfigManager::getJsonObj(
@@ -49,6 +52,78 @@ std::string ConfigManager::getChassisId(
             "Failed to extract chassis ID from given path {}, error: {}",
             i_inventoryObjPath, l_ex.what()));
         return std::string{};
+    }
+}
+
+void ConfigManager::buildChassisToFruMap()
+{
+    if (!m_systemConfigJson.contains("frus"))
+    {
+        throw JsonException{"System config JSON is invalid"};
+    }
+
+    // Iterate through "frus" under system config JSON
+    const nlohmann::json& l_listOfFrus =
+        m_systemConfigJson["frus"].get_ref<const nlohmann::json::object_t&>();
+
+    // build JSON object which is common across all chassis JSONs
+    //  Check if commonInterfaces exists in system config JSON and add to the
+    //  entry if yes
+    nlohmann::json l_commonJsonObj;
+    if (!m_systemConfigJson.contains("commonInterfaces"))
+    {
+        m_logger->logMessage(
+            "commonInterfaces not found in system config JSON");
+    }
+    else
+    {
+        // if the chassis JSON doesn't have "commonInterfaces", append it
+        l_commonJsonObj["commonInterfaces"] =
+            m_systemConfigJson["commonInterfaces"];
+    }
+
+    // Build maps for each FRU sequentially
+    for (const auto& l_fruJsonObj : l_listOfFrus.items())
+    {
+        const auto l_mapBuildResult =
+            l_commonJsonObj.empty()
+                ? buildMapsForFru(l_fruJsonObj)
+                : buildMapsForFru(l_fruJsonObj, l_commonJsonObj);
+
+        if (!l_mapBuildResult.has_value())
+        {
+            // Log the error but continue processing other FRUs
+            m_logger->logMessage(std::format(
+                "Failed to build map for FRU {}. Error code: {}",
+                l_fruJsonObj.key(),
+                commonUtility::getErrCodeMsg(l_mapBuildResult.error())));
+        }
+    }
+}
+
+std::expected<bool, error_code> ConfigManager::buildMapsForFru(
+    [[maybe_unused]] const auto& i_fruJsonObj,
+    [[maybe_unused]] std::optional<nlohmann::json> i_commonJsonObj) noexcept
+{
+    try
+    {
+        bool l_rc{true};
+        /*  TODO:
+            - extract chassis ID from inventory path of base FRU at index 0
+            - create entry in EEPROM to chassis ID map
+            - create entry in chassis ID to JSON map
+            - append sub FRU array to chassis JSON map
+            - append common JSON object if any to chassis JSON object
+        */
+
+        return l_rc;
+    }
+    catch (const std::exception& l_ex)
+    {
+        m_logger->logMessage(
+            std::format("Failed to build maps for FRU {}. Error: {}",
+                        i_fruJsonObj.key(), l_ex.what()));
+        return std::unexpected(error_code::STANDARD_EXCEPTION);
     }
 }
 } // namespace vpd
