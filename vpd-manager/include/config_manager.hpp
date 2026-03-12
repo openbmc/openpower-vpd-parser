e028ab2212693f0e1d474b8743426cf10e6d0cf8
Souvik Roy <souvikroyofficial10@gmail.com>
2026-03-12 05:31:36 +0000

    ConfigManager:method stubs to build maps
    This commit implements stub private methods in ConfigManager class to
    - extract chassis ID from object path
    - build maps for a given FRU
    
    These methods are required by public API buildChassisToFruMap
    
    Change-Id: I046909ab5cd10850bc90bf5f4508a3cfec159d99
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/config_manager.hpp
+++ vpd-manager/include/config_manager.hpp
@@ -1,9 +1,13 @@
 #pragma once
+#include "error_codes.hpp"
+#include "logger.hpp"
 
 #include <nlohmann/json.hpp>
 
+#include <expected>
 #include <map>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 
 namespace vpd
@@ -55,7 +59,8 @@ class ConfigManager final
      */
     explicit ConfigManager([[maybe_unused]] const WorkerPassKey& i_key,
                            const nlohmann::json& i_systemConfigJson) :
-        m_systemConfigJson{i_systemConfigJson}
+        m_systemConfigJson{i_systemConfigJson},
+        m_logger{Logger::getLoggerInstance()}
     {
         buildChassisToFruMap();
     }
@@ -85,18 +90,39 @@ class ConfigManager final
      *
      * @throw std::runtime_error
      */
-    void buildChassisToFruMap()
-    {
-        /* TODO:
-          1. Iterate through "frus" under system config JSON
-            1.i. For each FRU, iterate through the sub FRUS
-                  1.i.i. For each FRU, extract Chassis ID using Object path at
-          index 0, and build EEPROM to Chassis ID Map. 1.i.ii. For each sub FRU,
-          use the object path to get the chassis ID, and add the sub JSON to the
-          Chassis ID to Chassis JSON Map.
+    void buildChassisToFruMap();
 
-        */
-    }
+    /**
+     * @brief API to extract chassis ID string from a given object path
+     *
+     * This API extracts the chassis ID string from a given object path.
+     * For eg. object path
+     * /xyz/openbmc_project/inventory/system/chassis0/motherboard/connector2 has
+     * chassis ID "chassis0"
+     *
+     * @param[in] i_objPath - Object path
+     *
+     * @return On success, returns the chassis ID string, otherwise returns
+     * error code
+     */
+    std::expected<std::string_view, error_code> getChassisIdFromObjectPath(
+        const std::string_view i_objPath) const noexcept;
+
+    /**
+     * @brief API to build EEPROM to chassis ID map and chassis ID to JSON map
+     * for a single FRU
+     *
+     * This API builds maps for a single FRU in the system
+     * config JSON.
+     *
+     * @param[in] i_eepromPath - EEPROM path
+     * @param[in] i_fruJson - FRU JSON object
+     *
+     * @return On success, returns true, otherwise sets error code
+     */
+    std::expected<bool, error_code> buildMapsForFru(
+        const std::string& i_eepromPath,
+        const nlohmann::json& i_fruJson) noexcept;
 
     // System config JSON
     const nlohmann::json& m_systemConfigJson;
@@ -107,6 +133,9 @@ class ConfigManager final
 
     // EEPROM path to chassis ID - O(1) lookup
     std::unordered_map<std::string, std::string> m_eepromToChassisIdMap;
+
+    // Shared pointer to Logger object
+    std::shared_ptr<Logger> m_logger;
 };
 
 } // namespace vpd
