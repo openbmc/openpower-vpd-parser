f83f3f359d45d53e848893e1a6fd976ff09ce8de
Souvik Roy <souvikroyofficial10@gmail.com>
2026-03-16 18:32:24 +0530

    ConfigManager:Implement getChassisIdFromObjectPath
    This commit implements the getChassisIdFromObjectPath method which
    extracts the chassis ID from a given inventory object path. This API is
    required by other methods in the class.
    
    Test:
    ```
    - added debug logs to print EEPROM to chassis ID map to journal
    - installed BMC image on PST simics
    - verified the log against the system config JSON to see all EEPROMs in
      the JSON are correctly mapped to respective chassis ID
    ```
    
    Change-Id: I6b2da9d7b89eaf74e24d44dc7fa0d0919d32f14e
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/config_manager.cpp
+++ vpd-manager/src/config_manager.cpp
@@ -34,15 +34,47 @@ void ConfigManager::buildChassisToFruMap()
 
 std::expected<std::string_view, error_code>
     ConfigManager::getChassisIdFromObjectPath(
-        [[maybe_unused]] const std::string_view i_objPath) const noexcept
+        const std::string_view i_objPath) const noexcept
 {
     try
     {
-        std::string_view l_chassisId;
+        if (i_objPath.empty())
+        {
+            return std::unexpected(error_code::INVALID_INPUT_PARAMETER);
+        }
 
-        /* TODO
-            - extract chassis ID string from the object path
-        */
+        // Find system inventory path in the object path
+        const auto l_systemInvPathPos =
+            i_objPath.find(constants::systemInvPath);
+        if (l_systemInvPathPos == std::string_view::npos)
+        {
+            return std::unexpected(error_code::INVALID_INVENTORY_PATH);
+        }
+
+        // Extract portion after system inventory path
+        const auto l_afterSystemPath = i_objPath.substr(
+            l_systemInvPathPos +
+            std::string_view(constants::systemInvPath).length());
+
+        if (l_afterSystemPath.empty() || l_afterSystemPath[0] != '/')
+        {
+            return std::unexpected(error_code::INVALID_INVENTORY_PATH);
+        }
+
+        // Skip leading '/' and find next '/' to extract chassis ID
+        const auto l_pathAfterSlash = l_afterSystemPath.substr(1);
+        const auto l_nextSlashPos = l_pathAfterSlash.find('/');
+
+        std::string_view l_chassisId =
+            (l_nextSlashPos != std::string_view::npos)
+                ? l_pathAfterSlash.substr(0, l_nextSlashPos)
+                : l_pathAfterSlash;
+
+        if (l_chassisId.empty())
+        {
+            return std::unexpected(error_code::INVALID_INVENTORY_PATH);
+        }
+
         return l_chassisId;
     }
     catch (const std::exception& l_ex)
@@ -61,9 +93,38 @@ std::expected<bool, error_code> ConfigManager::buildMapsForFru(
     try
     {
         bool l_rc{true};
+
+        // Validate FRU JSON is an array with at least one element
+        if (!i_fruJson.is_array() || i_fruJson.empty())
+        {
+            m_logger->logMessage(std::format(
+                "FRU {} has no sub-FRUs or invalid format", i_eepromPath));
+            return std::unexpected(error_code::INVALID_INPUT_PARAMETER);
+        }
+
+        // Extract chassis ID from inventory path of base FRU at index 0
+        const auto l_baseInvObjPath =
+            i_fruJson.at(0).value("inventoryPath", "");
+        if (l_baseInvObjPath.empty())
+        {
+            m_logger->logMessage(std::format(
+                "Base object path is empty for FRU {}", i_eepromPath));
+            return std::unexpected(error_code::INVALID_INPUT_PARAMETER);
+        }
+
+        const auto l_chassisIdRes =
+            getChassisIdFromObjectPath(l_baseInvObjPath);
+        if (!l_chassisIdRes.has_value())
+        {
+            return std::unexpected(l_chassisIdRes.error());
+        }
+
+        const auto& l_chassisId = l_chassisIdRes.value();
+
+        // Create entry in EEPROM to chassis ID map
+        m_eepromToChassisIdMap.emplace(i_eepromPath, std::string(l_chassisId));
+
         /*  TODO:
-            - extract chassis ID from inventory path of base FRU at index 0
-            - create entry in EEPROM to chassis ID map
             - create entry in chassis ID to JSON map
             - get commonInterfaces JSON object from system config JSON and add
            in the entry
