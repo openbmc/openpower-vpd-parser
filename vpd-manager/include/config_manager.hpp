3bc62ec4585b0bfb469a251df7e073f6e5d13794
Anupama B R <anupama.b.r1@ibm.com>
2026-03-16 00:41:45 -0500

    ConfigManager: Add getChassisId API
    This commit implements a helper API getChassisId to extract the chassis
    identifier from an inventory object path.
    The API declaration and definition are added to ConfigManager and it is
    used in getJsonObj API to determine the appropriate chassis
    configuration.
    
    Change-Id: I7a707ce696f0842753de58f3954a2473ce786cf8
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/include/config_manager.hpp
+++ vpd-manager/include/config_manager.hpp
@@ -113,6 +113,16 @@ class ConfigManager final
         */
     }
 
+    /**
+     * @brief Extract chassis ID from given i_inventoryObjPath.
+     *
+     * @param[in] i_inventoryObjPath - Inventory object path.
+     *
+     * @return Chassis ID on successful extraction, empty string otherwise.
+     */
+    std::string getChassisId(
+        const std::string& i_inventoryObjPath) const noexcept;
+
     // System config JSON
     const nlohmann::json& m_systemConfigJson;
 
