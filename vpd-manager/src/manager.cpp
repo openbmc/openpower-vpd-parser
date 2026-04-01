a2f4c1b2e3a0adfde2906430d914d7e77e233dd1
Rekha Aparna <vrekhaaparna@ibm.com>
2026-03-31 23:58:55 -0500

    Skip deletion of system and logical BMC paths
    This commit updates deleteAllFRUVPD API to remove all paths under the
    PIM service, excluding the "system" and "logical_bmc" paths.
    
    These paths contain critical data and must not be removed during
    cleanup. This change ensures safe deletion while preserving required
    system information.
    
    Change-Id: I3e5a121d975353c0b5d17b59c8fbd251735764ea
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- vpd-manager/src/manager.cpp
+++ vpd-manager/src/manager.cpp
@@ -17,6 +17,8 @@
 #include <sdbusplus/bus/match.hpp>
 #include <sdbusplus/message.hpp>
 
+#include <format>
+
 namespace vpd
 {
 Manager::Manager(
@@ -895,7 +897,7 @@ void Manager::deleteAllFRUVPD() const noexcept
 
         const auto l_inventoryBackupPath{
             constants::pimPrimaryPath /
-            std::filesystem::path(constants::systemInvPath).relative_path()};
+            std::filesystem::path(constants::pimPath).relative_path()};
 
         if (!std::filesystem::exists(l_inventoryBackupPath))
         {
@@ -905,41 +907,25 @@ void Manager::deleteAllFRUVPD() const noexcept
         }
 
         bool l_directoryRemoved = false;
+        uint16_t l_errCode = 0;
 
-        for (const auto& l_entry :
-             std::filesystem::directory_iterator(l_inventoryBackupPath))
-        {
-            // ToDo -- Remove the logical BMC check when path is moved
-            // to power-he-platform daemon.
-            if (std::filesystem::is_directory(l_entry) &&
-                l_entry.path().filename().compare("logical_bmc") !=
-                    vpd::constants::STR_CMP_SUCCESS)
-            {
-                std::error_code l_ec;
-                std::filesystem::remove_all(l_entry, l_ec);
-
-                if (l_ec)
-                {
-                    m_logger->logMessage("Failed to delete directory : " +
-                                         l_entry.path().string() + " error : " +
-                                         std::string(l_ec.message()));
-
-                    continue;
-                }
-
-                l_directoryRemoved = true;
-            }
-        }
+        commonUtility::deleteDirectory(l_inventoryBackupPath,
+                                       l_directoryRemoved, l_errCode);
 
         if (!l_directoryRemoved)
         {
+            if (l_errCode)
+            {
+                m_logger->logMessage(
+                    std::format("Failed to delete directory {}, error : {}.",
+                                l_inventoryBackupPath.string(),
+                                commonUtility::getErrCodeMsg(l_errCode)));
+            }
             // Since no directories were removed, skip restarting of service.
             return;
         }
 
-        uint16_t l_errCode = 0;
-
-        constexpr auto l_numRetries{3};
+        constexpr auto l_numRetries{constants::VALUE_3};
 
         for (unsigned l_attempt = 0; l_attempt < l_numRetries; ++l_attempt)
         {
