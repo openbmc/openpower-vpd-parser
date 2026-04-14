a79632084d72e08ffefa10d748bba25e41eea6ee
Chanchal Tiwari <chanchaltiwari70021@gmail.com>
2026-04-13 23:57:13 -0500

    Add ThreadManager class infrastructure
    This commit introduces the 'ThreadManager' class in the vpd-manager
    repo with basic functionality .The class is now designed to hold an
    object the ConfigManager class, allowing it to access public APIs
    exposed by this class.
    
    Change-Id: Ia81685faedfcd8299d2ec9819b01036289f36918
    Signed-off-by: Chanchal Tiwari <chanchaltiwari70021@gmail.com>

---
+++ vpd-manager/include/thread_manager.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "config_manager.hpp"
+#include "worker.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <memory>
+#include <optional>
+#include <string>
+
+namespace vpd
+{
+
+/**
+ * @brief
+ *
+ * This class acts as a central mediator between the Manager and Worker layers,
+ * It encapsulates the entire threading domain,ensuring that both the Manager
+ * and Worker classes remain entirely agnostic of thread execution and
+ * synchronization details.
+ *
+ * Its primary responsibilities include:
+ * - **Concurrency Abstraction**: Starting and Managing chassis-specific thread
+ * pools and task scheduling.
+ * - **State Management**: Implementing skip logic for redundant requests,
+ * completion tracking, and failure handling.
+ * - **Asynchronous Communication**: Monitoring background tasks and
+ * publishing status (In Progress, Failed, Completed) to D-Bus.
+ *
+ * By strictly isolating the threading logic here, the architecture
+ * achieves loose coupling, allowing the classes on data processing
+ * without awareness of the underlying execution model.
+ */
+
+class ThreadManager
+{
+  public:
+    /**
+     * @brief ThreadManager Constructor
+     */
+    ThreadManager(std::shared_ptr<Worker>& i_worker,
+                  std::shared_ptr<Worker>& i_configManager);
+
+    // deleted methods
+    ThreadManager(const ThreadManager&) = delete;
+    ThreadManager& operator=(const ThreadManager&) = delete;
+    ThreadManager(ThreadManager&&) = delete;
+    ThreadManager& operator=(ThreadManager&&) = delete;
+
+    /**
+     * @brief Destructor
+     */
+    ~ThreadManager() = default;
+
+    /**
+     *  @brief API to get the json from given path
+     *
+     *  @param[in] i_vpdPath - Optional EEPROM or inventory object path.
+     */
+    const nlohmann::json& getConfigJson(
+        const std::optional<std::string>& i_vpdPath) const noexcept;
+
+  private:
+    // Shared pointer to ConfigManager object
+    const std::shared_ptr<ConfigManager>& m_configManager{nullptr};
+
+    // shared pointer to worker object
+    const std::shared_ptr<Worker>& m_worker{nullptr};
+};
+
+} // namespace vpd
