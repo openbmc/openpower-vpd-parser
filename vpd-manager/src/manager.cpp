3e4e15fed50532c2b902b1f0eb0738a6024a4a8c
Rekha Aparna <vrekhaaparna@ibm.com>
2026-05-27 02:08:30 -0500

    Update validateRedundantEeprom API
    This commit updates validateRedundantEeprom API to use chassis-based
    configuration JSON instead of system configuration JSON, improves
    lookup efficiency for the input VPD path.
    
    Change-Id: I7fc4c5adba2d1f03466aa1b665ac64e44cc97559
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- vpd-manager/src/manager.cpp
+++ vpd-manager/src/manager.cpp
@@ -949,11 +949,20 @@ bool Manager::validateRedundantEeprom(const types::Path& i_fruPath) const
 {
     bool l_rc{false};
 
-    auto l_sysCfgJsonObj = m_worker->getSysCfgJsonObj();
+    if (!m_configManager)
+    {
+        m_logger->logMessage(std::format(
+            "Failed to validate redundant Eeprom path for [{}] because ConfigManager is not initialized",
+            i_fruPath));
+        return l_rc;
+    }
+
+    // get the chassis specific JSON for the object path
+    const auto& l_jsonObj = m_configManager->getJsonObj(i_fruPath);
 
     uint16_t l_errCode;
     std::string l_redundantEeprom = jsonUtility::getRedundantEepromPathFromJson(
-        l_sysCfgJsonObj, i_fruPath, l_errCode);
+        l_jsonObj, i_fruPath, l_errCode);
 
     if (l_redundantEeprom.empty())
     {
@@ -967,8 +976,7 @@ bool Manager::validateRedundantEeprom(const types::Path& i_fruPath) const
 
     try
     {
-        Parser l_parserForPrimary(i_fruPath, l_sysCfgJsonObj,
-                                  m_vpdCollectionMode);
+        Parser l_parserForPrimary(i_fruPath, l_jsonObj, m_vpdCollectionMode);
 
         return l_parserForPrimary.compareData(l_redundantEeprom);
     }
