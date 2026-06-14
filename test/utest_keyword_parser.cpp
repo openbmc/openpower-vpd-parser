c632cceeb113e769b575b82e688c2e547861867a
Rekha Aparna <vrekhaaparna@ibm.com>
2026-06-14 00:26:19 -0500

    WriteKeywordOnHardware Support for keyword VPD
    This commit implements an API writeKeywordOnHardware for keyword VPD
    type. This adds support to write keyword value on hardware for keyword
    VPD type from WriteKeywordOnHardware API exposed under vpd-manager
    service.
    
    ```
    Test:
    
    Value before updating on hardware
    root@huygens:~# busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager ReadKeyword sv /sys/bus/i2c/drivers/at24/0-0050/eeprom s "Z0"
    v ay 6 0 17 37 193 0 0
    
    Updating value on hardware
    root@huygens:~# busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager WriteKeywordOnHardware sv /sys/bus/i2c/drivers/at24/0-0050/eeprom "(say)" "Z0" 6 0x00 0x11 0x25 0xc1 0x00 0x01
    i 6
    
    After updating value on hardware
    root@huygens:~#  busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager ReadKeyword sv /sys/bus/i2c/drivers/at24/0-0050/eeprom s "Z0"
    v ay 6 0 17 37 193 0 1
    ```
    
    Change-Id: I328fbd698e624a5b569d4a9dba2916fc4ff921d3
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- test/utest_keyword_parser.cpp
+++ test/utest_keyword_parser.cpp
@@ -139,7 +139,7 @@ TEST(KeywordVpdParserTest, EmptyInput)
 {
     // Blank keyword VPD
     types::BinaryVector emptyVector{};
-    KeywordVpdParser l_keywordParser(std::move(emptyVector));
+    KeywordVpdParser l_keywordParser(std::move(emptyVector), "");
 
     EXPECT_THROW(l_keywordParser.parse(), std::exception);
 }
