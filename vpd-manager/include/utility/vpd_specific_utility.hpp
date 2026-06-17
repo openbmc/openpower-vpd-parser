0721dd4e26f8c2b4b93d7e8b3603d1fb1924b165
Rekha Aparna <vrekhaaparna@ibm.com>
2026-06-17 07:39:56 -0500

    Update sub-FRU inventory paths on keyword update
    Currently, when a VPD keyword is updated, only the base inventory path
    associated with the FRU is updated on D-Bus, resulting in inconsistent
    values across sub-FRUs.
    
    This commit updates the code to update the value for all inventory
    paths of the FRU based on the inherit, copyRecord and skiprecord tags
    defined in the configuration JSON.
    
    ```
    Test:
    Applied patchset on PST simics
    When keyword is getting updated observed that value is updated only for the inventory paths of the associated FRU that has inherit true or if the value is part of copyRecords tag in JSON
    ```
    
    Change-Id: I2888dd497bfdb514e7426fc3d499d0fcce10ae02
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- vpd-manager/include/utility/vpd_specific_utility.hpp
+++ vpd-manager/include/utility/vpd_specific_utility.hpp
@@ -13,6 +13,7 @@
 #include <utility/event_logger_utility.hpp>
 
 #include <filesystem>
+#include <format>
 #include <fstream>
 #include <ranges>
 #include <regex>
@@ -2011,5 +2012,134 @@ inline std::string expandMtsLocationCode(uint16_t& o_errCode)
     return l_expanded.replace(l_pos, 3, l_tmKwdValue + "." + l_seKwdValue);
 }
 
+/**
+ * @brief Update keyword value on D-Bus for all associated inventory paths.
+ *
+ * This API updates the keyword value on DBus for all inventory paths
+ * associated with the given FRU path. It iterates through all inventory paths
+ * for the given EEPROM path and updates the property if:
+ * - The "inherit" tag is true for the inventory path, OR
+ * - The record is part of the "copyRecords" tag and not in "skipRecords" list
+ *
+ * @param[in] i_fruPath - EEPROM path of the FRU.
+ * @param[in] i_paramsToWriteData - Write VPD parameters
+ * @param[in] i_sysCfgJsonObj - Config JSON object.
+ * @param[out] o_errCode - To set error code incase of failure.
+ */
+inline void updateKeywordOnDBus(
+    const std::string& i_fruPath,
+    const types::WriteVpdParams& i_paramsToWriteData,
+    const nlohmann::json& i_sysCfgJsonObj, uint16_t& o_errCode)
+{
+    o_errCode = 0;
+    try
+    {
+        if (i_fruPath.empty())
+        {
+            o_errCode = error_code::INVALID_INPUT_PARAMETER;
+            return;
+        }
+
+        if (!i_sysCfgJsonObj.contains("frus"))
+        {
+            o_errCode = error_code::INVALID_JSON;
+            return;
+        }
+
+        if (!i_sysCfgJsonObj["frus"].contains(i_fruPath))
+        {
+            o_errCode = error_code::FRU_PATH_NOT_FOUND;
+            return;
+        }
+
+        const types::IpzData* l_ipzData =
+            std::get_if<types::IpzData>(&i_paramsToWriteData);
+
+        if (!l_ipzData)
+        {
+            o_errCode = error_code::UNSUPPORTED_VPD_TYPE;
+            return;
+        }
+
+        //  iterate through all inventory paths for given EEPROM path,
+        //  if for an inventory path, "inherit" tag is true OR the record
+        //  is part of "copyRecords" tag, update the inventory path's
+        //  com.ibm.ipzvpd.<record>,keyword property
+
+        types::ObjectMap l_objectInterfaceMap;
+
+        auto l_populateInterfaceMap = [&l_objectInterfaceMap,
+                                       &l_ipzData = std::as_const(l_ipzData)](
+                                          const auto& l_inventoryItem) {
+            if (!l_ipzData)
+            {
+                return;
+            }
+
+            // check if record is part of copyRecords list.
+            const bool l_isPartOfCopyRecord =
+                l_inventoryItem.contains("copyRecords") &&
+                std::find(l_inventoryItem["copyRecords"].begin(),
+                          l_inventoryItem["copyRecords"].end(),
+                          std::get<0>(*l_ipzData)) !=
+                    l_inventoryItem["copyRecords"].end();
+
+            // check if record is part of skipRecord list
+            const bool l_isPartOfSkipRecord =
+                l_inventoryItem.contains("skipRecords") &&
+                std::find(l_inventoryItem["skipRecords"].begin(),
+                          l_inventoryItem["skipRecords"].end(),
+                          std::get<0>(*l_ipzData)) !=
+                    l_inventoryItem["skipRecords"].end();
+
+            // update for inherited FRUs or if the record is part of
+            // copyRecord tag
+            if (l_inventoryItem.value("inherit", true) ||
+                (l_isPartOfCopyRecord && !l_isPartOfSkipRecord))
+            {
+                l_objectInterfaceMap.emplace(
+                    sdbusplus::object_path{l_inventoryItem["inventoryPath"]},
+                    types::InterfaceMap{
+                        {constants::ipzVpdInf + std::get<0>(*l_ipzData),
+                         types::PropertyMap{{std::get<1>(*l_ipzData),
+                                             std::get<2>(*l_ipzData)}}}});
+            }
+        };
+
+        // iterate through all inventory paths of the FRU.
+        std::for_each(i_sysCfgJsonObj["frus"][i_fruPath].begin(),
+                      i_sysCfgJsonObj["frus"][i_fruPath].end(),
+                      l_populateInterfaceMap);
+
+        if (!l_objectInterfaceMap.empty())
+        {
+            if (!dbusUtility::publishVpdOnDBus(std::move(l_objectInterfaceMap)))
+            {
+                o_errCode = error_code::DBUS_FAILURE;
+                return;
+            }
+        }
+    }
+    catch (const std::exception& l_ex)
+    {
+        o_errCode = error_code::STANDARD_EXCEPTION;
+
+        std::string l_recordKeywordInfo;
+        const types::IpzData* l_ipzData =
+            std::get_if<types::IpzData>(&i_paramsToWriteData);
+
+        if (l_ipzData)
+        {
+            l_recordKeywordInfo =
+                std::format("[{}] : [{}]", std::get<0>(*l_ipzData),
+                            std::get<1>(*l_ipzData));
+        }
+
+        Logger::getLoggerInstance()->logMessage(std::format(
+            "Failed to update keyword [{}] on DBus for path [{}], error : {}.",
+            l_recordKeywordInfo, i_fruPath, l_ex.what()));
+        return;
+    }
+}
 } // namespace vpdSpecificUtility
 } // namespace vpd
