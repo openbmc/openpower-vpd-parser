7d5d8285efa3e1519eed3bd81f254c427a6814ef
Souvik Roy <souvikroyofficial10@gmail.com>
2026-06-30 23:56:12 +0530

    json_utility: use ConfigManager for tag-processing
    This commit refactors three functions used in the tag dispatch table
    (processSystemCmdTag, processGpioPresenceTag, procesSetGpioTag) to use
    ConfigManager instance to fetch the respective configuration JSON
    internally instead of relying on caller to pass the system configuration
    JSON. Using ConfigManager to fetch the configuration JSON ensures that
    the JSON is pre-validated which eliminates redundant JSON validation
    code in each json utility API. This process also ensures that only the
    chassis-specific JSON is fetched which improves key look-up time.
    
    This commit also updates meson.build file of test directory and
    wait-vpd-parsers to resolve linker failures, as test and
    wait-vpd-parsers both have include dependencies on vpd-manager/include
    and hence need to see definitions of ConfigManager as json_utility.hpp
    now includes config_manager.hpp
    
    Test:
    ```
    - preAction tag test:
     installed BMC image in PST simics, observed preAction successfully
     executed for the FRUs for which preAction is defined and VPD is
     collected once preAction is complete
    ```
    
    Change-Id: Ideedbd4c5595ab42b03049d139dfc811a64db965
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/gpio_monitor.cpp
+++ vpd-manager/src/gpio_monitor.cpp
@@ -74,7 +74,7 @@ void GpioEventHandler::handleTimerExpiry(
 
     uint16_t l_errCode = 0;
     bool l_currentPresencePinValue = jsonUtility::processGpioPresenceTag(
-        m_configJson, m_fruPath, "pollingRequired", "hotPlugging", l_errCode);
+        m_fruPath, "pollingRequired", "hotPlugging", l_errCode);
 
     if (l_errCode && l_errCode != error_code::DEVICE_NOT_PRESENT)
     {
@@ -101,7 +101,7 @@ void GpioEventHandler::setEventHandlerForGpioPresence(
 {
     uint16_t l_errCode = 0;
     m_prevPresencePinValue = jsonUtility::processGpioPresenceTag(
-        m_configJson, m_fruPath, "pollingRequired", "hotPlugging", l_errCode);
+        m_fruPath, "pollingRequired", "hotPlugging", l_errCode);
 
     if (l_errCode && l_errCode != error_code::DEVICE_NOT_PRESENT)
     {
