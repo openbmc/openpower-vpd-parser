9badefe8dfa13cbf02f50e6b8c13373be5f640c1
Rekha Aparna <vrekhaaparna@ibm.com>
2026-06-30 09:26:55 -0500

    vpd-tool: Make record option as optional
    This commit updates record option to optional for read and write
    operations in vpd-tool. This enables support to perform read and write
    operations on VPD type which does not have records.
    
    Updated the help text for read and write operations for keyword VPD
    format.
    
    ```
    Test:
    
    root@huygens:~# vpd-tool -r -H -O /sys/bus/i2c/drivers/at24/0-0050/eeprom -K Z0
    {
        "/sys/bus/i2c/drivers/at24/0-0050/eeprom": {
            "Z0": "0x001125c10000"
        }
    }
    ```
    
    Change-Id: I78393579f67806f94ddd975745b6582af16c4c0a
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- vpd-tool/include/vpd_tool.hpp
+++ vpd-tool/include/vpd_tool.hpp
@@ -252,7 +252,7 @@ class VpdTool
     int readKeyword(const std::string& i_vpdPath,
                     const std::string& i_recordName,
                     const std::string& i_keywordName, const bool i_onHardware,
-                    const std::string& i_fileToSave = {});
+                    const std::string& i_fileToSave = {}) const noexcept;
 
     /**
      * @brief Dump the given inventory object in JSON format to console.
@@ -295,11 +295,11 @@ class VpdTool
      * get updated, also redundant EEPROM(if any) path with new keyword's value.
      *
      * In case of hardware path, only given hardware path gets updated with new
-     * keyword’s value, any backup or redundant EEPROM (if exists) paths won't
+     * keyword's value, any backup or redundant EEPROM (if exists) paths won't
      * get updated.
      *
      * @param[in] i_vpdPath - DBus object path or EEPROM path.
-     * @param[in] i_recordName - Record name.
+     * @param[in] i_recordName - Record name
      * @param[in] i_keywordName - Keyword name.
      * @param[in] i_keywordValue - Keyword value.
      * @param[in] i_onHardware - True if i_vpdPath is EEPROM path, false
@@ -310,7 +310,7 @@ class VpdTool
     int writeKeyword(std::string i_vpdPath, const std::string& i_recordName,
                      const std::string& i_keywordName,
                      const std::string& i_keywordValue,
-                     const bool i_onHardware) noexcept;
+                     const bool i_onHardware) const noexcept;
 
     /**
      * @brief Reset specific keywords on System VPD to default value.
--- vpd-tool/src/vpd_tool.cpp
+++ vpd-tool/src/vpd_tool.cpp
@@ -34,7 +34,7 @@ const types::BiosAttributeKeywordMap VpdTool::m_biosAttributeVpdKeywordMap = {
 int VpdTool::readKeyword(
     const std::string& i_vpdPath, const std::string& i_recordName,
     const std::string& i_keywordName, const bool i_onHardware,
-    const std::string& i_fileToSave)
+    const std::string& i_fileToSave) const noexcept
 {
     int l_rc = constants::FAILURE;
     try
@@ -42,11 +42,29 @@ int VpdTool::readKeyword(
         types::DbusVariantType l_keywordValue;
         if (i_onHardware)
         {
-            l_keywordValue = utils::readKeywordFromHardware(
-                i_vpdPath, std::make_tuple(i_recordName, i_keywordName));
+            // For hardware read, support both IPZ format (with record) and
+            // Keyword format (without record)
+            if (i_recordName.empty())
+            {
+                // Keyword VPD format - read keyword directly
+                l_keywordValue =
+                    utils::readKeywordFromHardware(i_vpdPath, i_keywordName);
+            }
+            else
+            {
+                // IPZ VPD format - read keyword from record
+                l_keywordValue = utils::readKeywordFromHardware(
+                    i_vpdPath, std::make_tuple(i_recordName, i_keywordName));
+            }
         }
         else
         {
+            if (i_recordName.empty())
+            {
+                throw std::runtime_error(
+                    "Record name is required for DBus read operations.");
+            }
+
             std::string l_inventoryObjectPath(
                 constants::baseInventoryPath + i_vpdPath);
 
@@ -110,8 +128,11 @@ int VpdTool::readKeyword(
     {
         // TODO: Enable logging when verbose is enabled.
         std::cerr << "Read keyword's value failed for path: " << i_vpdPath
-                  << ", Record: " << i_recordName << ", Keyword: "
-                  << i_keywordName << ", error: " << l_ex.what() << std::endl;
+                  << (!i_recordName.empty()
+                          ? (std::format(", Record : {}", i_recordName))
+                          : "")
+                  << ", Keyword: " << i_keywordName
+                  << ", error: " << l_ex.what() << std::endl;
     }
     return l_rc;
 }
@@ -423,28 +444,45 @@ int VpdTool::fixSystemVpd() const noexcept
 int VpdTool::writeKeyword(
     std::string i_vpdPath, const std::string& i_recordName,
     const std::string& i_keywordName, const std::string& i_keywordValue,
-    const bool i_onHardware) noexcept
+    const bool i_onHardware) const noexcept
 {
     int l_rc = constants::FAILURE;
     try
     {
-        if (i_vpdPath.empty() || i_recordName.empty() ||
-            i_keywordName.empty() || i_keywordValue.empty())
+        if (i_vpdPath.empty() || i_keywordName.empty() ||
+            i_keywordValue.empty())
         {
             throw std::runtime_error("Received input is empty.");
         }
 
-        auto l_paramsToWrite =
-            std::make_tuple(i_recordName, i_keywordName,
-                            utils::convertToBinary(i_keywordValue));
-
         if (i_onHardware)
         {
+            types::WriteVpdParams l_paramsToWrite;
+            if (i_recordName.empty())
+            {
+                l_paramsToWrite = std::make_tuple(
+                    i_keywordName, utils::convertToBinary(i_keywordValue));
+            }
+            else
+            {
+                l_paramsToWrite =
+                    std::make_tuple(i_recordName, i_keywordName,
+                                    utils::convertToBinary(i_keywordValue));
+            }
             l_rc = utils::writeKeywordOnHardware(i_vpdPath, l_paramsToWrite);
         }
         else
         {
+            if (i_recordName.empty())
+            {
+                throw std::runtime_error(
+                    "Record name is required for DBus write operations.");
+            }
+
             i_vpdPath = constants::baseInventoryPath + i_vpdPath;
+            auto l_paramsToWrite =
+                std::make_tuple(i_recordName, i_keywordName,
+                                utils::convertToBinary(i_keywordValue));
             l_rc = utils::writeKeyword(i_vpdPath, l_paramsToWrite);
         }
 
@@ -458,7 +496,9 @@ int VpdTool::writeKeyword(
     {
         // TODO: Enable log when verbose is enabled.
         std::cerr << "Write keyword's value for path: " << i_vpdPath
-                  << ", Record: " << i_recordName
+                  << (!i_recordName.empty()
+                          ? std::format(", Record : {}", i_recordName)
+                          : "")
                   << ", Keyword: " << i_keywordName
                   << " is failed. Exception: " << l_ex.what() << std::endl;
     }
