dfb62a9767aa5fc8b6679e49ab43060361ee7c4d
Souvik Roy <souvikroyofficial10@gmail.com>
2026-07-08 09:01:05 +0000

    json_utility: refactor getVPDOffset
    This commit refactors getVPDOffset json utility API to use ConfigManager
    instance to fetch respective configuration JSON rather than depending on
    caller to pass the system configuration JSON. Using ConfigManager to
    fetch the configuration JSON ensures that the JSON is pre-validated
    which eliminates redundant JSON validation code in each json utility
    API. This process also ensures that only the chassis-specific JSON is
    fetched which improves key look-up time.
    
    Test:
    ```
    - Genesis boot
      Build BMC image and start PST simics with image. Observe BMC reaches
      Ready, system VPD is collected and FRUs VPD is also collected
    - Reboot BMC
      Reboot BMC at standby, after reboot observe BMC reaches
      Ready, system VPD is collected and FRUs VPD is also collected
    - UpdateKeyword
      Execute vpd-manager D-Bus API updateKeyword and observe its working as
      expected
    - UpdateKeywordOnHardware
      Execute vpd-manager D-Bus API updateKeywordOnHardware and observe its
      working as expected
    - ReadKeyword
      Execute vpd-manager D-Bus API readKeyword and observe its working as
      expected
    - deleteSingleFruVpd
      Execute vpd-manager D-Bus API deleteSingleFruVpd
      and observe its working as expected
    ```
    
    Change-Id: I286a1c2c370e73fdb90dbb16276fe20a4dc3ee79
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/parser.cpp
+++ vpd-manager/src/parser.cpp
@@ -50,17 +50,13 @@ Parser::Parser(const std::string& i_vpdFilePath, nlohmann::json i_parsedJson,
     }
 
     // Read VPD offset if applicable.
-    if (!m_parsedJson.empty())
-    {
-        m_vpdStartOffset =
-            jsonUtility::getVPDOffset(m_parsedJson, m_vpdFilePath, l_errorCode);
+    m_vpdStartOffset = jsonUtility::getVPDOffset(m_vpdFilePath, l_errorCode);
 
-        if (l_errorCode)
-        {
-            m_logger->logMessage(
-                "Failed to get vpd offset for path [" + m_vpdFilePath +
-                "], error: " + commonUtility::getErrCodeMsg(l_errorCode));
-        }
+    if (l_errorCode)
+    {
+        m_logger->logMessage(std::format(
+            "VPD offset for path [{}] not found. {}. Assuming offset as 0",
+            m_vpdFilePath, commonUtility::getErrCodeMsg(l_errorCode)));
     }
 }
 
