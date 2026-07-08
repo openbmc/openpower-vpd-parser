2e0eb33caaf1239a32afcc0a90d6c9f56d82dd23
Souvik Roy <souvikroyofficial10@gmail.com>
2026-07-08 09:44:30 +0000

    json_utility: refactor isBackupAndRestoreRequired
    This commit refactors isBackupAndRestoreRequired json utility API to use
    ConfigManager instance to fetch respective configuration JSON rather
    than depending on caller to pass the system configuration JSON. Using
    ConfigManager to fetch the configuration JSON ensures that the JSON is
    pre-validated which eliminates redundant JSON validation code in each
    json utility API. This process also ensures that only the
    chassis-specific JSON is fetched which improves key look-up time.
    
    Test:
    ```
    ```
    
    Change-Id: Id497000933cba810560bb74cb1bfffab98c592fc
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/oem-handler/ibm_handler.cpp
+++ vpd-manager/oem-handler/ibm_handler.cpp
@@ -145,8 +145,7 @@ void IbmHandler::initBackupAndRestore() noexcept
                     m_configJsonPath);
             }
 
-            if (!jsonUtility::isBackupAndRestoreRequired(m_sysCfgJsonObj,
-                                                         l_errCode))
+            if (!jsonUtility::isBackupAndRestoreRequired(l_errCode))
             {
                 if (l_errCode)
                 {
@@ -1144,8 +1143,8 @@ void IbmHandler::setDeviceTreeAndJson(
 
         if (!l_sysVpdObjMap.empty())
         {
-            if (isBackupOnCache() && jsonUtility::isBackupAndRestoreRequired(
-                                         m_sysCfgJsonObj, l_errCode))
+            if (isBackupOnCache() &&
+                jsonUtility::isBackupAndRestoreRequired(l_errCode))
             {
                 performBackupAndRestore(o_parsedSystemVpdMap);
             }
