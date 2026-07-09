cc92271d02aae72b662e89532ab312366ee96243
Souvik Roy <souvikroyofficial10@gmail.com>
2026-07-09 04:18:19 +0000

    json_utility: refactor isFruReplaceableAtRuntime
    This commit refactors isFruReplaceableAtRuntime json utility API to use
    ConfigManager instance to fetch respective configuration JSON rather
    than depending on caller to pass the system configuration JSON. Using
    ConfigManager to fetch the configuration JSON ensures that the JSON is
    pre-validated which eliminates redundant JSON validation code in each
    json utility API. This process also ensures that only the
    chassis-specific JSON is fetched which improves key look-up time.
    
    Test:
    ```
    ```
    
    Change-Id: Idd421841b13135c3417c83c762bcdc5c32d8afc7
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/src/worker.cpp
+++ vpd-manager/src/worker.cpp
@@ -1570,8 +1570,7 @@ void Worker::collectSingleFruVpd(const sdbusplus::object_path& i_dbusObjPath)
         {
             uint16_t l_errCode = 0;
             bool isFruReplaceableAtRuntime =
-                jsonUtility::isFruReplaceableAtRuntime(m_parsedJson, l_fruPath,
-                                                       l_errCode);
+                jsonUtility::isFruReplaceableAtRuntime(l_fruPath, l_errCode);
 
             if (l_errCode)
             {
@@ -1606,8 +1605,7 @@ void Worker::collectSingleFruVpd(const sdbusplus::object_path& i_dbusObjPath)
             }
 
             bool isFruReplaceableAtRuntime =
-                jsonUtility::isFruReplaceableAtRuntime(m_parsedJson, l_fruPath,
-                                                       l_errCode);
+                jsonUtility::isFruReplaceableAtRuntime(l_fruPath, l_errCode);
 
             if (l_errCode)
             {
