53f49d6f320683d60314ca9997161ea9d88d6a88
Souvik Roy <souvikroyofficial10@gmail.com>
2026-07-09 04:17:52 +0000

    json_utility: refactor isFruPowerOffOnly
    This commit refactors isFruPowerOffOnly json utility API to use
    ConfigManager instance to fetch respective configuration JSON rather
    than depending on caller to pass the system configuration JSON. Using
    ConfigManager to fetch the configuration JSON ensures that the JSON is
    pre-validated which eliminates redundant JSON validation code in each
    json utility API. This process also ensures that only the
    chassis-specific JSON is fetched which improves key look-up time.
    
    This commit also disables unit tests written for this API for now as
    they won't work with this refactoring. These
    unit tests need to be re-written and will be handled in subsequent
    commits.
    
    Test:
    ```
    - Genesis boot
      Build BMC image and start PST simics with image. Observe BMC reaches
      Ready, system VPD is collected and FRUs VPD is also collected, also
      observe BackupAndRestore is triggered
    - Reboot BMC
      Reboot BMC at standby, after reboot observe BMC reaches
      Ready, system VPD is collected and FRUs VPD is also collected
       and observe BackupAndRestore is triggered
    - Execute reset-reload and see power off only FRU collection is skipped
    
    ```
    
    Change-Id: Ie74eed08ca20d38b33c949ca95e43d7c397b861b
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- test/utest_json_utility.cpp
+++ test/utest_json_utility.cpp
@@ -12,20 +12,9 @@ using namespace vpd;
 TEST(IsFruPowerOffOnlyTest, PositiveTestCase)
 {
     uint16_t l_errCode = 0;
-    const std::string l_jsonPath{"/usr/local/share/vpd/50001001.json"};
     const std::string l_vpdPath{"/sys/bus/spi/drivers/at25/spi12.0/eeprom"};
-    const nlohmann::json l_parsedJson =
-        jsonUtility::getParsedJson(l_jsonPath, l_errCode);
 
-    if (l_errCode)
-    {
-        Logger::getLoggerInstance()->logMessage(
-            "Failed to parse JSON file [" + l_jsonPath +
-            "], error : " + commonUtility::getErrCodeMsg(l_errCode));
-    }
-
-    const bool l_result =
-        jsonUtility::isFruPowerOffOnly(l_parsedJson, l_vpdPath, l_errCode);
+    const bool l_result = jsonUtility::isFruPowerOffOnly(l_vpdPath, l_errCode);
 
     if (l_errCode)
     {
@@ -40,20 +29,9 @@ TEST(IsFruPowerOffOnlyTest, PositiveTestCase)
 TEST(IsFruPowerOffOnlyTest, NegativeTestCase)
 {
     uint16_t l_errCode = 0;
-    const std::string l_jsonPath{"/usr/local/share/vpd/50001001.json"};
     const std::string l_vpdPath{"/sys/bus/i2c/drivers/at24/4-0050/eeprom"};
-    const nlohmann::json l_parsedJson =
-        jsonUtility::getParsedJson(l_jsonPath, l_errCode);
 
-    if (l_errCode)
-    {
-        Logger::getLoggerInstance()->logMessage(
-            "Failed to parse JSON file [" + l_jsonPath +
-            "], error : " + commonUtility::getErrCodeMsg(l_errCode));
-    }
-
-    const bool l_result =
-        jsonUtility::isFruPowerOffOnly(l_parsedJson, l_vpdPath, l_errCode);
+    const bool l_result = jsonUtility::isFruPowerOffOnly(l_vpdPath, l_errCode);
 
     if (l_errCode)
     {
