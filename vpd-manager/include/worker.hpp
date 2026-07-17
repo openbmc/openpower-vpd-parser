1afe5ef805b192786faa407851b2feeb31e230b6
Anupama B R <anupama.b.r1@ibm.com>
2026-07-17 03:38:34 -0500

    Remove config JSON parsing from Worker
    The Worker class previously accepted a config JSON file path in its
    constructor, parsed the JSON there, and held a reference to the path
    as a member. This created tight coupling between object construction
    and JSON loading.
    
    As config JSON is now handled by the ConfigManager, all other required
    classes must get the config JSON from ConfigManager.
    
    Includes the following changes,
    * Refactored Worker APIs parseVpdFile, populateDbus,
    collectSingleFruVpd to take config JSON as input, removed the path-based
    constructor argument.
    * IbmHandler doesn’t take Worker object in constructor, instead
    constructs Worker object locally wherever is required.
    * IbmBiosHandler gets config JSON from the ConfigManager directly.
    
    ```
    Test:
    
    Verified system VPD is collected.
    Verified all FRUs VPD are collected.
    Verified manager exposed API CollectFRUVPD is working.
    Verified manager exposed API DeleteFRUVPD is working.
    Verified system VPD gets collected when system is in "File Mode".
    Verified 'vpd-parser' application collects VPD from a given file path,
      with and without a JSON path as input.
    Verified BIOS handler syncs values between VPD and BIOS table whenever
      there is any update in VPD or in BIOS table.
    
    ```
    
    Change-Id: I7056443a9e2014f3c34344a529b343629a23130d
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/include/worker.hpp
+++ vpd-manager/include/worker.hpp
@@ -44,15 +44,13 @@ class Worker
      * Constructor will also, based on symlink pick the correct JSON and
      * initialize the parsed JSON variable.
      *
-     * @param[in] pathToConfigJSON - Path to the config JSON, if applicable.
      * @param[in] i_vpdCollectionMode - Mode in which VPD collection should take
      * place.
      *
      * Note: Throws std::exception in case of construction failure. Caller needs
      * to handle to detect successful object creation.
      */
-    Worker(std::string pathToConfigJson = std::string(),
-           types::VpdCollectionMode i_vpdCollectionMode =
+    Worker(types::VpdCollectionMode i_vpdCollectionMode =
                types::VpdCollectionMode::DEFAULT_MODE);
 
     /**
@@ -63,6 +61,7 @@ class Worker
     /**
      * @brief API to parse VPD data
      *
+     * @param[in] i_configJsonObj - Config json object.
      * @param[in] i_vpdFilePath - Path to the VPD file.
      * @param[in] i_processRedundant - Enables VPD collection for redundant
      * EEPROM path.
@@ -73,9 +72,9 @@ class Worker
      * only the FRU pre-action is performed and VPD collection, post-action
      * actions are skipped.
      */
-    types::VPDMapVariant parseVpdFile(const std::string& i_vpdFilePath,
-                                      const bool& i_processRedundant,
-                                      bool& o_presenceState);
+    types::VPDMapVariant parseVpdFile(
+        const nlohmann::json& i_configJsonObj, const std::string& i_vpdFilePath,
+        const bool& i_processRedundant, bool& o_presenceState);
 
     /**
      * @brief An API to populate DBus interfaces for a FRU.
@@ -83,11 +82,13 @@ class Worker
      * Note: Call this API to populate D-Bus. Also caller should handle empty
      * objectInterfaceMap.
      *
+     * @param[in] i_configJsonObj - Config json object.
      * @param[in] parsedVpdMap - Parsed VPD as a map.
      * @param[out] objectInterfaceMap - Object and its interfaces map.
      * @param[in] vpdFilePath - EEPROM path of FRU.
      */
-    void populateDbus(const types::VPDMapVariant& parsedVpdMap,
+    void populateDbus(const nlohmann::json& i_configJsonObj,
+                      const types::VPDMapVariant& parsedVpdMap,
                       types::ObjectMap& objectInterfaceMap,
                       const std::string& vpdFilePath);
 
@@ -135,9 +136,11 @@ class Worker
      * current state of the system matches with the state at which the FRU is
      * allowed for VPD recollection.
      *
+     * @param[in] i_configJsonObj - Config Json object.
      * @param[in] i_dbusObjPath - D-bus object path
      */
-    void collectSingleFruVpd(const sdbusplus::object_path& i_dbusObjPath);
+    void collectSingleFruVpd(const nlohmann::json& i_configJsonObj,
+                             const sdbusplus::object_path& i_dbusObjPath);
 
     /**
      * @brief  Perform VPD recollection
@@ -489,7 +492,7 @@ class Worker
     nlohmann::json m_parsedJson{};
 
     // Path to config JSON if applicable.
-    std::string& m_configJsonPath;
+    std::string m_configJsonPath{};
 
     // List of EEPROM paths for which VPD collection thread creation has failed.
     std::forward_list<std::string> m_failedEepromPaths;
--- vpd-manager/src/manager.cpp
+++ vpd-manager/src/manager.cpp
@@ -166,7 +166,7 @@ Manager::Manager(
         readVpdCollectionMode();
 
         m_ibmHandler = std::make_shared<IbmHandler>(
-            m_worker, m_backupAndRestoreObj, m_interface, m_progressInterface,
+            m_backupAndRestoreObj, m_interface, m_progressInterface,
             m_ioContext, m_asioConnection, m_vpdCollectionMode);
 
         // once IBM handler is initialized, the symlink points to the
@@ -176,7 +176,6 @@ Manager::Manager(
         m_configManager =
             ConfigManager::initialize(l_configMgrKey, INVENTORY_JSON_SYM_LINK);
 #else
-        m_worker = std::make_shared<Worker>(INVENTORY_JSON_DEFAULT);
         m_progressInterface->set_property(
             "Status", std::string(constants::vpdCollectionCompleted));
 #endif
@@ -537,10 +536,19 @@ void Manager::collectSingleFruVpd(const sdbusplus::object_path& i_dbusObjPath)
         return;
     }
 
-    if (m_worker.get() != nullptr)
+    const auto l_configJsonResult =
+        m_configManager->getJsonObj(std::string(i_dbusObjPath));
+
+    if (!l_configJsonResult.has_value())
     {
-        m_worker->collectSingleFruVpd(i_dbusObjPath);
+        logging::logMessage(std::format(
+            "Failed to get JSON for path {}. Error: {}, can't collect VPD.",
+            std::string(i_dbusObjPath),
+            commonUtility::getErrCodeMsg(l_configJsonResult.error())));
     }
+
+    Worker{}.collectSingleFruVpd(l_configJsonResult.value().get(),
+                                 i_dbusObjPath);
 }
 
 void Manager::deleteSingleFruVpd(const sdbusplus::object_path& i_dbusObjPath)
