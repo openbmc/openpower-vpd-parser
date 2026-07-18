a86655f071955ab526310f4d9952721e23cd3968
Anupama B R <anupama.b.r1@ibm.com>
2026-07-18 15:35:57 +0000

    Remove PowerVS and Single FAB support
    PowerVS and Single FAB support is no longer required. Removing all code
    related to these features.
    
    Change-Id: I1b0827cb6de5bd3ae2bfa24d3045177816fc33d2
    Signed-off-by: Anupama B R <anupama.b.r1@ibm.com>

--- vpd-manager/oem-handler/ibm_handler.hpp
+++ vpd-manager/oem-handler/ibm_handler.hpp
@@ -139,26 +139,6 @@ class IbmHandler
     void resetNonSystemInvPaths(types::ObjectMap& io_objectMap) const noexcept;
 
     /**
-     * @brief API to check and update PowerVS VPD.
-     *
-     * The API will read the existing data from the DBus and if found
-     * different than what has been read from JSON, it will update the VPD with
-     * JSON data on hardware and DBus both.
-     *
-     * @param[in] i_powerVsJsonObj - PowerVS JSON object.
-     * @param[out] o_failedPathList - List of path failed to update.
-     */
-    void checkAndUpdatePowerVsVpd(const nlohmann::json& i_powerVsJsonObj,
-                                  std::vector<std::string>& o_failedPathList);
-    /**
-     * @brief API to handle configuration w.r.t. PowerVS systems.
-     *
-     * Some FRUs VPD is specific to powerVS system. The API detects the
-     * powerVS configuration and updates the VPD accordingly.
-     */
-    void ConfigurePowerVsSystem();
-
-    /**
      * @brief API to perform initial setup before manager claims Bus name.
      *
      * Before BUS name for VPD-Manager is claimed, fitconfig would be set for
