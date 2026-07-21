ed40666d89e419715555bc796117bf7a258f947f
Alpana Kumari <alpankum@in.ibm.com>
2026-07-21 12:19:20 -0500

    Dbus API GetParsedVPD support
    Added GetParsedVPD D-Bus method to com.ibm.VPD.Manager interface.
    
    The API accepts a FRU EEPROM path and returns the parsed VPD as a
    variant of:
      - a{sa{say}} (DbusIPZVpdMap)  for IPZ format VPD
      - a{sv}      (DbusKeywordVpdMap) for Keyword format VPD
    
    Added ParsedVpdMap, DbusIPZVpdMap, DbusKeywordVpdMap type
    definitions to represent the D-Bus returnable VPD map variant.
    
    Test:
    - Patch applied on PST simics.
    - Verified GetParsedVPD API, working fine.
    ```
    busctl call com.ibm.VPD.Manager /com/ibm/VPD/Manager com.ibm.VPD.Manager GetParsedVPD s "/sys/bus/i2c/drivers/at24/8-0053/eeprom"
    v a{sa{say}} 7 "DINF" 3 "FL" 20 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 "RI" 4 0 0 0 0 "RT" 4 68 73 78 70 "UTIL" 21 "D0" 1 0 "D1" 1 0 "D2" 1 0 "D3" 1 0 "D4" 4 0 0 0 0 "D5" 4 0 0 0 0 "D6" 4 0 0 0 0 "D7" 4 0 0 0 0 "D8" 8 0 0 0 0 0 0 0 0 "D9" 8 0 0 0 0 0 0 0 0 "F0" 8 0 0 0 0 0 0 0 0 "F1" 8 0 0 0 0 0 0 0 0 "F2" 8 0 0 0 0 0 0 0 0 "F3" 8 0 0 0 0 0 0 0 0 "F4" 8 0 0 0 0 0 0 0 0 "F5" 16 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "F6" 16 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "F7" 16 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "F8" 16 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "F9" 16 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "RT" 4 85 84 73 76 "VCEN" 7 "DR" 6 67 69 67 32 32 32 "FC" 8 53 55 48 71 45 48 48 49 "RB" 4 32 32 32 32 "RG" 4 32 32 32 32 "RT" 4 86 67 69 78 "SE" 7 49 50 51 52 53 54 70 "TM" 8 53 55 48 71 45 48 48 49 "VINI" 14 "B3" 6 0 0 0 0 0 0 "B4" 1 0 "B7" 12 0 0 0 0 0 0 0 0 0 0 0 0 "CC" 4 55 57 68 54 "CE" 1 49 "CT" 4 64 179 2 7 "DR" 16 69 66 77 67 32 83 87 73 84 67 72 32 67 65 82 68 "FN" 7 70 50 54 48 50 49 48 "HE" 4 48 48 48 49 "HW" 2 0 1 "PN" 7 80 78 49 50 51 52 53 "RT" 4 86 73 78 73 "SN" 12 89 76 55 57 68 54 48 49 48 48 48 48 "VZ" 2 48 52 "VMPU" 5 "DI" 4 0 0 0 0 "IN" 128 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "RT" 4 86 77 80 85 "SO" 2 0 0 "VZ" 2 48 49 "VSBP" 4 "DR" 16 86 80 68 32 82 79 79 84 32 78 79 68 69 32 32 32 "IM" 4 112 0 16 0 "PA" 1 89 "RT" 4 86 83 66 80 "VSYS" 22 "AA" 8 0 0 0 0 0 0 0 0 "AB" 8 0 0 0 0 0 0 0 0 "AK" 8 0 0 0 0 0 0 0 0 "AT" 32 46 46 46 46 46 46 46 46 46 83 69 82 86 69 82 45 50 69 52 67 45 48 48 49 45 66 65 68 67 79 70 69 "BA" 1 0 "BN" 64 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 "BR" 2 83 48 "DR" 6 83 89 83 84 69 77 "FV" 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 "ID" 2 32 32 "MM" 1 1 "MN" 7 32 32 32 32 32 32 32 "NN" 16 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 32 "RB" 4 48 48 48 49 "RG" 4 32 32 32 32 "RT" 4 86 83 89 83 "SE" 7 83 73 77 80 49 48 82 "SG" 7 32 32 32 32 32 32 32 "SU" 6 0 4 172 30 68 44 "TM" 8 57 49 48 53 45 52 50 65 "TN" 8 32 32 32 32 32 32 32 32 "WN" 12 67 48 53 48 55 54 48 66 57 54 52 69
    ```
    
    Change-Id: I69650a33ff57fc453d9b30516dabb3bd4d17ac47
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

--- vpd-manager/include/manager.hpp
+++ vpd-manager/include/manager.hpp
@@ -264,6 +264,18 @@ class Manager
     void deleteAllFRUVPD() const noexcept;
 
     /**
+     * @brief Get parsed VPD for a given FRU EEPROM path.
+     *
+     * @param[in] i_fruPath - FRU EEPROM path.
+     *
+     * @throw xyz.openbmc_project.Common.Error.InvalidArgument
+     * @throw xyz.openbmc_project.Common.Device.Error.ReadFailure
+     *
+     * @return Parsed VPD as ParsedVpdMap variant.
+     */
+    types::ParsedVpdMap getParsedVpd(const types::Path& i_fruPath);
+
+    /**
      * @brief Checks whether the primary EEPROM's VPD matches that of its
      * redundant EEPROM.
      *
