a92fed407e4c36ac39c40aa31280d398e39c82ae
Alpana Kumari <alpankum@in.ibm.com>
2026-07-21 02:54:13 -0500

    Replace logging::logMessage with Logger::logMessage
    Now that the Logger class is established, remove the shim entirely and
    migrate all call-sites to the canonical Logger::logMessage API.
    
    Changes:
    - Replace all logging::logMessage call-sites across
      vpd-manager sources with the appropriate Logger call
    
    Change-Id: I20ca2c8428f512fbfee10686c86a7a82d8a89436
    Signed-off-by: Alpana Kumari <alpankum@in.ibm.com>

--- vpd-manager/src/vpd_parser_main.cpp
+++ vpd-manager/src/vpd_parser_main.cpp
@@ -48,7 +48,8 @@ int main(int argc, char** argv)
 
         CLI11_PARSE(app, argc, argv);
 
-        vpd::logging::logMessage("VPD file path received" + vpdFilePath);
+        vpd::Logger::getLoggerInstance()->logMessage(
+            "VPD file path received" + vpdFilePath);
 
         // VPD file path is a mandatory parameter to execute any parser.
         if (vpdFilePath.empty())
@@ -62,7 +63,7 @@ int main(int argc, char** argv)
         // Below are two different ways of parsing the VPD.
         if (!configFilePath.empty())
         {
-            vpd::logging::logMessage(
+            vpd::Logger::getLoggerInstance()->logMessage(
                 "Processing with config file - " + configFilePath);
 
             uint16_t l_errCode{0};
@@ -107,7 +108,7 @@ int main(int argc, char** argv)
     }
     catch (const std::exception& ex)
     {
-        vpd::logging::logMessage(ex.what());
+        vpd::Logger::getLoggerInstance()->logMessage(ex.what());
         return -1;
     }
 
