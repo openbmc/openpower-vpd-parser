42bbc81f45debd0f5c491a3aa305807c43ad974c
Rekha Aparna <vrekhaaparna@ibm.com>
2026-08-02 22:49:11 -0500

    vpd-tool: Block operations during VPD collection
    Added a check to verify the VPD collection status before executing any
    vpd-tool operation. If VPD collection is in progress, the requested
    operation is rejected to prevent conflicts with the collection process.
    
    ```
    Test:
    Applied patch on the PST simics
    Verified that `vpd-tool` operations are blocked while VPD collection is in progress.
    ```
    
    Change-Id: Iab719a7f4c2c47190f855452abd4cb0c9375db34
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

--- vpd-tool/include/tool_constants.hpp
+++ vpd-tool/include/tool_constants.hpp
@@ -62,6 +62,10 @@ constexpr auto validateRedundantEepromMethod = "ValidateRedundantEEPROM";
 
 constexpr auto KwdIM = "IM";
 constexpr auto badVpdPath = "/var/lib/vpd/dumps";
+constexpr auto vpdCollectionInterface = "xyz.openbmc_project.Common.Progress";
+constexpr auto vpdCollectionStatusProperty = "Status";
+constexpr auto vpdCollectionInProgress =
+    "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress";
 
 // Valid IM values list.
 static std::vector<std::string> validImValues{
