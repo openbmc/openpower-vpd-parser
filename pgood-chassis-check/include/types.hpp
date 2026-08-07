6263ccddc2cfe8b7e4d33d9fd72094ad1a9c40a3
Souvik Roy <souvikroyofficial10@gmail.com>
2026-08-07 12:31:26 +0000

    Implement chassis poweron check service
    This commit implements a service to check if local chassis is powered
    on. When local chassis is powered on, and Active BMC is rebooted, FRUs
    VPD recollection should not be triggered. This service runs on the
    Active BMC, reads the local BMC position to decide which pgood GPIO to
    read and then reads the respective pgood GPIO. If the pgood GPIO
    indicates local chassis is powered on, it dictates
    wait-vpd-parsers.service to not trigger FRUs VPD collection, and to only
    update the overall FRUs VPD collection status to "Completed".
    
    Note:
    1. this commit only includes stub changes for the service logic. The
       actual implementation of the service will be handled in subsequent
       commit.
    2. wait-vpd-parsers changes will be introduced using subsequent commit.
    
    Test:
    ```
    - Install BMC image on PST simics
    - Run PST simics in sled_cfg=0x03 (Chassis 0,1,2 present)
    - Genesis boot:
      Once BMC comes up as Active, other as Passive, check PowerState
    property on Active BMC:
    
     root@huygens:~# busctl get-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system xyz.openbmc_project.State.Decorator.PowerState PowerState
    s "xyz.openbmc_project.State.Decorator.PowerState.State.Off"
    
    -  Reset reload test:
    	Once Active BMC reaches Ready state, trigger poweron using obmcutil
    	Once poweron is successful, trigger reboot on the Active BMC
    	Observe Passive BMC is unaffected as expected.
            Check the PowerState property on the Active BMC:
    
     root@huygens:~# busctl get-property xyz.openbmc_project.Inventory.Manager /xyz/openbmc_project/inventory/system xyz.openbmc_project.State.Decorator.PowerState PowerState
    s "xyz.openbmc_project.State.Decorator.PowerState.State.On"
    
    ```
    
    Change-Id: I78bc7beb9091530f0cf0d7b5fc18043f5058691d
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

---
+++ pgood-chassis-check/include/types.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <xyz/openbmc_project/State/Decorator/PowerState/common.hpp>
+
+namespace pgood_chassis_check
+{
+using PowerStateIface =
+    sdbusplus::common::xyz::openbmc_project::state::decorator::PowerState;
+}; // namespace pgood_chassis_check
