dcd93a72d6e43fa2015cced9f0f2cdc6113c5e99
Souvik Roy <souvikroyofficial10@gmail.com>
2026-08-07 12:31:26 +0000

    Introduce chassis poweron check service
    This commit introduces a service to check if local chassis with BMC is
    powered on. This service reads the BMC position to decide which pgood
    GPIO to read and then reads the respective pgood GPIO to determine the
    power state. It then publishes the D-Bus property "PowerState" on
    "/system" inventory path of inventory manager service so that any
    service interested in knowing the chassis power state can read it from
    the "PowerState" property on inventory manager service.
    
    Note:
    1. This commit only includes stub changes for the service logic. The
       actual implementation of the service will be handled in subsequent
       commit.
    2. On IBM systems, the chassis power state is indicated by a GPIO called
       pgood GPIO. The specific pgood GPIO representing the chassis power
       state  depends on the BMC position.
    
    Test:
    ```
    - Install BMC image on PST simics
    - Genesis boot:
      Once BMCs come up as Active, other as Passive, check
      Active BMC and see the sequence of service launch is
    	1. pgood-chassis-check
    	2. wait-vpd-parsers
    ```
    
    Change-Id: I78bc7beb9091530f0cf0d7b5fc18043f5058691d
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

---
+++ pgood-chassis-check/include/constants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace pgood_chassis_check
+{
+namespace constants
+{
+constexpr auto success = 0;
+constexpr auto failure = 1;
+} // namespace constants
+
+} // namespace pgood_chassis_check
