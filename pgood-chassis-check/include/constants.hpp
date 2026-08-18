723b339e1c8837cfa5768fc9209e6b45a2ab34bb
Souvik Roy <souvikroyofficial10@gmail.com>
2026-08-18 15:31:38 +0000

    pgood-chassis-check: read BMC position from D-Bus
    This commit implements private method to read the BMC position from
    D-Bus. The BMC position helps determine which pgood GPIO should be read
    in order to determine local chassis power state. In case of any failure
    while reading the position from D-Bus, this method returns invalid
    value.
    
    publishChassisPowerState() and main() remain as stubs to be
    filled in the next commit.
    
    Change-Id: Ibdd93cb766876de326d68c6f3f28da1f2809b491
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- pgood-chassis-check/include/constants.hpp
+++ pgood-chassis-check/include/constants.hpp
@@ -6,6 +6,15 @@ namespace constants
 {
 constexpr auto success = 0;
 constexpr auto failure = 1;
+
+constexpr auto systemVpdInvPath = "/xyz/openbmc_project/inventory/system";
+constexpr auto pimService = "xyz.openbmc_project.Inventory.Manager";
+
+//@todo: use PDI generated header once Position interface gets merged upstream
+constexpr auto positionInterface =
+    "xyz.openbmc_project.Inventory.Decorator.Position";
+constexpr auto positionPropertyName = "Position";
+
 } // namespace constants
 
 } // namespace pgood_chassis_check
