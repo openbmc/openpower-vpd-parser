99c0d31ac7cbbc6a6b5f54cc57b26d80e5117bec
Souvik Roy <souvikroyofficial10@gmail.com>
2026-08-18 15:38:35 +0000

    pgood-chassis-check:publish PowerState property
    This commit implements private method to read pgood GPIO which
    indicates local chassis power state and publish "On" or "Off" on
    PowerState property on "/system" inventory path of inventory manager
    service.
    
    Change-Id: I3b8b5fdf11e9e5c9b11a537917ac524681667b74
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- pgood-chassis-check/include/constants.hpp
+++ pgood-chassis-check/include/constants.hpp
@@ -6,14 +6,29 @@ namespace constants
 {
 constexpr auto success = 0;
 constexpr auto failure = 1;
+<<<<<<< HEAD
 constexpr auto systemVpdInvPath = "/xyz/openbmc_project/inventory/system";
 constexpr auto pimService = "xyz.openbmc_project.Inventory.Manager";
+<<<<<<< HEAD
 
 //@todo: use PDI generated header once Position interface gets merged upstream
 constexpr auto positionInterface =
     "xyz.openbmc_project.Inventory.Decorator.Position";
 constexpr auto positionPropertyName = "Position";
 
+=======
+=======
+constexpr auto bmcPositionFile = "/run/openbmc/bmc_position";
+constexpr auto gpioLineBmc0 = "power-good-chassis1";
+constexpr auto gpioLineBmc1 = "power-good-chassis2";
+constexpr auto consumerName = "pgood-chassis-check";
+
+// D-Bus constants for the PIM Notify call
+constexpr auto pimServiceName = "xyz.openbmc_project.Inventory.Manager";
+constexpr auto pimPath = "/xyz/openbmc_project/inventory";
+constexpr auto pimInterface = "xyz.openbmc_project.Inventory.Manager";
+>>>>>>> b5e421a (pgood-chassis-check:publish PowerState property)
+>>>>>>> eb28bd9 (pgood-chassis-check:publish PowerState property)
 } // namespace constants
 
 } // namespace pgood_chassis_check
--- pgood-chassis-check/include/types.hpp
+++ pgood-chassis-check/include/types.hpp
@@ -4,12 +4,19 @@
 #include <xyz/openbmc_project/Logging/Entry/common.hpp>
 #include <xyz/openbmc_project/State/Decorator/PowerState/common.hpp>
 
+#include <map>
+#include <string>
+#include <variant>
+
 namespace pgood_chassis_check
 {
 namespace types
 {
 using PowerStateIface =
     sdbusplus::common::xyz::openbmc_project::state::decorator::PowerState;
+using PropertyMap = std::map<std::string, std::variant<std::string>>;
+using InterfaceMap = std::map<std::string, PropertyMap>;
+using ObjectMap = std::map<sdbusplus::object_path, InterfaceMap>;
 
 using CreateIface = sdbusplus::common::xyz::openbmc_project::logging::Create;
 using EntryIface = sdbusplus::common::xyz::openbmc_project::logging::Entry;
