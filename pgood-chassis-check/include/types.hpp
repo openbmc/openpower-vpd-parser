89ce5ed4787ef23238ef60d715f9eae7ddaeee73
Souvik Roy <souvikroyofficial10@gmail.com>
2026-08-18 15:31:38 +0000

    pgood-chassis-check: read BMC position from D-Bus
    This commit implements private method to read the BMC position from
    D-Bus. The BMC position helps determine which pgood GPIO should be read
    in order to determine local chassis power state. In case of any failure
    while reading the position from file, this method returns invalid value.
    
    publishChassisPowerState() and main() remain as stubs to be
    filled in the next commit.
    
    Change-Id: Ibdd93cb766876de326d68c6f3f28da1f2809b491
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- pgood-chassis-check/include/types.hpp
+++ pgood-chassis-check/include/types.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <com/ibm/VPD/error.hpp>
+#include <xyz/openbmc_project/Logging/Create/common.hpp>
+#include <xyz/openbmc_project/Logging/Entry/common.hpp>
 #include <xyz/openbmc_project/State/Decorator/PowerState/common.hpp>
 
 namespace pgood_chassis_check
@@ -8,6 +11,10 @@ namespace types
 using PowerStateIface =
     sdbusplus::common::xyz::openbmc_project::state::decorator::PowerState;
 
+using CreateIface = sdbusplus::common::xyz::openbmc_project::logging::Create;
+using EntryIface = sdbusplus::common::xyz::openbmc_project::logging::Entry;
+using DbusFailureError = sdbusplus::error::com::ibm::vpd::DbusFailure;
+
 enum class GpioValue : int8_t
 {
     INVALID_VALUE = -1,
@@ -15,11 +22,11 @@ enum class GpioValue : int8_t
     ON
 };
 
-enum class BmcPosition : uint8_t
+enum class BmcPosition : int8_t
 {
+    INVALID_VALUE = -1,
     POSITION_0,
-    POSITION_1,
-    DEFAULT = POSITION_0
+    POSITION_1
 };
 } // namespace types
 } // namespace pgood_chassis_check
