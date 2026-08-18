0a64e8274ae87a9681cf570aa09f9caef6a26dd0
Souvik Roy <souvikroyofficial10@gmail.com>
2026-08-18 07:10:11 +0000

    Introduce IPMI FRU VPD parser
    This commit introduces infrastructure changes in vpd-manager for parsing
    VPD of FRUs which follow the format specified in IPMI Platform
    Management FRU Information Storage Definition[1]. Many FRUs in the
    industry for example NVMe SSDs have VPD stored in this format, hence
    vpd-manager should also be able to parse this format.
    
    This commit only includes build file changes and stub implementation of
    the parser. The actual parser implementation shall be handled in
    subsequent commits.
    
    ```
    [1] https://nvmexpress.org/wp-content/uploads/NVM-Express-Management-Interface-Specification-Revision-1.2e-2024.07.28-Ratified.pdf
    ```
    
    Change-Id: I6865d8ef61d7b7b4679561eda1b57041d7653baf
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/types.hpp
+++ vpd-manager/include/types.hpp
@@ -7,6 +7,7 @@
 #include <xyz/openbmc_project/Common/Progress/common.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
 
+#include <optional>
 #include <tuple>
 #include <unordered_map>
 #include <variant>
@@ -78,6 +79,26 @@ using KWdVPDValueType = std::variant<BinaryVector,std::string, size_t>;
 /* This hold map of parsed data of keyword VPD type*/
 using KeywordVpdMap = std::unordered_map<std::string, KWdVPDValueType>;
 
+/* Data structure for holding IPMI VPD data */
+enum class IpmiVpdAreaIndex : uint8_t
+{
+    COMMON_HEADER,
+    INTERNAL_USE_AREA,
+    CHASSIS_INFO_AREA,
+    BOARD_INFO_AREA,
+    PRODUCT_INFO_AREA,
+    MULTI_RECORD_AREA,
+    SIZE    // total number of areas. This should always be the last entry in the enum
+};
+
+/* Map for holding per Area data
+    The format is <Keyword, Value>. For example, for Manufacturer Name in  Product Info Area,  the map entry will be of format <MNAME, IBM>
+*/
+using IPMIVpdValueMap = std::unordered_map<std::string, KWdVPDValueType>;
+/* Map for holding IPMI VPD data by area. For example for Manufacturer Name in  Product Info Area Product Info Area, the map entry will be of format <Product Info Area, <MNAME, IBM>> */
+using IPMIVpdMap = std::array<std::optional<IPMIVpdValueMap>,
+                              static_cast<std::size_t>(IpmiVpdAreaIndex::SIZE)>;
+
 /**
  * Both Keyword VPD parser and DDIMM parser stores the
  * parsed VPD in the same format.
@@ -115,7 +136,7 @@ using PoundKwSize = uint16_t;
 
 using RecordOffsetList = std::vector<uint32_t>;
 
-using VPDMapVariant = std::variant<std::monostate, IPZVpdMap, KeywordVpdMap>;
+using VPDMapVariant = std::variant<std::monostate, IPZVpdMap, KeywordVpdMap, IPMIVpdMap>;
 
 using HWVerList = std::vector<std::pair<std::string, std::string>>;
 /**
