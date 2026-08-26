20a7c49166eb8e48a720fe54798b9e17ac86899d
Souvik Roy <souvikroyofficial10@gmail.com>
2026-08-26 16:37:32 +0000

    IpmiFruParser: unit test: add file parsing
    Change-Id: I12cc76826f9054cad9372ecfd578047343cb6b10
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- test/utest_ipmi_fru_parser.cpp
+++ test/utest_ipmi_fru_parser.cpp
@@ -3,6 +3,7 @@
 #include "types.hpp"
 
 #include <exception>
+#include <fstream>
 #include <string>
 #include <variant>
 #include <vector>
@@ -26,6 +27,24 @@ uint8_t calculateChecksum(const types::BinaryVector& i_vector, size_t i_start,
         l_sum = static_cast<uint8_t>(l_sum + i_vector[l_idx]);
     }
     return static_cast<uint8_t>(-l_sum);
+}
+
+/**
+ * @brief Helper function to read binary VPD data from a file
+ *
+ * @param[in] i_filePath - Path to the binary VPD file
+ * @return types::BinaryVector - Raw bytes read from file
+ */
+types::BinaryVector readVpdFile(const std::string& i_filePath)
+{
+    std::ifstream l_stream(i_filePath, std::ios::binary);
+    if (!l_stream)
+    {
+        throw std::runtime_error("Failed to open VPD file: " + i_filePath);
+    }
+
+    return types::BinaryVector(std::istreambuf_iterator<char>(l_stream),
+                               std::istreambuf_iterator<char>());
 }
 
 /**
@@ -136,6 +155,57 @@ TEST(IpmiFruParserTest, GoodTestCase)
     l_checkStringField("PSN", "S6Z1NE0W123456");
     l_checkStringField("ASSET_TAG", "ASSET123");
     l_checkStringField("FRU_FILE_ID", "FRUFILE01");
+}
+
+TEST(IpmiFruParserTest, ParseFromFile_MZ3L615THBLF)
+{
+    const std::string l_vpdFilePath = "vpd_files/MZ3L615THBLF-00AW7.bin";
+    types::BinaryVector l_vpdVector = readVpdFile(l_vpdFilePath);
+
+    IpmiFruParser l_parser(l_vpdVector);
+    auto l_parsedMap = l_parser.parse();
+    auto l_ipmiVpdMapPtr = std::get_if<types::IPMIVpdMap>(&l_parsedMap);
+    ASSERT_NE(l_ipmiVpdMapPtr, nullptr);
+
+    const auto& l_fruMap = *l_ipmiVpdMapPtr;
+
+    // 1. Validate Common Header parsing
+    const auto& l_hdrOpt =
+        l_fruMap[static_cast<size_t>(types::IpmiVpdAreaIndex::COMMON_HEADER)];
+    ASSERT_TRUE(l_hdrOpt.has_value());
+    auto l_hdrItr = l_hdrOpt->find("HEADER_FormatVersion");
+    ASSERT_NE(l_hdrItr, l_hdrOpt->end());
+    auto l_hdrVer = std::get_if<types::BinaryVector>(&l_hdrItr->second);
+    ASSERT_NE(l_hdrVer, nullptr);
+    EXPECT_EQ((*l_hdrVer)[0], 0x01);
+
+    // 2. Validate Product Info Area parsing
+    const auto& l_prodOpt = l_fruMap[static_cast<size_t>(
+        types::IpmiVpdAreaIndex::PRODUCT_INFO_AREA)];
+    ASSERT_TRUE(l_prodOpt.has_value());
+    const auto& l_prodMap = *l_prodOpt;
+
+    // Language Code (0x19 = English)
+    auto l_langItr = l_prodMap.find("PLCODE");
+    ASSERT_NE(l_langItr, l_prodMap.end());
+    auto l_langCode = std::get_if<types::BinaryVector>(&l_langItr->second);
+    ASSERT_NE(l_langCode, nullptr);
+    EXPECT_EQ((*l_langCode)[0], 0x19);
+
+    // Predefined 8-bit ASCII fields
+    auto l_checkStringField =
+        [&](const std::string& i_key, const std::string& i_expectedVal) {
+            auto l_itr = l_prodMap.find(i_key);
+            ASSERT_NE(l_itr, l_prodMap.end());
+            auto l_val = std::get_if<std::string>(&l_itr->second);
+            ASSERT_NE(l_val, nullptr);
+            EXPECT_EQ(*l_val, i_expectedVal);
+        };
+
+    l_checkStringField("MNAME", "Samsung");
+    l_checkStringField("PNAME", "PM9D3a");
+    l_checkStringField("PPMN", "SAMSUNG MZ3L615THBLF-00AW7");
+    l_checkStringField("PSN", std::string(20, ' '));
 }
 
 TEST(IpmiFruParserTest, BufferTooSmall)
