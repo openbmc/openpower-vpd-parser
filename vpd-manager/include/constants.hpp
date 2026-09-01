b76df31f1ae94bc4d7a6f900ddeb0da29e63e72c
Souvik Roy <souvikroyofficial10@gmail.com>
2026-09-01 10:10:59 +0000

    Implement method to handle BMC ReadyToRemove
    This commit implements processBmcReadyToRemove method in
    wait-vpd-parsers which was introduced in previous commit[1].
    
    ```
    [1](https://gerrit.openbmc.org/c/openbmc/openpower-vpd-parser/+/94142)
    ```
    
    Change-Id: I9c3b98770f9647ac23090071eecef30bbdbc2477
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/constants.hpp
+++ vpd-manager/include/constants.hpp
@@ -170,6 +170,9 @@ constexpr auto hostService = "xyz.openbmc_project.State.Host";
 constexpr auto hostRunningState =
     "xyz.openbmc_project.State.Host.HostState.Running";
 
+constexpr auto readyToRemoveIface = "xyz.openbmc_project.State.ReadyToRemove";
+constexpr auto readyToRemoveProperty = "ReadyToRemove";
+
 constexpr auto badVpdDir = "/var/lib/vpd/dumps/";
 constexpr auto functionalProperty = "Functional";
 constexpr auto enabledProperty = "Enabled";
@@ -239,5 +242,10 @@ static constexpr auto fileModeDirectoryPath = "/var/lib/vpd/file";
 static constexpr auto pimBackupPath =
     "/var/lib/phosphor-data-sync/bmc_data_bkp/var/lib/phosphor-inventory-manager";
 static constexpr auto pimPrimaryPath = "/var/lib/phosphor-inventory-manager";
+
+//@todo: use PDI generated header once Position interface gets merged upstream
+constexpr auto positionInterface =
+    "xyz.openbmc_project.Inventory.Decorator.Position";
+constexpr auto positionPropertyName = "Position";
 } // namespace constants
 } // namespace vpd
