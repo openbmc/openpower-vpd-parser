dc7c30c9ace2913206a838df31b8ee689f94246d
Souvik Roy <souvikroyofficial10@gmail.com>
2026-09-04 09:46:52 +0000

    Implement utility API to get BMC inventory paths
    This commit implements utility API to get BMC inventory paths which was
    introduced in a previous commit[1]. This API is required by IbmHandler
    to handle ReadyToRemove interface.
    
    Change-Id: Ifdb05471ddbda0befda7f2695010e9f382aa6d73
    Signed-off-by: Souvik Roy <souvikroyofficial10@gmail.com>

--- vpd-manager/include/constants.hpp
+++ vpd-manager/include/constants.hpp
@@ -253,5 +253,13 @@ static constexpr auto positionPropertyName = "Position";
 static constexpr auto readyToRemoveIface =
     "xyz.openbmc_project.State.ReadyToRemove";
 static constexpr auto readyToRemoveProperty = "ReadyToRemove";
+
+//@todo: use PDI generated header once PhysicalContext interface gets merged
+// upstream
+static constexpr auto physicalContextInterface =
+    "xyz.openbmc_project.Common.PhysicalContext";
+static constexpr auto physicalContextTypeProperty = "Type";
+static constexpr auto physicalContextManagerValue =
+    "xyz.openbmc_project.Common.PhysicalContext.PhysicalContextType.Manager";
 } // namespace constants
 } // namespace vpd
