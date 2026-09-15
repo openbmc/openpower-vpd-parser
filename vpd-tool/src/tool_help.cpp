025638c41854ec5a54d32fca4969d569111fa2de
Rekha Aparna <vrekhaaparna@ibm.com>
2026-09-15 01:05:07 -0500

    Refactor vpd-tool help
    Previously, vpd-tool displayed all usage information through a single
    CLI footer, resulting in verbose and noisy help output without
    operation-specific guidance.
    
    Removed the updateFooter() implementation and introduced VpdToolHelp
    class to handle help based on the requested operation. Added generic
    help for the available operations and introduced writeKeyword specific
    help handling as a stub. The writeKeyword help content will be
    implemented in a subsequent commit.
    
    This provides a cleaner help structure and makes the help content
    easier to maintain independently for each operation.
    
    ```
    Test:
    
    root@huygens:~# vpd-tool -u --help
    VPD Command Line Tool
    
    Operations:
      readKeyword                   Read a keyword value from DBus or hardware
      writeKeyword                  Write a keyword value to DBus and/or hardware
      dumpInventory                 Dump inventory information
      validateRedundantEeprom       Validate an EEPROM against its redundant copy
      enterSplitMode                Configure the system to operate in split mode
      exitSplitMode                 Configure the system to exit split mode
    
    Use 'vpd-tool <operation> --help' for detailed usage.
    ```
    
    Change-Id: I1e87b51fc0a21235723552efbfa5230a53b613ea
    Signed-off-by: Rekha Aparna <vrekhaaparna@ibm.com>

---
+++ vpd-tool/src/tool_help.cpp
@@ -0,0 +1,74 @@
+#include "tool_help.hpp"
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace vpd
+{
+
+bool VpdToolHelp::printHelp(int argc, char** argv) const
+{
+    // Print generic help when no options are provided.
+    if (argc == 1)
+    {
+        printGenericHelp();
+        return true;
+    }
+
+    const std::vector<std::string> args(argv + 1, argv + argc);
+
+    // Return early if --help / -h is not present.
+    const bool wantHelp = std::ranges::any_of(args, [](const auto& arg) {
+        return arg == "--help" || arg == "-h";
+    });
+
+    if (!wantHelp)
+    {
+        return false;
+    }
+
+    // Map every recognised operation flag to its print method.
+    const std::unordered_map<std::string,
+                             std::function<void(const VpdToolHelp*)>>
+        flagToHelp{
+            {"writeKeyword", &VpdToolHelp::printWriteKeywordHelp},
+        };
+
+    for (const auto& arg : args)
+    {
+        if (const auto it = flagToHelp.find(arg); it != flagToHelp.end())
+        {
+            it->second(this);
+            return true;
+        }
+    }
+
+    // No operation specified: print generic help.
+    printGenericHelp();
+    return true;
+}
+
+void VpdToolHelp::printGenericHelp() const noexcept
+{
+    std::cout
+        << "VPD Command Line Tool\n\n"
+        << "Operations:\n"
+        << "  readKeyword                   Read a keyword value from DBus or hardware\n"
+        << "  writeKeyword                  Write a keyword value to DBus and/or hardware\n"
+        << "  dumpInventory                 Dump inventory information\n"
+        << "  validateRedundantEeprom       Validate an EEPROM against its redundant copy\n"
+        << "  enterSplitMode                Configure the system to operate in split mode\n"
+        << "  exitSplitMode                 Configure the system to exit split mode\n"
+        << "\n"
+        << "Use 'vpd-tool <operation> --help' for more information on a specific operation.\n";
+}
+
+void VpdToolHelp::printWriteKeywordHelp() const noexcept
+{
+    // TODO - print write keyword help
+}
+} // namespace vpd
